714cd7094c605c19e3e7de3a586a3a05ccd3efa4
Jon Code <programcodex@gmail.com>
2012-07-22 20:06:26 -0400

    change phot_url to photo in app

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,10 @@
 module UsersHelper
   
   def gravatar_for(user)
-    if user.photo_url.blank?
+    if user.photo.blank?
       gravatar_url = "blank.png"
     else
-      gravatar_url =  user.photo_url.to_s
+      gravatar_url =  user.photo.to_s
     end 
     image_tag(gravatar_url, alt: user.username, class: "gravatar") 
   end 
