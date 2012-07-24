6d58e55959da53273afa380f7fe47169deaaac80
Jon Code <programcodex@gmail.com>
2012-07-24 00:34:28 -0400

    new icons

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,7 @@ module UsersHelper
   
   def gravatar_for(user)
     if user.photo.blank?
-      gravatar_url = "blank.png"
+      gravatar_url = "ninja_ghost_128.png"
     else
       gravatar_url =  user.photo.to_s
     end 
