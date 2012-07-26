3996671f893b4de719a5c3177cf9266d7647f9d9
Jon Code <programcodex@gmail.com>
2012-07-26 00:19:53 -0400

    explicit key to object data store

--- app/controllers/iphone_controller.rb
+++ app/controllers/iphone_controller.rb
@@ -24,7 +24,20 @@ class IphoneController < AppController
   
     def create_user_account(data)
       # data_hash = JSON.parse data
-      new_user = User.new(data)
+      new_user = User.new
+      new_user.credit_number = data["credit_number"]
+      new_user.phone = data["phone"]
+      new_user.email = data["email"]
+      new_user.password = data["password"]
+      new_user.password_confirmation = data["password_confirmation"]
+      new_user.state = data["state"]
+      new_user.city = data["city"]
+      new_user.zip = data["zip"].to_i
+      new_user.first_name = data["first_name"]
+      new_user.last_name = data["last_name"]
+      new_user.address = data["address"]
       return new_user
     end
 end
+
+# {"credit_number"=>"[FILTERED]", "first_name"=>"Taylor", "phone"=>"2052920078", "address"=>"200 Spooner St", "last_name"=>"Addison", "city"=>"Tuscaloosa", "email"=>"test@gmail.com", "state"=>"AL", "zip"=>"35401", "password"=>"[FILTERED]", "password_confirmation"=>"[FILTERED]", "controller"=>"iphone", "action"=>"create_account", "format"=>"json"}
