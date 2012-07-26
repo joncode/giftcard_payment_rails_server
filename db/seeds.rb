9bd1e49cd7cf4b297ea7dfbb3b8fb673e260992c
Jon Code <programcodex@gmail.com>
2012-07-26 06:18:58 -0400

    gift random creator for seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -138,12 +138,71 @@ p_id.each do |provider|
   menu_string.save!
 end
   
-  
-  
-# database issues    
+######################                GIFTS               ######################
 
-# 2 association between menu_string and menu is off because .menu in menu_strings breaks due to confusion with association
-# 1 has and belongs to many association from items and menu are off
+#  credit_card          :string(100)
+#  redeem_id            :integer
+#  status               :string(255)
+#  created_at           :datetime        not null
+#  updated_at           :datetime        not null
 
-# 4 the json string has row numbers for iphone when it should just be a menu key with an array of item hashes
+# make 100 gifts
+# have 50 of them redeemed
+# what about using the created_at to put them in microposts
+# make ten gifts , then redeem 5 , have the 5 redeems be random
+# keep going till you have redeemed 50
+# randomly pick a user to give
+# randomly pick a user to receive
+# randomly pick a provider
+# randomly pick an item
+messages = ["Happy Birthday!", "Congratulations!", "One good turn deserves another", "have fun tonight", "Cheers"]
+notes = ["Shaken not Stirred", "Draft if you got it", "Fireworks!", "Ice Cold", "Surprise please", "Umbrellas" ]
+users_total = User.count
+users_array = User.all
+providers_total = Provider.count
+providers_array = Provider.all
+items_total = Item.count
+items_array = Item.all
+messages_total = message.count
+notes_total = notes.count
+gift_hash = {}
+20.times do |t|
+  
+  giving_user_index = receiving_user_index = rand users_total
+  while receiving_user_index == giving_user_index
+    receiving_user_index = rand users_total
+  end
+  giving_user = User.find users_array[giving_user_index]
+  receiving_user = User.find users_array[receiving_user_index]
+  
+  provider_index = rand providers_total 
+  provider = Provider.find providers_array[provider_index]
+  
+  item_index = rand items_total 
+  item = Item.find items_array[item_index]
+  menu_item = Menu.find_by_item_id_and_provider_id item, provider
+  quantity = rand(5) + 1
+  total = mene_item.price.to_i * (quantity + 1)
+  message = note = ""
+  if quantity < 4 
+    message_index = rand messages_total
+    message = messages[message_index]
+  end
+  if giving_user_index < 3  
+    notes_index = rand notes_total
+    note = notes[notes_index]
+  end
+  
+  gift_hash = {}
+  gift_hash = {giver_id: giving_user.id, 
+    receiver_id: receiving_user.id,
+    provider_id: provider.id, 
+    item_id: item.id, price: menu_item.price, 
+    quantity: quantity, total: total.to_s,
+     message: message, special_instruction: note
+     status: 'open'}
+  Gift.create([gift_hash])
+end
+     
+
 
