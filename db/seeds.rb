62c7fbf1d4162b9aed337d12cf299714c056723a
Jon Code <programcodex@gmail.com>
2012-07-26 18:44:58 -0400

    seed file making gifts redeems and orders and changing gift status

--- db/seeds.rb
+++ db/seeds.rb
@@ -158,33 +158,45 @@ end
 messages = ["Happy Birthday!", "Congratulations!", "One good turn deserves another", "have fun tonight", "Cheers"]
 notes = ["Shaken not Stirred", "Draft if you got it", "Fireworks!", "Ice Cold", "Surprise please", "Umbrellas" ]
 reply = ["thanks dude your rock!", "yo yo yo get over here", "thats so sweet of you ", "there is a season", "Cheers!", "down the hatch!"] 
-users_total = User.count
-users_array = User.all
-providers_total = Provider.count
+
+users_array     = User.all
+users_total     = users_array.count
+
 providers_array = Provider.all
-items_total = Item.count
-items_array = Item.all
-messages_total = message.count
-notes_total = notes.count
+providers_total = providers_array.count
+
+items_array     = Item.all
+items_total     = items_array.count
+
+messages_total  = messages.count
+notes_total     = notes.count
+replies = reply.count
+
+
 gift_hash = {}
+
 5.times do 
   20.times do 
-    giving_user_index = receiving_user_index = rand users_total
+    first_user_id           = rand users_total
+    giving_user_index       = first_user_id
+    receiving_user_index    = first_user_id
     while receiving_user_index == giving_user_index
-      receiving_user_index = rand users_total
+      receiving_user_index  = rand users_total
     end
-    giving_user = User.find users_array[giving_user_index]
-    receiving_user = User.find users_array[receiving_user_index]
+    giving_user     = users_array[giving_user_index]
+    receiving_user  = users_array[receiving_user_index]
   
-    provider_index = rand providers_total 
-    provider = Provider.find providers_array[provider_index]
+    provider_index  = rand providers_total 
+    provider        = providers_array[provider_index]
   
-    item_index = rand items_total 
-    item = Item.find items_array[item_index]
+    item_index      = rand items_total 
+    item            = items_array[item_index]
+    
     menu_item = Menu.find_by_item_id_and_provider_id item, provider
     quantity = rand(5) + 1
-    total = mene_item.price.to_i * (quantity + 1)
-    message = note = ""
+    total = menu_item.price.to_i * (quantity + 1)
+    message = "" 
+    note = ""
     if quantity < 4 
       message_index = rand messages_total
       message = messages[message_index]
@@ -194,13 +206,13 @@ gift_hash = {}
       note = notes[notes_index]
     end
   
-    gift_hash = {}
+    # gift_hash = {}
     gift_hash = {giver_id: giving_user.id, 
       receiver_id: receiving_user.id,
       provider_id: provider.id, 
       item_id: item.id, price: menu_item.price, 
       quantity: quantity, total: total.to_s,
-       message: message, special_instruction: note
+       message: message, special_instructions: note,
        status: 'open'}
     Gift.create([gift_hash])
   end
@@ -212,11 +224,11 @@ gift_hash = {}
 
   gifts = Gift.all
   gifts_total = gifts.count
-  replies = reply.count
+
   10.times do 
     index = rand gifts_total
-
-    gift = gifts.slice!(index)
+    gift = gifts.slice! index
+    gifts_total -= 1
     redeem = Redeem.new
     redeem.gift_id = gift
     odds = rand 3
@@ -225,10 +237,51 @@ gift_hash = {}
       redeem.reply_message = reply[reply_index]
     end
     redeem.redeem_code = rand 10000
+    gift.update_attribute(:status, 'notified')
+    if odds != 2  
+      notes_index = rand notes_total
+      redeem.special_instructions = notes[notes_index]
+    end
+    
     redeem.save
     # how to make the redeem code uniq, but adding the location_id and comparing
   end
-end
+
 
 ######################                ORDERS               ######################
+#  id          :integer         not null, primary key
+#  redeem_id   :integer
+#  gift_id     :integer
+#  redeem_code :integer
+#  created_at  :datetime        not null
+#  updated_at  :datetime        not null
+# make 3 orders per 5 redeems undone 
+
+  redeems = Redeem.all
+  redeems_total = redeems.count
+
+  6.times do
+     redeem_index = rand redeems_total
+     redeem = redeems.slice! redeem_index
+     redeems_total -= 1
+     order = Order.new
+     order.gift_id = redeem.gift_id
+     order.redeem_id = redeem
+     order.redeem_code = redeem.redeem_code
+     order.save
+     gift = Gift.find(order.gift_id)
+     gift.update_attribute(:status, 'redeemed')
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
 
