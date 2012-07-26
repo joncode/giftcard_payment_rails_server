a7dd261050b3af576823dde993792308ff9bf53f
Jon Code <programcodex@gmail.com>
2012-07-26 00:48:21 -0400

    remove commented old code, it works

--- app/controllers/iphone_controller.rb
+++ app/controllers/iphone_controller.rb
@@ -23,7 +23,6 @@ class IphoneController < AppController
   private
   
     def create_user_account(data)
-      # data_hash = JSON.parse data
       worked_data = data.symbolize_keys
       zip_code = worked_data[:zip]
       worked_data[:zip] = zip_code.to_i
@@ -31,19 +30,6 @@ class IphoneController < AppController
       worked_data.delete :action
       worked_data.delete :format
       new_user = User.new(worked_data)
-      # new_user.credit_number = data["credit_number"]
-      # new_user.phone = data["phone"]
-      # new_user.email = data["email"]
-      # new_user.password = data["password"]
-      # new_user.password_confirmation = data["password_confirmation"]
-      # new_user.state = data["state"]
-      # new_user.city = data["city"]
-      # new_user.zip = data["zip"].to_i
-      # new_user.first_name = data["first_name"]
-      # new_user.last_name = data["last_name"]
-      # new_user.address = data["address"]
       return new_user
     end
 end
-
-# {"credit_number"=>"[FILTERED]", "first_name"=>"Taylor", "phone"=>"2052920078", "address"=>"200 Spooner St", "last_name"=>"Addison", "city"=>"Tuscaloosa", "email"=>"test@gmail.com", "state"=>"AL", "zip"=>"35401", "password"=>"[FILTERED]", "password_confirmation"=>"[FILTERED]", "controller"=>"iphone", "action"=>"create_account", "format"=>"json"}
