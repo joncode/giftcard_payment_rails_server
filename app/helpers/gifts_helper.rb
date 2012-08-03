da74a4e18411a02475d57a5814eb004585052287
Jon Code <programcodex@gmail.com>
2012-08-03 15:53:38 -0400

    add icon images to drinkboards

--- app/helpers/gifts_helper.rb
+++ app/helpers/gifts_helper.rb
@@ -1,2 +1,20 @@
 module GiftsHelper
+  
+  def get_category_image(gift)
+    case gift.item.category
+    when 0
+     "special_white.png"
+    when 1
+      "beer_white.png"
+    when 2
+      "wine_white.png"
+    when 3
+      "cocktail_white.png"
+    when 4
+      "shots_white.png"
+    end
+  end
+  
 end
+
+# BEVERAGE_CATEGORIES = ['special', 'beer', 'wine', 'cocktail', 'shot']
