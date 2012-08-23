d9266b5da4030166222efd2ff32bf85650482623
Jon Code <programcodex@gmail.com>
2012-08-22 23:56:07 -0400

    provider logo image in tables throughou plus provider.photo in db

--- app/helpers/providers_helper.rb
+++ app/helpers/providers_helper.rb
@@ -1,2 +1,26 @@
 module ProvidersHelper
+  
+  def logo_for(provider)
+    if provider.logo.blank?
+      gravatar_url = "biz_logo.png"
+    else
+      gravatar_url =  provider.logo.to_s
+    end 
+    image_tag(gravatar_url, alt: provider.name, class: "iconListView") 
+  end
+  
+  def logo_from_id_for(provider_id)
+    if provider_id.nil? 
+      provider = Provider.new
+    else
+      provider = Provider.find(provider_id)
+    end
+    if provider.logo.blank?
+      gravatar_url = "biz_logo.png"
+    else
+      gravatar_url =  provider.logo.to_s
+    end 
+    image_tag(gravatar_url, alt: provider.name, class: "iconListView") 
+  end
+
 end
