28f5b1ed4ed1ce2930db4bc9e8b8f7567c6e3ff2
Jon Code <programcodex@gmail.com>
2012-09-04 19:43:11 -0400

    remove serialize on providers and users

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -27,8 +27,8 @@
 class Provider < ActiveRecord::Base
   attr_accessible :address, :city, :description, :logo, :name, :state, :user_id, :staff_id, :zip, :zinger, :phone, :email, :twitter, :facebook, :website
   
-  serialize  :user_id, Array
-  serialize  :staff_id, Array
+  # serialize  :user_id, Array
+  # serialize  :staff_id, Array
                                                                                                   
   belongs_to :user                                                                              
   has_one    :menu                                                                              
