e6d7abc6e6e2ba9477b686cce2d6af8a3d651ed8
Jon Code <programcodex@gmail.com>
2012-09-04 20:18:55 -0400

    merchant sign up sign in fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   
   def merchant_tag(user)
-    if user.provider_id.empty? || !(user.provider_id.kind_of? Array)
+    if user.provider_id.nil? 
       link_to "Merchant Sign Up", new_provider_path
     else
       link_to "Merchant Sign In", merchants_path
