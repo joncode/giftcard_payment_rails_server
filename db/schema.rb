c1aa017db32c3c9706241724ec8f2afe7ff43bc5
robertSahm <robertsahm@gmail.com>
2012-09-14 18:01:19 -0400

    new

--- db/schema.rb
+++ db/schema.rb
@@ -79,7 +79,7 @@ ActiveRecord::Schema.define(:version => 20120908052501) do
     t.text     "data",         :null => false
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
-  end  
+  end
 
   create_table "menus", :force => true do |t|
     t.integer  "provider_id",               :null => false
