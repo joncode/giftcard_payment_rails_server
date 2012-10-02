fdfc4f207b2d8d7fefb3dbe36c4aa47c4e89882f
Jon Code <programcodex@gmail.com>
2012-10-02 00:56:24 -0400

    custom icon helper with cloudinary calls

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -10,6 +10,17 @@ module UsersHelper
       image_tag(user.photo_url(:gravatar), :width => width, :height => height)
     end   
   end
+
+  def list_standard_icon_for(user)
+    width   = 150
+    height  = 150
+    if user.photo.blank?
+      photo = "http://res.cloudinary.com/htaaxtzcv/image/upload/c_fill,h_#{height},w_#{width}/v1349148077/ezsucdxfcc7iwrztkags.png"
+      image_tag(photo, alt: "hello",:class => 'gravatar' )
+    else
+      image_tag(user.photo_url(:standard), :width => width, :height => height)
+    end   
+  end
   
   def list_icon_for(user)
     width   = 50
@@ -31,6 +42,17 @@ module UsersHelper
     list_icon_for user  
   end
   
+  def custom_image_tag(user,width,height)
+    if user.photo.blank?
+      photo = "http://res.cloudinary.com/drinkboard/image/upload/c_fill,h_#{height},w_#{width}/v1349148077/ezsucdxfcc7iwrztkags.png"
+    else
+      photo_url = user.photo.dup.to_s
+      photo_array = photo_url.split('upload/')
+      photo = "http://res.cloudinary.com/drinkboard/image/upload/c_fill,h_#{height},w_#{width}/#{photo_array[1]}"      
+    end 
+    image_tag(photo, alt: "customImageTag", :class => 'gravatar' )
+  end
+
   def large_photo(user)
     image_tag(user.photo_url(:large), :width => 400, :height => 400, :id => "cropbox")
   end
