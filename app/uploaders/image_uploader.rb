0693d29b8576467254b37b7be7320266467881e6
Jon Code <programcodex@gmail.com>
2012-10-01 22:12:58 -0400

    re-adding carrierwave functions

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -9,13 +9,13 @@ class ImageUploader < CarrierWave::Uploader::Base
     process :resize_to_fill => [400, 400, :north]
   end
   
-  # version :large do
-  #   process :resize_to_fill => [400, 400]
-  # end
+  version :large do
+    process :resize_to_fill => [400, 400]
+  end
   
-  # version :thumbnail do
-  #   process :resize_to_fit => [75, 100]
-  # end
+  version :thumbnail do
+    process :resize_to_fit => [75, 100]
+  end
 
   # def crop
     # if model.crop_x.present?
