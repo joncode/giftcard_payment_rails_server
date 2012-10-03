15d0746478c5465d4e72f78dd4e545c26693d6f2
Jon Code <programcodex@gmail.com>
2012-10-02 20:09:06 -0400

    custom image tag that uses use_photo to choose photo url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,19 +8,52 @@ module ApplicationHelper
     end
   end
  
+  # def custom_image_tag(object,width,height)
+  #   if object.photo.blank?
+  #     if object.kind_of? User
+  #       photo = "#{CLOUDINARY_IMAGE_URL}/c_fill,h_#{height},w_#{width}/v1349148077/ezsucdxfcc7iwrztkags.png"
+  #     else
+  #       photo = "#{CLOUDINARY_IMAGE_URL}/c_fill,h_#{height},w_#{width}/v1349150293/upqygknnlerbevz4jpnw.png"
+  #     end
+  #   else
+  #     photo_url   = object.photo.dup.to_s
+  #     photo_array = photo_url.split('upload/')
+  #     photo = "#{CLOUDINARY_IMAGE_URL}/c_fill,h_#{height},w_#{width}/#{photo_array[1]}"      
+  #   end 
+  #   image_tag(photo, alt: "customImageTag", :class => 'customImageTag' )
+  # end
+
   def custom_image_tag(object,width,height)
-    if object.photo.blank?
-      if object.kind_of? User
-        photo = "#{CLOUDINARY_IMAGE_URL}/c_fill,h_#{height},w_#{width}/v1349148077/ezsucdxfcc7iwrztkags.png"
-      else
-        photo = "#{CLOUDINARY_IMAGE_URL}/c_fill,h_#{height},w_#{width}/v1349150293/upqygknnlerbevz4jpnw.png"
-      end
+    if object.kind_of? Provider
+      photo = "#{CLOUDINARY_IMAGE_URL}/c_fill,h_#{height},w_#{width}/v1349150293/upqygknnlerbevz4jpnw.png"
     else
-      photo_url   = object.photo.dup.to_s
-      photo_array = photo_url.split('upload/')
-      photo = "#{CLOUDINARY_IMAGE_URL}/c_fill,h_#{height},w_#{width}/#{photo_array[1]}"      
+      photo = choose_photo(object, width, height)
     end 
     image_tag(photo, alt: "customImageTag", :class => 'customImageTag' )
   end
+
+  def choose_photo(object, width, height)
+    case object.use_photo
+    when "cw"
+      photo_url   = object.photo.dup.to_s
+      photo_array = photo_url.split('upload/')
+      "#{CLOUDINARY_IMAGE_URL}/c_fill,h_#{height},w_#{width}/#{photo_array[1]}" 
+    when "ios"
+      object.iphone_photo
+    when "fb"
+      # object.fb_photo
+      # need to add code for fb photo store and use  
+      "#{CLOUDINARY_IMAGE_URL}/c_fill,h_#{height},w_#{width}/v1349148077/ezsucdxfcc7iwrztkags.png"
+    else
+      if object.photo.blank?
+        "#{CLOUDINARY_IMAGE_URL}/c_fill,h_#{height},w_#{width}/v1349148077/ezsucdxfcc7iwrztkags.png"
+      else
+        photo_url   = object.photo.dup.to_s
+        photo_array = photo_url.split('upload/')
+        "#{CLOUDINARY_IMAGE_URL}/c_fill,h_#{height},w_#{width}/#{photo_array[1]}"
+      end
+    end
+  end
+
 
 end
