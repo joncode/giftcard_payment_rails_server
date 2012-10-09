95ab44fad136cd0cf3aa9fbb505b73ed8017bbfb
robertSahm <robertsahm@gmail.com>
2012-10-08 21:03:13 -0400

    added tons of styles/textures

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,9 +3,10 @@
 // be included in the compiled file accessible from http://example.com/assets/application.js
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file. remeber_to_require jquery.ui.all
-//= require bootstrap.js.coffee
+
 //= require jquery
 //= require scripts.js
+//= require twitter/bootstrap
 //= require jquery.isotope.min
 //= require jquery.Jcrop.min
 //= require isotope_script
