7a5b78eb3b6516c4166b917032bd6882ae476269
Jon Code <programcodex@gmail.com>
2012-10-09 04:36:24 -0400

    new custom image tag with user.get_photo method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,43 +9,14 @@ module ApplicationHelper
   end
 
   def custom_image_tag(object,width,height)
+    crop = "/c_fill,h_#{height},w_#{width}/"
     if object.kind_of? Provider
-      photo = "#{CLOUDINARY_IMAGE_URL}/c_fill,h_#{height},w_#{width}/v1349150293/upqygknnlerbevz4jpnw.png"
+      photo = "#{CLOUDINARY_IMAGE_URL}" + crop + "v1349150293/upqygknnlerbevz4jpnw.png"
     else
-      photo = choose_photo(object, width, height)
+      url_array = object.get_photo.split('upload/')
+      photo     = url_array[0] + 'upload' + crop + url_array[1]
     end 
-    image_tag(photo, alt: "customImageTag2", :class => 'customImageTag' )
+    image_tag(photo, alt: "customImageTag3", :class => 'customImageTag' )
   end
-
-  def choose_photo(object, width, height)
-    case object.use_photo
-    when "cw"
-      puts "in choose_photo with cw flag"
-      photo_url   = object.photo.dup.to_s
-      photo_array = photo_url.split('upload/')
-      photo = "#{CLOUDINARY_IMAGE2_URL}/c_fill,h_#{height},w_#{width}/#{photo_array[1]}" 
-    when "ios"
-      puts "in choose_photo with ios flag"
-      photo_url  = object.iphone_photo
-      photo_array = photo_url.split('upload/')
-      photo = "#{CLOUDINARY_IMAGE_URL}/c_fill,h_#{height},w_#{width}/#{photo_array[1]}" 
-    when "fb"
-      puts "in choose_photo with fb flag"
-      # object.fb_photo
-      # need to add code for fb photo store and use  
-      photo = "#{CLOUDINARY_IMAGE_URL}/c_fill,h_#{height},w_#{width}/v1349221640/yzjd1hk2ljaycqknvtyg.png"
-    else
-      if object.photo.blank?
-        photo = "#{CLOUDINARY_IMAGE_URL}/c_fill,h_#{height},w_#{width}/v1349221640/yzjd1hk2ljaycqknvtyg.png"
-      else
-        photo_url   = object.photo.dup.to_s
-        photo_array = photo_url.split('upload/')
-        puts "in choose_photo else but object.photo not blank"
-        photo = "#{CLOUDINARY_IMAGE2_URL}/c_fill,h_#{height},w_#{width}/#{photo_array[1]}"
-      end
-    end
-    return photo
-  end
-
 
 end
