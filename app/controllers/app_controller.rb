4b2df5bd3caa6237f4f2e1ad61a8c9e6cd5e1fcb
Jon Code <programcodex@gmail.com>
2012-10-17 18:33:51 -0400

    add controller gifts array

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -1,4 +1,84 @@
 class AppController < ApplicationController
- include ActionView::Helpers::DateHelper
-  
+	include ActionView::Helpers::DateHelper
+	GIFT_REPLY = ["giver_id", "giver_name", "item_id", "item_name", "provider_id", "provider_name", "category", "quantity", "message", "created_at", "status", "id"]
+
+ 	def gifts
+	    puts "Gifts"
+	    puts "#{params}"
+
+	    user  = User.find_by_remember_token(params["token"])
+	    if user
+	    	gifts = Gift.get_gifts(user)
+	    	gifts_array = array_these_gifts(gifts, GIFT_REPLY, true)
+	  	else
+	  		gift_hash = {"error" => "user was not found in database"}
+	  		gifts_array = gift_hash
+	  	end
+	    respond_to do |format|
+	      logger.debug gifts_array
+	      format.json { render text: gifts_array.to_json }
+	    end
+  	end
+
+   	def array_these_gifts(obj, send_fields, address_get=false, receiver=false)
+      gifts_ary = []
+      index = 1 
+      obj.each do |g|
+      
+        ### >>>>>>>    item_name pluralizer
+        # g.item_name = g.item_name.pluralize if g.quantity > 1
+        ###  7/27 6:45 UTC
+        
+        if g.created_at
+          time = g.created_at.to_time
+        else
+          time = g.updated_at.to_time
+        end
+        time_string = time_ago_in_words(time)
+      
+        gift_obj = g.serializable_hash only: send_fields
+        gift_obj.each_key do |key|
+          value = gift_obj[key]
+          gift_obj[key] = value.to_s
+        end
+        
+        # add other person photo url 
+        if receiver
+          if g.receiver
+            gift_obj["receiver_photo"]  = g.receiver.get_photo
+          else
+            puts "#Gift ID = #{g.id} -- SAVE FAIL No gift.receiver"
+          end
+        else
+          gift_obj["giver_photo"]       = g.giver.get_photo
+        end
+
+        provider = g.provider 
+        gift_obj["provider_photo"]     = provider.get_photo
+        # add the full provider address
+        if address_get
+          gift_obj["provider_address"] = provider.complete_address
+        end
+
+        gift_obj["time_ago"] = time_string
+      
+        ### >>>>>>>    this is not stored in gift object
+        gift_obj["redeem_code"] = add_redeem_code(g)
+        ###  07-27 9:08 UTC
+            
+        gifts_ary << gift_obj
+      end
+      return gifts_ary
+    end
+
+    protected
+	
+		def add_redeem_code(obj)
+			if obj.status == "notified" 
+				obj.redeem.redeem_code
+			else
+				"none"
+			end
+		end
+ 
 end
