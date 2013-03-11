e0ad56769c0752b5dfd145f0958e96df0b67563f
Jon Code <programcodex@gmail.com>
2013-03-11 02:11:37 -0400

    redis to go env variable pre-set

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,6 +1,7 @@
 Dir[File.join(Rails.root, 'app', 'jobs', '*.rb')].each { |file| require file }
 
 if Rails.env.production?
+  ENV["REDISTOGO_URL"] ||= "redis://redistogo:973cdc884e18482f5b324194e3b4cde1@barb.redistogo.com:9497/"
   uri = URI.parse(ENV["REDISTOGO_URL"])
   Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 else
