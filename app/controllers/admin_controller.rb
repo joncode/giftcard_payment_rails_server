3bf7cb9361ead6f50aaa073ac715a6c88d02be15
Jon Code <programcodex@gmail.com>
2013-04-04 22:18:11 -0400

    adding code to find gift for tests with order and shoppingCart

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -26,15 +26,48 @@ class AdminController < ApplicationController
 		email = "noreplydrinkboard@gmail.com"
 		user  = User.find_by_email "test@test.com"
 		gifts = Gift.where(giver_id: user.id, receiver_id: user.id)
-		gift  = gift[0]
+		# needs to check that there is a shoppingCart
+		gift, flag = find_gift_with_order_and_shoppingCart(gifts)
+
+		if flag
+			Resque.enqueue(EmailJob, 'notify_giver_order_complete', user.id , {:gift_id => gift.id}) 
+ 			@message = "8 email tests sent to #{email}"
+ 		else
+ 			@message = "7 email tests sent to #{email}, 1 not sent 'noitfy_giver_order_complete'"
+ 		end
+		provider = gift.provider
 		Resque.enqueue(EmailJob, 'confirm_email', 	user.id , 	{}) 
 		Resque.enqueue(EmailJob, 'reset_password', 	user.id, 	{}) 
 		Resque.enqueue(EmailJob, 'invite_friend', 	user.id , 	{:email => email, :gift_id => gift.id})
-		Resque.enqueue(EmailJob, 'invite_employee', user.id , 	{:email => email, :gift_id => gift.id})
+		Resque.enqueue(EmailJob, 'invite_employee', user.id , 	{:provider_id => provider.id,:email => email, :gift_id => gift.id})
 		Resque.enqueue(EmailJob, 'alert_giver', 	user.id  , 	{:gift_id => gift.id}) 
-		Resque.enqueue(EmailJob, 'notify_receiver', user_id , 	{:gift_id => gift.id, :email => email}) 
-		Resque.enqueue(EmailJob, 'notify_giver_order_complete', user.id , {:gift_id => gift.id}) 
+		Resque.enqueue(EmailJob, 'notify_receiver', user.id , 	{:gift_id => gift.id, :email => email}) 
 		Resque.enqueue(EmailJob, 'invoice_giver', 	user.id  , 	{:gift_id => gift.id})
+	end
+
+	def find_gift_with_order_and_shoppingCart(gifts)
+		flag = false
+		gifts.each do |g|
+			if g.order 
+				flag = true 
+				gift = g
+			end
+		end
+		
+		gift = gifts[0] if !flag
+		cart = JSON.parse gift.shoppingCart
+		items = cart.count
+		index = 1
+		while items < 2
+			cart = JSON.parse gifts[index].shoppingCart
+			items = cart.count
+			gift = gifts[index]
+			index += 1
+			if index == gifts.count
+				items = 3
+			end
+		end
+		return gift, flag
 	end
 
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -2,7 +2,8 @@ class EmployeesController < ApplicationController
   
   def invite_employee
     if params[:email] && params[:user_id]
-      Resque.enqueue(EmailJob, 'invite_employee', params[:user_id], {:email => params[:email], :gift_id => params[:gift_id]})
+    	# >>>>>> NEED THE PROVIDER ID FOR THIS TO WORK !!!!!!
+      # Resque.enqueue(EmailJob, 'invite_employee', params[:user_id], {:provider_id => @provider.id, :email => params[:email], :gift_id => params[:gift_id]})
     end
   end
   
