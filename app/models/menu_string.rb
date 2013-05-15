039ddfad8e2d83799e6879069fc1698bda6deb4f
Jon Code <programcodex@gmail.com>
2013-05-15 18:24:50 -0400

    removing whitespace from menustring

--- app/models/menu_string.rb
+++ app/models/menu_string.rb
@@ -1,28 +1,28 @@
 class MenuString < ActiveRecord::Base
   	attr_accessible :full_address, :data, :provider_id, :version, :sections_json
-  
+
   	belongs_to :provider
 
   	validates_uniqueness_of :provider_id
 
   	after_save :update_merchant
-  	
+
   	def update_merchant
   		self.provider.update_attribute(:menu_is_live, true)
   	end
-  
+
    	def self.get_menu_for_provider(provider_id)
-  		menu_string = MenuString.find_by_provider_id(provider_id) 
+  		menu_string = MenuString.find_by_provider_id(provider_id)
   		if !menu_string
 			menu_string = MenuString.new
 			menu_string_data = menu_string.generate_new_menu_string(provider_id)
-		elsif menu_string.version == 1 
+		elsif menu_string.version == 1
   			menu_string_data = menu_string.generate_menu_string(provider_id)
-		
+
 		else
 			menu_string_data = menu_string.data
 		end
-		return menu_string_data	
+		return menu_string_data
   	end
 
 			# remake menu string from menu
@@ -33,7 +33,7 @@ class MenuString < ActiveRecord::Base
 	end
 
 	def generate_menu_string(provider_id)
-		menu_string_data = Menu.get_full_menu_array(provider_id).to_json 
+		menu_string_data = Menu.get_full_menu_array(provider_id).to_json
 
 		self.data = menu_string_data
 		puts "IN GENERATE MENU STRING"
@@ -41,10 +41,10 @@ class MenuString < ActiveRecord::Base
 		sections_array 		= Menu.get_sections(provider_id)
 		self.sections_json 	= sections_array.to_json
 		self.provider_id 	= provider_id if !self.provider_id
-		
+
 		if self.save
 			puts "MENU STRING FOR #{provider_id} SAVED"
-		else 
+		else
 			puts "FAILED !! MENU STRING FOR #{provider_id} FAILED !! "
 		end
 		return menu_string_data
@@ -55,7 +55,7 @@ class MenuString < ActiveRecord::Base
 		if !menu_string
 			menu_string = MenuString.new
 			menu_string_data = menu_string.generate_new_menu_string(provider_id)
-		else 
+		else
   			menu_string_data = menu_string.generate_menu_string(provider_id)
   		end
 
@@ -63,7 +63,7 @@ class MenuString < ActiveRecord::Base
 	end
 
 	def method_name
-		
+
 	end
 end
 
