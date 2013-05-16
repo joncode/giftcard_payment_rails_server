a46622abb9aced67ab0b29f560d87bfb45abb5aa
Jon Code <programcodex@gmail.com>
2013-05-16 16:06:37 -0400

    --ammend

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -4,7 +4,7 @@ if Rails.env.production?
 	ENV["REDISTOGO_URL"] ||= "redis://redistogo:973cdc884e18482f5b324194e3b4cde1@barb.redistogo.com:9497/"
 	uri = URI.parse(ENV["REDISTOGO_URL"])
 	Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-if Rails.env.staging?
+elsif Rails.env.staging?
 		ENV["REDISTOGO_URL"] ||= "redis://redistogo:7a26911511c6ef1c1b2f32fad240ae0a@squawfish.redistogo.com:9819/"
 		uri = URI.parse(ENV["REDISTOGO_URL"])
 		Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
