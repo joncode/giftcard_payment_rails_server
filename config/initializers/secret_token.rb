3fda0e0bad2e2eb05cd1b9dae0bd7515727efcd4
Jon Code <programcodex@gmail.com>
2013-05-17 19:47:11 -0400

    adding the new routes for merchant tools prod/staging

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -7,11 +7,17 @@
 Drinkboard::Application.config.secret_token = '80cb854c626de01bc9fce3ffdbb7c83961ca54311520874da313b8bb98eeeb3e343843187d0b743604ff43963bb43d0f62f4e13265d3776a9cae2912eb1a2687'
 
 if Rails.env.production?
-    PAGE_NAME = 'Drinkboard - Admin Tools'
+    PAGE_NAME       = 'Drinkboard - Admin Tools'
+    TEST_URL        = "http://drinkboard.herokuapp.com"
+    MERCHANT_URL    = "http://merchtools.herokuapp.com"
 elsif Rails.env.staging?
-    PAGE_NAME = 'QA Drinkboard - Admin Tools (staging)'
+    PAGE_NAME       = 'QA Drinkboard - Admin Tools (staging)'
+    TEST_URL        = "http://dbappdev.herokuapp.com"
+    MERCHANT_URL    = "http://happy2dev.herokuapp.com"
 else
-    PAGE_NAME = 'Dev Drinkboard - Admin Tools (local)'
+    PAGE_NAME       = 'Dev Drinkboard - Admin Tools (local)'
+    TEST_URL        = "http://0.0.0.0:3000"
+    MERCHANT_URL    = "http://0.0.0.0:3000"
 end
 
 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -25,24 +31,6 @@ PROOF_LEVELS  		= ['zero', 'lite', 'normal', 'double']
 PIPE        = "%7C"
 SD_ROOT     = "https://www.subtledata.com/API/M/1/?Q="
 WEB_KEY     = "RlgrM1Uw"
-
-if Rails.env.production?
-	TEST_URL = "http://drinkboard.herokuapp.com"
-elsif Rails.env.staging?
-    TEST_URL = "http://dbappdev.herokuapp.com"
-else
-	TEST_URL = "http://0.0.0.0:3000"
-end
-
-if Rails.env.production?
-    MERCHANT_URL = "http://happy2.herokuapp.com"
-elsif Rails.env.staging?
-    MERCHANT_URL = "http://happy2dev.herokuapp.com"
-else
-    MERCHANT_URL = "http://0.0.0.0:3000"
-end
-
-
 
 BUTTONS = ["burger", "openlate", "bar", "club", "signature", "brunch", "steak", "martini", "wine", "beer", "cocktail", "dinig"]
 
