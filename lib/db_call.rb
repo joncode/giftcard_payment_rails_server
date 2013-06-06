6ae6162c069d9205106d38bd8ee72a6d36f0919a
Jon Code <programcodex@gmail.com>
2013-06-06 15:54:35 -0400

    db deactivate takes arrays or strings

--- lib/db_call.rb
+++ lib/db_call.rb
@@ -1,14 +1,41 @@
 module DbCall
 
-    def self.pattr(item_array, attribute)
+    def self.pattr(attribute, item_array)
         if item_array.count > 0
             item_array.map do |i|
-                puts "#{i.class.to_s} ID = #{i.id} | #{attribute.to_s} = #{i.send(attribute)}"
+                puts DbCall::string_attribute_obj(attribute, i)
             end
         else
             puts "No Items in array"
         end
         nil
+    end
+
+    def self.deactivate(attribute, data, obj)
+        if data.kind_of? Array
+            puts "Array received"
+            data.each do |item|
+                DbCall::deactivate_from_attr(attribute, item, obj)
+            end
+        else
+            puts "#{data.class.to_s} received"
+            DbCall::deactivate_from_attr(attribute, data, obj)
+        end
+        nil
+    end
+
+    def self.deactivate_from_attr(attribute, data, obj)
+        user = obj.class.where({attribute => data} ).first
+        puts DbCall::string_attribute_obj(attribute, user) + " | #{user.name}"
+        print "Deactivate #{user.name} ? -> (y/n) "
+        response = gets.chomp.downcase
+        if response == 'y'
+            user.update_attribute(:active, false)
+        end
+    end
+
+    def self.string_attribute_obj(attribute, obj)
+        "#{obj.class.to_s} ID = #{obj.id} | #{attribute.to_s} = #{obj.send(attribute)}"
     end
 
 
