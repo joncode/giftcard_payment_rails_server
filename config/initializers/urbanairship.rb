fe050f2940e1c493cc6c778daeee673308935640
Jon Code <programcodex@gmail.com>
2013-06-21 19:24:06 -0400

    config urbadairship

---
+++ config/initializers/urbanairship.rb
@@ -0,0 +1,5 @@
+Urbanairship.application_key    = 'q_NVI6G1RRaOU49kKTOZMQ'
+Urbanairship.application_secret = 'yQEhRtd1QcCgu5nXWj-2zA'
+Urbanairship.master_secret      = 'Lugw6dSXT6-e5mruDtO14g'
+Urbanairship.logger             = Rails.logger
+Urbanairship.request_timeout    = 5 # default
