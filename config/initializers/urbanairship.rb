2523f555f1965cab0eca62e2e33ed0b732ad6e74
Jon Code <programcodex@gmail.com>
2013-06-21 20:07:59 -0400

    production apsn keys for urbanairship

--- config/initializers/urbanairship.rb
+++ config/initializers/urbanairship.rb
@@ -1,5 +1,13 @@
-Urbanairship.application_key    = 'q_NVI6G1RRaOU49kKTOZMQ'
-Urbanairship.application_secret = 'yQEhRtd1QcCgu5nXWj-2zA'
-Urbanairship.master_secret      = 'Lugw6dSXT6-e5mruDtO14g'
-Urbanairship.logger             = Rails.logger
-Urbanairship.request_timeout    = 5 # default
+if Rails.env.production?
+    Urbanairship.application_key    = 'pgM5UjUvRaSGsxCQs2JkfA'
+    Urbanairship.application_secret = 'haOyK9IEScuguPw1z3N0Ew'
+    Urbanairship.master_secret      = 'nMU0qujYRTSGgzSMyz_H2A'
+    Urbanairship.logger             = Rails.logger
+    Urbanairship.request_timeout    = 5 # default
+else
+    Urbanairship.application_key    = 'q_NVI6G1RRaOU49kKTOZMQ'
+    Urbanairship.application_secret = 'yQEhRtd1QcCgu5nXWj-2zA'
+    Urbanairship.master_secret      = 'Lugw6dSXT6-e5mruDtO14g'
+    Urbanairship.logger             = Rails.logger
+    Urbanairship.request_timeout    = 5 # default
+end
