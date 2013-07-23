96917e85a9b6b66f3c965b37b689fbecb98ff673
Jon Code <programcodex@gmail.com>
2013-07-22 20:12:53 -0400

    adding the time to the log message framework

--- lib/common_utils.rb
+++ lib/common_utils.rb
@@ -9,12 +9,14 @@ module CommonUtils
 		x.delete('controller')
 		x.delete('action')
 		x.delete('format')
+		@start_time_logger = Time.now
 		puts "#{log_message_header} request: #{x}"
 	end
 
 	def method_end_log_message
-		print "END #{log_message_header} "
-		puts "response: #{@app_response[0..200]}" if @app_response
+		end_time = Time.now - @start_time_logger
+		print "END #{log_message_header} Total time = #{end_time.round(2)}"
+		puts "response: #{@app_response}" if @app_response
 	end
 
 end
