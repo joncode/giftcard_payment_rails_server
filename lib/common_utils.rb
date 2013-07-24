c0316918e481622b586ce0a61f8d93f48d0d0229
Jon Code <programcodex@gmail.com>
2013-07-24 11:51:50 -0400

    adding more ms to time in log msgs

--- lib/common_utils.rb
+++ lib/common_utils.rb
@@ -15,7 +15,7 @@ module CommonUtils
 
 	def method_end_log_message
 		end_time = Time.now - @start_time_logger
-		print "END #{log_message_header} Total time = #{end_time.round(2)}"
+		print "END #{log_message_header} Total time = #{end_time.round(3)}"
 		puts "response: #{@app_response}" if @app_response
 	end
 
