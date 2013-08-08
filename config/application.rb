431275366fd67538ea279d23919686b7b1ad1f2d
Jon Code <programcodex@gmail.com>
2013-08-08 16:34:52 -0400

    updating the log message lib

--- config/application.rb
+++ config/application.rb
@@ -37,9 +37,9 @@ module Drinkboard
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    
-    config.filter_parameters += [:password, :password_digest, :token, :uid, :credit_number, :card_number, :verification_value]
-    
+    FILTER_PARAMS = [:password, :password_confirmation, :token, :merchant_tkn, :merchant_token, :remember_token, :uid, :credit_number, :card_number, :verification_value]
+    config.filter_parameters += FILTER_PARAMS
+
     # this stops rails from connecting the database before aset precompile which heroku will not allow
     config.assets.initialize_on_precompile = false
 
--- lib/common_utils.rb
+++ lib/common_utils.rb
@@ -1,4 +1,8 @@
+
+	####  MUST have FILTER_PARAMS loaded into Rails already
+
 module CommonUtils
+	include ActionView::Helpers::TextHelper
 
 	def log_message_header
 		"#{params["controller"].upcase} -#{params["action"].upcase}-"
@@ -10,13 +14,31 @@ module CommonUtils
 		x.delete('action')
 		x.delete('format')
 		@start_time_logger = Time.now
-		puts "#{log_message_header} request: #{x}"
+		puts
+		puts "#{log_message_header} request: #{filter_params(x).inspect}"
 	end
 
 	def method_end_log_message
 		end_time = Time.now - @start_time_logger
-		print "END #{log_message_header} Total time = #{end_time.round(3)}"
-		puts "response: #{@app_response}" if @app_response
+		print "END #{log_message_header} Total time = #{end_time.round(3)}s | "
+		if @app_response
+			resp = "#{filter_params(@app_response).inspect}"
+			puts "response: #{truncate(resp ,length: 600)}"
+		end
+	end
+
+	def filter_params hsh
+		if hsh.kind_of? Hash
+			filter_text 	= "[FILTERED]"
+			filters 		= FILTER_PARAMS + FILTER_PARAMS.map {|fp| fp.to_s }
+
+			hsh.keys.each do |k|
+			    if filters.include? k
+			   		hsh[k] = filter_text
+			    end
+			end
+		end
+	    hsh
 	end
 
 end
