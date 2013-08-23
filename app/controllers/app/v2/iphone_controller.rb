4287f7f127b8e86766fb30dae721949688597aaf
Jon Code <programcodex@gmail.com>
2013-08-23 01:33:08 -0400

    --ammend

--- app/controllers/app/v2/iphone_controller.rb
+++ app/controllers/app/v2/iphone_controller.rb
@@ -8,11 +8,8 @@ module App
             def regifter
                 # get the gift from the gift ID
                 if old_gift = Gift.find(params["data"].to_i)
-                    # make a new gift with regifting code
                     new_gift = old_gift.regift
-                    # save the new gift
                     new_gift.save
-                    # change the status of the old gift
                     old_gift.update_attribute(:status, 'regifted')
                     success new_gift.serialize
                 else
