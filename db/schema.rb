2ba285ce11c30a23cd7964ce981786086232f986
Jon Code <programcodex@gmail.com>
2013-09-04 16:44:03 -0400

    adding detail to gift_items db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130827155825) do
+ActiveRecord::Schema.define(:version => 20130904204017) do
 
   create_table "admin_tokens", :force => true do |t|
     t.string   "token"
@@ -106,6 +106,7 @@ ActiveRecord::Schema.define(:version => 20130827155825) do
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.text     "detail"
   end
 
   add_index "gift_items", ["gift_id"], :name => "index_gift_items_on_gift_id"
