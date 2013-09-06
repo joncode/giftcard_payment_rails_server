565b0234162e5b5ec85d600cdae176a0203befb9
Jon Code <programcodex@gmail.com>
2013-09-06 18:26:17 -0400

    poking around orders MT route

--- app/controllers/mt/v1/merchant_tools_controller.rb
+++ app/controllers/mt/v1/merchant_tools_controller.rb
@@ -54,16 +54,17 @@ module Mt
             def orders
                 data  = params["data"]
                 if data["page"] == "new"
-                        gifts = Gift.get_provider(@provider)
-                    elsif data["page"]  == 'reports'
+                    gifts = Gift.get_provider(@provider)
+                elsif data["page"]  == 'reports'
 
-                        start_time = data["start_time"].to_datetime if data["start_time"]
-                        end_time   = data["end_time"].to_datetime   if data["end_time"]
-                        puts "hitting the date correct #{start_time}|#{end_time}"
-                        gifts = Gift.get_history_provider_and_range(@provider, start_time, end_time )
-                    else
-                        gifts = Gift.get_history_provider(@provider)
-                    end
+                    start_time = data["start_time"].to_datetime if data["start_time"]
+                    end_time   = data["end_time"].to_datetime   if data["end_time"]
+                    puts "hitting the date correct #{start_time}|#{end_time}"
+                    gifts = Gift.get_history_provider_and_range(@provider, start_time, end_time )
+                else
+                    puts "hitting get_history_provider"
+                    gifts = Gift.get_history_provider(@provider)
+                end
 
                 if gifts
                     success array_these_gifts(gifts, MERCHANT_REPLY, false, true, true)
