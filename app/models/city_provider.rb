b877a835afada966a9d13bb4fa6f314dcc6239d5
iumehara <iumehara10@gmail.com>
2013-09-20 19:43:27 -0400

    created city_provider model

---
+++ app/models/city_provider.rb
@@ -0,0 +1,3 @@
+class CityProvider < ActiveRecord::Base
+  attr_accessible :city, :provider_id, :providers_array
+end
---
+++ db/migrate/20130920231649_create_city_providers.rb
@@ -0,0 +1,11 @@
+class CreateCityProviders < ActiveRecord::Migration
+  def change
+    create_table :city_providers do |t|
+      t.integer :provider_id
+      t.string :city
+      t.text :providers_array
+
+      t.timestamps
+    end
+  end
+end
