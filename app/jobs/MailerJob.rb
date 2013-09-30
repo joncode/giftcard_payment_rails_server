9a1dbaae3df4f2e97d331b2e7c7a282882549695
Jon Code <programcodex@gmail.com>
2013-09-30 17:42:03 -0400

    putting the emailers on a resque worker

--- app/jobs/MailerJob.rb
+++ app/jobs/MailerJob.rb
@@ -1,10 +1,22 @@
 class MailerJob
+    extend Emailer
     @queue = :email
 
-    def self.perform(type_of, message)
-        puts "this is email #{type_of}"
-        puts "here is message #{message}"
+    def self.perform(data)
+        begin
+            if not Rails.env.test?
+                self.call_mandrill(data)
+            end
+        rescue
+            puts "No #{data['text']} email ERROR"
+        end
     end
-    
+
+
+private
+
+    def self.call_mandrill data
+        MailerJob.send(data['text'], data)
+    end
 
 end
