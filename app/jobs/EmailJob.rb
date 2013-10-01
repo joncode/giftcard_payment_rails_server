dfa2480f0d9861856edf10076478b65e82e4744f
Jon Code <programcodex@gmail.com>
2013-10-01 00:18:42 -0400

    commenting out old emailer

--- app/jobs/EmailJob.rb
+++ app/jobs/EmailJob.rb
@@ -1,79 +1,79 @@
 class EmailJob
-	@queue = :email
+	# @queue = :email
 
-	def self.perform(email_type, user_id, opthash = {})
-		case email_type
-		when "confirm_email"
-			puts "OPT CONFIRM EMAIL"
-			@user = User.find(user_id.to_i)
-			UserMailer.confirm_email(@user).deliver
+	# def self.perform(email_type, user_id, opthash = {})
+	# 	case email_type
+	# 	when "confirm_email"
+	# 		puts "OPT CONFIRM EMAIL"
+	# 		@user = User.find(user_id.to_i)
+	# 		UserMailer.confirm_email(@user).deliver
 
-		when "reset_password"
-			puts "OPT RESETPASSWORD"
-			@user = User.find(user_id.to_i)
-			UserMailer.reset_password(@user).deliver
+	# 	when "reset_password"
+	# 		puts "OPT RESETPASSWORD"
+	# 		@user = User.find(user_id.to_i)
+	# 		UserMailer.reset_password(@user).deliver
 
-		# when "invite_friend"
-		# 	@user = User.find(user_id.to_i)
-		# 	puts "OPTHASH INVITE FRIEND"
-		# 	puts opthash
-		# 	puts opthash["email"]
-		# 	UserMailer.invite_friend(@user, opthash["email"], opthash["gift_id"]).deliver
+	# 	# when "invite_friend"
+	# 	# 	@user = User.find(user_id.to_i)
+	# 	# 	puts "OPTHASH INVITE FRIEND"
+	# 	# 	puts opthash
+	# 	# 	puts opthash["email"]
+	# 	# 	UserMailer.invite_friend(@user, opthash["email"], opthash["gift_id"]).deliver
 
-		when "invite_employee"
-			puts "OPTHASH INVITE EMPLOYEE"
-			puts "Invite Employee #{opthash}"
-			if user_id.to_i > 0
-				@user = User.find(user_id.to_i)    #Person making the request
-			else
-				@user = User.new(first_name: user_id)    # user_id from merchant tools is the name
-			end
-			@provider = Provider.find(opthash["provider_id"].to_i)
-			UserMailer.invite_employee(@user, @provider, opthash["email"], opthash["route"]).deliver
+	# 	when "invite_employee"
+	# 		puts "OPTHASH INVITE EMPLOYEE"
+	# 		puts "Invite Employee #{opthash}"
+	# 		if user_id.to_i > 0
+	# 			@user = User.find(user_id.to_i)    #Person making the request
+	# 		else
+	# 			@user = User.new(first_name: user_id)    # user_id from merchant tools is the name
+	# 		end
+	# 		@provider = Provider.find(opthash["provider_id"].to_i)
+	# 		UserMailer.invite_employee(@user, @provider, opthash["email"], opthash["route"]).deliver
 
-		when "invoice_giver"
-			puts "OPTHASH INVOICE GIVER"
-			@user = User.find(user_id)
-			if @user.get_or_create_settings.email_invoice
-				@gift = Gift.find(opthash["gift_id"].to_i)
-				UserMailer.invoice_giver(@user, @gift).deliver
-			else
-				puts "#{@user.fullname} does not receiver Invoice Giver Email"
-			end
+	# 	when "invoice_giver"
+	# 		puts "OPTHASH INVOICE GIVER"
+	# 		@user = User.find(user_id)
+	# 		if @user.get_or_create_settings.email_invoice
+	# 			@gift = Gift.find(opthash["gift_id"].to_i)
+	# 			UserMailer.invoice_giver(@user, @gift).deliver
+	# 		else
+	# 			puts "#{@user.fullname} does not receiver Invoice Giver Email"
+	# 		end
 
-		when "notify_receiver"
-			puts "OPTHASH NOTIFY RECEIVER"
-			email = opthash["email"]
-			@gift = Gift.find(opthash["gift_id"].to_i)
-			send  = false
-			if @gift.receiver_id.to_i > 0
-				receiver = @gift.receiver
-				send     = receiver.get_or_create_settings.email_receiver_new
-			elsif !@gift.receiver_email.nil?
-				send     = true
-			end
-			if send
-				UserMailer.notify_receiver(@gift).deliver
-			else
-				puts "Receiver is origin (fb,tw,txt) or no notify receiver via email giftID = #{@gift.id.to_s}"
-			end
+	# 	when "notify_receiver"
+	# 		puts "OPTHASH NOTIFY RECEIVER"
+	# 		email = opthash["email"]
+	# 		@gift = Gift.find(opthash["gift_id"].to_i)
+	# 		send  = false
+	# 		if @gift.receiver_id.to_i > 0
+	# 			receiver = @gift.receiver
+	# 			send     = receiver.get_or_create_settings.email_receiver_new
+	# 		elsif !@gift.receiver_email.nil?
+	# 			send     = true
+	# 		end
+	# 		if send
+	# 			UserMailer.notify_receiver(@gift).deliver
+	# 		else
+	# 			puts "Receiver is origin (fb,tw,txt) or no notify receiver via email giftID = #{@gift.id.to_s}"
+	# 		end
 
-		when "notify_giver_order_complete"
-			puts "OPTHASH NOTIFY GIVER ORDER COMPLETE"
-			@user = User.find(user_id.to_i)
-			if @user.get_or_create_settings.email_redeem
-				@gift = Gift.find(opthash["gift_id"].to_i)
-				UserMailer.notify_giver_order_complete(@user, @gift).deliver
-			else
-				puts "#{@user.fullname} does not receiver Invoice Giver Email"
-			end
+	# 	when "notify_giver_order_complete"
+	# 		puts "OPTHASH NOTIFY GIVER ORDER COMPLETE"
+	# 		@user = User.find(user_id.to_i)
+	# 		if @user.get_or_create_settings.email_redeem
+	# 			@gift = Gift.find(opthash["gift_id"].to_i)
+	# 			UserMailer.notify_giver_order_complete(@user, @gift).deliver
+	# 		else
+	# 			puts "#{@user.fullname} does not receiver Invoice Giver Email"
+	# 		end
 
-		when "notify_giver_created_user"
-			puts "OPTHASH NOTIFY GIVER CREATED USER"
-			@user = User.find(user_id.to_i)
-			@gift = Gift.find(opthash["gift_id"].to_i)
-			UserMailer.notify_giver_created_user(@user, @gift).deliver
-		end
-	end
+	# 	when "notify_giver_created_user"
+	# 		puts "OPTHASH NOTIFY GIVER CREATED USER"
+	# 		@user = User.find(user_id.to_i)
+	# 		@gift = Gift.find(opthash["gift_id"].to_i)
+	# 		UserMailer.notify_giver_created_user(@user, @gift).deliver
+	# 	end
+	# end
 
 end
