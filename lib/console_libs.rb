c8fd0c02c542f5cf3c46180375839b19677e353b
Jon Code <programcodex@gmail.com>
2013-10-03 19:31:43 -0400

    adding legacy gift pay stat method

--- lib/console_libs.rb
+++ lib/console_libs.rb
@@ -6,10 +6,12 @@ require 'qa_team'
 require 'locate'
 require 'legacy'
 require 'legacy_user'
+require 'legacy_gift'
 require 'cron'
 require 'gift_console'
 include Legacy
 include LegacyUser
+include LegacyGift
 include Cron
 include Dbcall
 include GiftConsole
