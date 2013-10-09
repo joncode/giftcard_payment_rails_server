36733a9188daccf031aba27130cffff10c5da5fe
Jon Code <programcodex@gmail.com>
2013-10-08 20:51:10 -0400

    removing db logs

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -20,7 +20,9 @@ def lcon
     puts "loading rails console scripts [lcon]"
 end
 
-ActiveRecord::Base.logger = Logger.new(STDOUT)
+if Rails.env.production?
+    ActiveRecord::Base.logger = Logger.new(STDOUT)
+end
 if Rails.env.test?
     ActiveRecord::Base.logger.level = 1
     require 'auth_response'
