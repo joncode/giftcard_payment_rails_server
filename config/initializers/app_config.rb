d75a3a1f39a4e90b7ba6417549fea95fcf68d540
Jon Code <programcodex@gmail.com>
2013-10-08 22:06:59 -0400

    --ammend

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -23,6 +23,7 @@ end
 if Rails.env.production?
     ActiveRecord::Base.logger = Logger.new(STDOUT)
 end
+    
 if Rails.env.test?
     ActiveRecord::Base.logger.level = 1
     require 'auth_response'
