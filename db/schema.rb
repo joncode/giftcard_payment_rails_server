d6b2c3021f3d54b0a45a8087ff5358c0afef1505
iumehara <iumehara10@gmail.com>
2013-10-11 12:41:17 -0400

    migrated db with new data/columns

--- db/schema.rb
+++ db/schema.rb
@@ -86,6 +86,16 @@ ActiveRecord::Schema.define(:version => 20131002213728) do
     t.datetime "updated_at",      :null => false
   end
 
+  create_table "connections", :force => true do |t|
+    t.integer  "giver_id"
+    t.integer  "receiver_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  add_index "connections", ["giver_id"], :name => "index_connections_on_giver_id"
+  add_index "connections", ["receiver_id"], :name => "index_connections_on_receiver_id"
+
   create_table "credit_accounts", :force => true do |t|
     t.string   "owner"
     t.integer  "owner_id"
