a418d0536502bcb19b4923327ce8137205da39ac
Jon Code <programcodex@gmail.com>
2013-10-14 14:52:52 -0400

    adding down for maintenance for db change

--- app/controllers/json_controller.rb
+++ app/controllers/json_controller.rb
@@ -4,6 +4,7 @@ class JsonController < ActionController::Base
     include JsonHelper
 
 	skip_before_filter   :verify_authenticity_token
+    before_filter        :down_for_maintenance
     before_filter        :log_request_header
     before_filter        :method_start_log_message
     after_filter         :cross_origin_allow_header
@@ -171,6 +172,11 @@ private
     def cross_origin_allow_header
         headers['Access-Control-Allow-Origin']   = "*"
         headers['Access-Control-Request-Method'] = '*'
+    end
+
+    def down_for_maintenance
+        @app_response = { "error" => "Server is down for maintenance.  Thank you for your patience. Be back shortly" }
+        respond
     end
 
 
