4f20dfb1f12aa21f25863a2540c626d68a996fa1
Jon Code <programcodex@gmail.com>
2013-10-21 14:00:04 -0400

    undoing moving provider create to merchant tools

--- app/controllers/mt/v2/merchants_controller.rb
+++ app/controllers/mt/v2/merchants_controller.rb
@@ -1,6 +1,17 @@
 class Mt::V2::MerchantsController < JsonController
-    before_filter :authenticate_merchant_tools, except: [:reconcile_merchants]
-    before_filter :authenticate_general_token,  only:   [:reconcile_merchants]
+    before_filter :authenticate_merchant_tools, except: [:create, :reconcile_merchants]
+    before_filter :authenticate_general_token,  only:   [:create, :reconcile_merchants]
+
+    def create
+        merchant_hsh = params["data"]
+        provider = Provider.new merchant_hsh
+        if provider.save
+            success provider.id
+        else
+            fail    provider.errors.messages
+        end
+        respond
+    end
 
     def update
         provider_hsh = params["data"]
--- spec/controllers/mt/v2/merchants_controller_spec.rb
+++ spec/controllers/mt/v2/merchants_controller_spec.rb
@@ -1,7 +1,70 @@
 require 'spec_helper'
 
 describe Mt::V2::MerchantsController do
-    
+
+    before(:each) do
+        request.env["HTTP_TKN"] = ENV["GENERAL_TOKEN"]
+
+        Provider.delete_all
+        #@provider = FactoryGirl.create(:provider)
+    end
+
+    describe :create do
+
+        context "authorization" do
+
+            it "should not allow unauthenticated access" do
+                request.env["HTTP_TKN"] = "No_Entrance"
+                put :create, id: 1, format: :json
+                response.response_code.should == 401
+            end
+
+        end
+
+        it "should create new merchant" do
+            new_provider_hsh = {"name"=>"Yonaka Modern Japanese", "zinger"=>"A Perfect Bite To Inspire Conversation", "description"=>"We offer a Japanese Tapas style dining with a unique experience through Modern Japanese Cuisine. Yonaka provides a fresh and relaxing atmosphere with highly attentive and informative staff. ", "address"=>"4983 W Flamingo Road, Suite A", "city"=>"Las Vegas", "state"=>"NV", "zip"=>"89103", "phone"=>"7026858358", "merchant_id"=>34, "token"=>"_96WweqJfzLEZNbrtVREiw", "image"=>"blank_photo_profile.png", "mode"=>"coming_soon"}
+            post :create, format: :json, data: new_provider_hsh
+            provider = Provider.last
+            provider.name.should        == new_provider_hsh["name"]
+            provider.description.should == new_provider_hsh["description"]
+            provider.zinger.should      == new_provider_hsh["zinger"]
+            provider.city.should        == new_provider_hsh["city"]
+            provider.state.should       == new_provider_hsh["state"]
+            provider.zip.should         == new_provider_hsh["zip"]
+            provider.address.should     == new_provider_hsh["address"]
+            provider.phone.should       == new_provider_hsh["phone"]
+            provider.token.should       == new_provider_hsh["token"]
+            provider.merchant_id.should == new_provider_hsh["merchant_id"]
+            provider.mode.should        == new_provider_hsh["mode"]
+            provider.get_photo.should   == new_provider_hsh["image"]
+            provider.active.should be_true
+        end
+
+
+        it "should create new menu_string" do
+            new_provider_hsh = {"name"=>"Yonaka Modern Japanese", "zinger"=>"A Perfect Bite To Inspire Conversation", "description"=>"We offer a Japanese Tapas style dining with a unique experience through Modern Japanese Cuisine. Yonaka provides a fresh and relaxing atmosphere with highly attentive and informative staff. ", "address"=>"4983 W Flamingo Road, Suite A", "city"=>"Las Vegas", "state"=>"NV", "zip"=>"89103", "phone"=>"7026858358", "merchant_id"=>34, "token"=>"_96WweqJfzLEZNbrtVREiw", "image"=>"blank_photo_profile.png", "mode"=>"coming_soon"}
+            new_provider_hsh['menu'] = "[{\"section\":\"Gift Vouchers\",\"items\":[{\"detail\":\"The entire gift amount must be used at one time.\\n\\t    Unused portions of this gift cannot be saved, transferred, or redeemed for cash.\",\"price\":\"10\",\"item_id\":154,\"item_name\":\"$10\"},{\"detail\":\"The entire gift amount must be used at one time.\\n\\t    Unused portions of this gift cannot be saved, transferred, or redeemed for cash.\",\"price\":\"25\",\"item_id\":155,\"item_name\":\"$25\"},{\"detail\":\"The entire gift amount must be used at one time.\\n\\t    Unused portions of this gift cannot be saved, transferred, or redeemed for cash.\",\"price\":\"50\",\"item_id\":156,\"item_name\":\"$50\"}]}]"
+            post :create, format: :json, data: new_provider_hsh
+            provider                 = Provider.last
+            menu_string              = MenuString.last
+            response.response_code.should   == 200
+            menu_string.provider_id.should  == provider.id
+            menu_string.menu.should_not     be_nil
+            menu_string.menu.should         == new_provider_hsh["menu"]
+            json["status"].should == 1
+            json["data"].should   == provider.id
+        end
+
+        it "should save latitude and longitude" do
+            new_provider_hsh = {"name"=>"Yonaka Modern Japanese", "zinger"=>"A Perfect Bite To Inspire Conversation", "description"=>"We offer a Japanese Tapas style dining with a unique experience through Modern Japanese Cuisine. Yonaka provides a fresh and relaxing atmosphere with highly attentive and informative staff. ", "address"=>"4983 W Flamingo Road, Suite A", "city"=>"Las Vegas", "state"=>"NV", "zip"=>"89103", "phone"=>"7026858358", "merchant_id"=>34, "token"=>"_96WweqJfzLEZNbrtVREiw", "image"=>"blank_photo_profile.png", "mode"=>"coming_soon"}
+            post :create, format: :json, data: new_provider_hsh
+            provider = Provider.last
+            provider.latitude.should    == new_provider_hsh["latitude"]
+            provider.longitude.should   == new_provider_hsh["longitute"]
+        end
+
+    end
+
     describe :update do
 
         before(:each) do
