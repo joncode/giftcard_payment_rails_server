7dbd70d2a76b8488cb9eb612abf53efa04c81de0
Jon Code <programcodex@gmail.com>
2013-11-27 17:14:23 -0500

    up and down for the sale payable migration

--- db/migrate/20131127072616_move_sales_to_payables_on_gift.rb
+++ db/migrate/20131127072616_move_sales_to_payables_on_gift.rb
@@ -1,21 +1,33 @@
 class MoveSalesToPayablesOnGift < ActiveRecord::Migration
-  def change
 
-    puts "-------------------------  if Gift.rb total is set to value \n"
-    puts " this WILL NOT WORK @!!!!!!!!!!!!!!!!!   comment out Gift.any_instance.#total"
+    def up
+        puts "-------------------------  if Gift.rb total is set to value \n"
+        puts " this WILL NOT WORK @!!!!!!!!!!!!!!!!!   comment out Gift.any_instance.#total"
 
-    gs = Gift.unscoped
-    gs.each do |gift|
-        sale = gift.sale
-        if sale
-            gift.payable = sale
-            user_id = gift.giver_id
-            user = User.find(user_id)
-            gift.giver = user
-            gift.value = gift.total
-            gift.save
+        gs = Gift.unscoped
+        gs.each do |gift|
+            sale = gift.sale
+            if sale
+                gift.payable = sale
+                user_id = gift.giver_id
+                user = User.find(user_id)
+                gift.giver = user
+                gift.value = gift.total
+                gift.save
+            end
         end
+
     end
 
-  end
+    def down
+        gs = Gift.unscoped
+        gs.each do |gift|
+            sale = gift.payable
+            if sale
+                gift.sale = sale
+                gift.total = gift.value
+                gift.save
+            end
+        end
+    end
 end
