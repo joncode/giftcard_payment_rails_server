a7f71ced75a77c8ccd7ce25e9317e2e1e3831d80
Jon Code <programcodex@gmail.com>
2013-11-27 11:20:43 -0500

    --ammend

--- spec/models/gift_serializers_spec.rb
+++ spec/models/gift_serializers_spec.rb
@@ -8,7 +8,7 @@ describe GiftSerializers do
     @user     = FactoryGirl.create(:user)
     @regifter = FactoryGirl.create(:user, first_name: "Jon", last_name: "Regifter")
     @receiver = FactoryGirl.create(:user, first_name: "Sarah", last_name: "Receiver")
-    @old_gift  = FactoryGirl.create(:gift, giver: @user, receiver: @regifter, message: "DO NOT REGIFT!", value: "201.00", service: '10.05')
+    @old_gift = FactoryGirl.create(:gift, giver: @user, receiver: @regifter, message: "DO NOT REGIFT!", value: "201.00", service: '10.05')
 
     @gift_hsh = {}
     @gift_hsh["message"]        = "I just REGIFTED!"
@@ -25,43 +25,43 @@ describe GiftSerializers do
     [@old_gift, regift].each do |gift|
 
         it "should serialize" do
-            keys = [ "gift_id", "giver","giver_photo" ,"receiver" ,"receiver_photo","receiver" ,"message","shoppingCart" ,"merchant_name" ,"merchant_address" ,"merchant_phone" ]
-            keys.delete("receiver_photo")
-            hsh = gift.serialize
+            keys = [ "gift_id", "giver","giver_photo" ,"receiver" ,"message","shoppingCart" ,"merchant_name" ,"merchant_address" ,"merchant_phone" ]
+            keys << "receiver_photo" if gift.receiver
+            hsh  = gift.serialize
             compare_keys hsh, keys
         end
 
         it "should badge_serialize" do
             keys = ["giver_id", "giver_name", "provider_id", "provider_name", "message", "updated_at", "created_at", "status", "shoppingCart", "giver_photo", "provider_photo", "provider_phone", "city", "latitude", "longitude", "live", "provider_address", "gift_id"]
-            hsh = gift.badge_serialize
+            hsh  = gift.badge_serialize
             compare_keys hsh, keys
 
         end
 
         it "should giver_serialize" do
             keys = ["created_at", "message", "provider_id", "provider_name", "receiver_id", "receiver_name", "total", "updated_at", "shoppingCart", "gift_id", "status", "receiver_photo", "provider_photo", "provider_phone", "city", "latitude", "longitude", "live", "provider_address"]
-            hsh = gift.giver_serialize
+            hsh  = gift.giver_serialize
             compare_keys hsh, keys
 
         end
 
         it "should receiver_serialize" do
             keys = ["giver_id", "giver_name", "message", "provider_id", "provider_name", "shoppingCart", "updated_at", "created_at", "gift_id", "status", "giver_photo", "provider_photo", "provider_phone", "city", "latitude", "longitude", "live", "provider_address"]
-            hsh = gift.receiver_serialize
+            hsh  = gift.receiver_serialize
             compare_keys hsh, keys
 
         end
 
         it "should admt_serialize" do
             keys = ["gift_id", "provider_id", "name", "merchant_address", "total", "updated_at", "pay_type"]
-            hsh = gift.admt_serialize
+            hsh  = gift.admt_serialize
             compare_keys hsh, keys
 
         end
 
         it "should report_serialize" do
             keys = ["order_num","updated_at","created_at","receiver_name","items","server","total"]
-            hsh = gift.report_serialize
+            hsh  = gift.report_serialize
             compare_keys hsh, keys
 
         end
