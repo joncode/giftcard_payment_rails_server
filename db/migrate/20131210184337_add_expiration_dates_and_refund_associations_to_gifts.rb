128a3a240d7707bd2bd0965676b833cb8f57ec99
Jon Code <programcodex@gmail.com>
2013-12-10 22:57:38 -0500

    --amend

--- db/migrate/20131210184337_add_expiration_dates_and_refund_associations_to_gifts.rb
+++ db/migrate/20131210184337_add_expiration_dates_and_refund_associations_to_gifts.rb
@@ -1,12 +1,12 @@
 class AddExpirationDatesAndRefundAssociationsToGifts < ActiveRecord::Migration
   def up
-    # add_column :gifts, :expires_at, :datetime
+    add_column :gifts, :expires_at, :datetime
     add_column :gifts, :refund_id,  :integer
     add_column :gifts, :refund_type, :string
   end
 
   def down
-    # remove_column :gifts, :expires_at
+    remove_column :gifts, :expires_at
     remove_column :gifts, :refund_id
     remove_column :gifts, :refund_type
   end
