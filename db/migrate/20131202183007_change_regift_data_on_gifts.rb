207f2288aabed5d21921b1f65ded112f0fe100b7
Jon Code <programcodex@gmail.com>
2013-12-10 23:09:31 -0500

    --ammend

--- db/migrate/20131202183007_change_regift_data_on_gifts.rb
+++ db/migrate/20131202183007_change_regift_data_on_gifts.rb
@@ -1,12 +1,12 @@
 class ChangeRegiftDataOnGifts < ActiveRecord::Migration
     def up
-        gs = Gift.unscoped
-        regifts = gs.select{|g| !g.regift_id.nil? }
-        regifts.each do |gift|
-            parent = gift.parent
-            gift.payable = parent
-            gift.save
-        end
+        # gs = Gift.unscoped
+        # regifts = gs.select{|g| !g.regift_id.nil? }
+        # regifts.each do |gift|
+        #     parent = gift.parent
+        #     gift.payable = parent
+        #     gift.save
+        # end
     end
 
     def down
