a325e2b1ddd105e496b74386dd3ee69ec3833b3e
Jon Code <programcodex@gmail.com>
2013-12-10 22:52:24 -0500

    going to up/down instead of change in migration

--- db/migrate/20131210184337_add_expiration_dates_and_refund_associations_to_gifts.rb
+++ db/migrate/20131210184337_add_expiration_dates_and_refund_associations_to_gifts.rb
@@ -1,7 +1,13 @@
 class AddExpirationDatesAndRefundAssociationsToGifts < ActiveRecord::Migration
-  def change
+  def up
     add_column :gifts, :expires_at, :datetime
-    add_column :gifts, :refund_id, :integer
+    add_column :gifts, :refund_id,  :integer
     add_column :gifts, :refund_type, :string
+  end
+
+  def down
+    remove_column :gifts, :expires_at
+    remove_column :gifts, :refund_id
+    remove_column :gifts, :refund_type
   end
 end
