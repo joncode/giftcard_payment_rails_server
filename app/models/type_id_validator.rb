ffe110f52ebf30a82950cf49327fe274b887e271
iumehara <iumehara10@gmail.com>
2013-12-13 18:28:44 -0500

    fixes empty db error in id validator

--- app/models/type_id_validator.rb
+++ app/models/type_id_validator.rb
@@ -2,9 +2,11 @@ class TypeIdValidator < ActiveModel::Validator
 
 	def validate(record)
 		record_type = record.type_of
-		UserSocial.where(active:true).to_a.each do |us|
-			if us.type_of == record_type && us.identifier == record.identifier 
-				return record.errors[record_type.to_sym] << "is already in use. Please email support@itson.me for assistance if this is in error"
+		unless UserSocial.count == 1
+			UserSocial.where(active:true).to_a.each do |us|
+				if us.type_of == record_type && us.identifier == record.identifier 
+					return record.errors[record_type.to_sym] << "is already in use. Please email support@itson.me for assistance if this is in error"
+				end
 			end
 		end
 	end
