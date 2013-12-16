8b418d4d7aa38b656ff9261b89dee9326c74d1d1
Jon Code <programcodex@gmail.com>
2013-12-16 14:48:57 -0600

    removing the extra code for photo_url transfers

--- app/uploaders/provider_box_uploader.rb
+++ app/uploaders/provider_box_uploader.rb
@@ -1,13 +1,13 @@
-class ProviderBoxUploader < CarrierWave::Uploader::Base
-  include Cloudinary::CarrierWave
+# class ProviderBoxUploader < CarrierWave::Uploader::Base
+#   include Cloudinary::CarrierWave
 
-  process :convert => 'jpg'
-  process :initial_crop
-    
-  def initial_crop
-    return { :transformation => 
-      [{:width => 350, :height => 500, :crop => :limit}, 
-      {:x => model.crop_x, :y => model.crop_y, :width => model.crop_w, :height => model.crop_h, :crop => :crop}]
-    }
-  end
-end
+#   process :convert => 'jpg'
+#   process :initial_crop
+
+#   def initial_crop
+#     return { :transformation =>
+#       [{:width => 350, :height => 500, :crop => :limit},
+#       {:x => model.crop_x, :y => model.crop_y, :width => model.crop_w, :height => model.crop_h, :crop => :crop}]
+#     }
+#   end
+# end
