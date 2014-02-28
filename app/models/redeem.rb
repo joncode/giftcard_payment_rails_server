d3e974a7865fb88c49ce4aecb7a68035e51966e7
Jon Code <programcodex@gmail.com>
2014-02-28 13:26:18 -0500

    formatting

--- app/models/redeem.rb
+++ app/models/redeem.rb
@@ -1,38 +1,38 @@
 class Redeem < ActiveRecord::Base
 
-  belongs_to      :gift
-  has_one         :giver,     :through => :gift
-  has_one         :receiver,  :through => :gift
-  has_one         :provider,  :through => :gift
-  has_one         :order
+	belongs_to      :gift
+	has_one         :giver,     :through => :gift
+	has_one         :receiver,  :through => :gift
+	has_one         :provider,  :through => :gift
+	has_one         :order
 
-  before_create :create_redeem_code
-  after_create  :add_redeem_to_gift
+	before_create :create_redeem_code
+	after_create  :add_redeem_to_gift
 
-  validates :gift_id , presence: true, uniqueness: true
-  # redeem must be unique for gift
+	validates :gift_id , presence: true, uniqueness: true
+	# redeem must be unique for gift
 
-  def self.find_or_create_with_gift(gift)
-    unless redeem = Redeem.find_by(gift_id: gift.id)
-        # redeem must be created
-      redeem = Redeem.init_with_gift(gift)
-    end
-    return redeem
-  end
+	def self.find_or_create_with_gift(gift)
+		unless redeem = Redeem.find_by(gift_id: gift.id)
+				# redeem must be created
+			redeem = Redeem.init_with_gift(gift)
+		end
+		return redeem
+	end
 
-  def self.init_with_gift(gift)
-    Redeem.create(gift_id: gift.id)
-  end
+	def self.init_with_gift(gift)
+		Redeem.create(gift_id: gift.id)
+	end
+	
+private
 
-  private
+	def create_redeem_code
+		self.redeem_code = "%04d" % rand(10000)
+	end
 
-    def create_redeem_code
-      self.redeem_code = "%04d" % rand(10000)
-    end
-
-    def add_redeem_to_gift
-      self.gift.update(status: 'notified')
-    end
+	def add_redeem_to_gift
+		self.gift.update(status: 'notified')
+	end
 end
 # == Schema Information
 #
