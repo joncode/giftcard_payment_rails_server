2352239bfb554b8b5bc7c622b5eabb074800afa5
Jon Code <programcodex@gmail.com>
2014-03-16 17:17:53 -0700

    --ammend

--- app/jobs/CreateGiftNotifySocialJob.rb
+++ app/jobs/CreateGiftNotifySocialJob.rb
@@ -4,10 +4,11 @@ class CreateGiftNotifySocial
 
     def self.perform gift_id
         puts " -------------   Notify Social Create Gift --------------------"
-        if gift = self.gift
+        if gift = Gift.find gift_id
+            oauth = gift.oauth
             cart     = JSON.parse gift.shoppingCart
             post_hsh = { "merchant"  => gift.provider_name, "title" => cart[0]["item_name"], "url" => "#{PUBLIC_URL}/signup/acceptgift/#{gift.obscured_id}" }
-            social_proxy = SocialProxy.new(self.to_proxy)
+            social_proxy = SocialProxy.new(oauth.to_proxy)
             social_proxy.create_post(post_hsh)
         end
     end
