f33ea914eb8aebac0e77ab0c8d75726d9b3bcf38
Jon Code <programcodex@gmail.com>
2014-03-18 15:03:34 -0700

    adding logs to sms collector

--- lib/sms_collector.rb
+++ lib/sms_collector.rb
@@ -9,15 +9,15 @@ module SmsCollector
 				sms_obj = Slicktext.new(word_hsh)
 				sms_obj.sms
 				contacts = sms_obj.contacts
-				puts "total contacts = #{contacts.count}"
+				puts "#{textword} - total contacts = #{contacts.count}"
 
 				if contacts.kind_of?(Array) && contacts.first.kind_of?(Hash)
 					SmsContact.bulk_create(contacts)
 				end
 				sms_contacts  = SmsContact.where(gift_id: nil, textword: textword.to_s)
-				puts "sms contacts from db to gift = #{sms_contacts.count}"
+				puts "#{textword} - sms contacts from db to gift = #{sms_contacts.count}"
 				sms_contacts.each do |sms_contact|
-					puts "creating a gift for #{sms_contact.inspect}"
+					puts "#{textword} - creating a gift for #{sms_contact.inspect}"
 					gift = self.create_gift(campaign_item, sms_contact)
 				end
 			else
@@ -49,3 +49,29 @@ private
 
 
 end
+
+# process
+
+# call ST-api for textwords
+# loop textwords
+	# call db for campaigns connect to textword
+	# call ST-api for contacts for that textword
+	# receive ALL contacts
+	# dump all contacts into the db whether they are there or not
+	# new contacts are sorted by validation textword + phone
+	# goes back into database and gets contacts that dont have gift associations - even tho thats the return value of bulk_create
+	# loop sms_contacts
+		# run GiftCampaign.create over contacts
+	# print campaign status_text to log
+
+# CONS
+# going to ST-api once per textword + once for textwords - 5-10 lookups
+# saving contact records that are already in database is a lot of noise
+# going into database to get the contacts when they are already return value of bulk_create
+
+
+
+
+
+
+
