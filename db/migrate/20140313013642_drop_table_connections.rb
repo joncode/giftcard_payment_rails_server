b1cad1f42fab3f49896f89a98a59811a197e2efa
Jon Code <programcodex@gmail.com>
2014-03-20 16:21:16 -0400

    removing droptable connectiuons

--- db/migrate/20140313013642_drop_table_connections.rb
+++ db/migrate/20140313013642_drop_table_connections.rb
@@ -1,18 +1,18 @@
 class DropTableConnections < ActiveRecord::Migration
 
   def up
-        drop_table :connections
+        # drop_table :connections
   end
 
   def down
-        create_table :connections do |t|
-            t.integer :friend_id
-            t.integer :contact_id
-            t.timestamps
-        end
+        # create_table :connections do |t|
+        #     t.integer :friend_id
+        #     t.integer :contact_id
+        #     t.timestamps
+        # end
 
-        add_index :connections, :friend_id
-        add_index :connections, :contact_id
-        add_index :connections, [:friend_id, :contact_id], unique: true
+        # add_index :connections, :friend_id
+        # add_index :connections, :contact_id
+        # add_index :connections, [:friend_id, :contact_id], unique: true
     end
 end
