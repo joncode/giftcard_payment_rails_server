4d8cc6835e9ede47dfc732a2dc816a38a60f75a7
Jon Code <programcodex@gmail.com>
2014-03-21 17:45:53 -0400

    --ammend

--- spec/models/gift_campaign_spec.rb
+++ spec/models/gift_campaign_spec.rb
@@ -148,7 +148,7 @@ describe GiftCampaign do
         it_should_behave_like "gift serializer" do
             let(:object) { GiftCampaign.create(@gift_hsh) }
         end
-        
+
         it "should create gift" do
             gift_campaign = GiftCampaign.create(@gift_hsh)
             gift_campaign.class.should          == GiftCampaign
@@ -175,8 +175,78 @@ describe GiftCampaign do
             payable.reason_text.should == "Transaction approved."
             payable.reason_code.should == 1
         end
-    end
 
+        context "messaging" do
+
+            before(:each) do
+                ResqueSpec.reset!
+                WebMock.reset!
+            end
+
+            it "should not email invoice to the sender" do
+                stub_request(:post, "https://us7.api.mailchimp.com/2.0/lists/subscribe.json").to_return(:status => 200, :body => "{}", :headers => {})
+                stub_request(:post, "https://mandrillapp.com/api/1.0/messages/send-template.json").to_return(:status => 200, :body => "{}", :headers => {})
+                stub_request(:post, "https://q_NVI6G1RRaOU49kKTOZMQ:Lugw6dSXT6-e5mruDtO14g@go.urbanairship.com/api/push/").to_return(:status => 200, :body => "", :headers => {})
+                response = GiftCampaign.create @gift_hsh
+
+                run_delayed_jobs
+                abs_gift_id = response.id + NUMBER_ID
+
+                WebMock.should have_requested(:post, "https://mandrillapp.com/api/1.0/messages/send-template.json").with { |req|
+                    puts req.body;
+                    b = JSON.parse(req.body);
+                    if b["template_name"] == "iom-gift-gift-receipt"
+                        link = b["message"]["merge_vars"].first["vars"].first["content"];
+                        link.match(/signup\/acceptgift\/#{abs_gift_id}/)
+                    else
+                        true
+                    end
+
+                }.once
+            end
+
+            it "should email notify the recipient" do
+                stub_request(:post, "https://q_NVI6G1RRaOU49kKTOZMQ:Lugw6dSXT6-e5mruDtO14g@go.urbanairship.com/api/push/").to_return(:status => 200, :body => "", :headers => {})
+                stub_request(:post, "https://us7.api.mailchimp.com/2.0/lists/subscribe.json").to_return(:status => 200, :body => "{}", :headers => {})
+                stub_request(:post, "https://mandrillapp.com/api/1.0/messages/send-template.json").to_return(:status => 200, :body => "{}", :headers => {})
+
+                response = GiftCampaign.create @gift_hsh
+                run_delayed_jobs
+                abs_gift_id = response.id + NUMBER_ID
+                WebMock.should have_requested(:post, "https://mandrillapp.com/api/1.0/messages/send-template.json").with { |req|
+                    puts req.body;
+                    b = JSON.parse(req.body);
+                    if b["template_name"] == "iom-gift-notify-receiver"
+                        link = b["message"]["merge_vars"].first["vars"].first["content"];
+                        link.match(/signup\/acceptgift\/#{abs_gift_id}/)
+                    else
+                        true
+                    end
+                }.once
+            end
+
+            it "should push notify to app-user recipients" do
+                @receiver = FactoryGirl.create(:user, email: "customer@gmail.com")
+                stub_request(:post, "https://q_NVI6G1RRaOU49kKTOZMQ:Lugw6dSXT6-e5mruDtO14g@go.urbanairship.com/api/push/").to_return(:status => 200, :body => "", :headers => {})
+                stub_request(:post, "https://us7.api.mailchimp.com/2.0/lists/subscribe.json").to_return(:status => 200, :body => "{}", :headers => {})
+                stub_request(:post, "https://mandrillapp.com/api/1.0/messages/send-template.json").to_return(:status => 200, :body => "{}", :headers => {})
+                good_push_hsh = {:aliases =>["#{@receiver.ua_alias}"],:aps =>{:alert => "#{@campaign.name} sent you a gift at #{@location.name}!",:badge=>1,:sound=>"pn.wav"},:alert_type=>1}
+                Urbanairship.should_receive(:push).with(good_push_hsh)
+                response = GiftCampaign.create @gift_hsh
+                run_delayed_jobs
+            end
+
+            it "should not message users when payment_error" do
+                @receiver = FactoryGirl.create(:user, email: "customer@gmail.com")
+                stub_request(:post, "https://us7.api.mailchimp.com/2.0/lists/subscribe.json").to_return(:status => 200, :body => "{}", :headers => {})
+                stub_request(:post, "https://mandrillapp.com/api/1.0/messages/send-template.json").to_return(:status => 200, :body => "{}", :headers => {})
+                good_push_hsh = {:aliases =>["#{@receiver.ua_alias}"],:aps =>{:alert => "#{@campaign.name} sent you a gift at #{@location.name}!",:badge=>1,:sound=>"pn.wav"},:alert_type=>1}
+                Urbanairship.should_not_receive(:push).with(good_push_hsh)
+                GiftCampaign.create @gift_hsh
+                run_delayed_jobs
+            end
+        end
+    end
 end# == Schema Information
 #
 # Table name: gifts
