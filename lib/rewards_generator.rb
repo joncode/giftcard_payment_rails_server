f3b574c098b53737105fbe9fe5729923b7abff37
Jon Code <programcodex@gmail.com>
2014-03-28 09:46:10 -0400

    --ammend

--- lib/rewards_generator.rb
+++ lib/rewards_generator.rb
@@ -30,7 +30,7 @@ private
     def self.campaign_items_ary
         ci1 = CampaignItem.where(textword: 'a').first
         ci2 = CampaignItem.where(textword: 'b').first
-        return [ci1, ci2].uniq
+        return [ci1, ci2].compact
     end
 
     def self.get_users
