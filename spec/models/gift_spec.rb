2bee5004a2d451434e57707341f3847f905171fb
Jon Code <programcodex@gmail.com>
2014-04-18 14:49:02 -0400

    updated require cat spec

--- spec/models/gift_spec.rb
+++ spec/models/gift_spec.rb
@@ -38,9 +38,7 @@ describe Gift do
 	end
 
 	it "requires cat" do
-		user = FactoryGirl.create :user
-		provider = FactoryGirl.create :provider
-		gift = Gift.create(giver: user, receiver_name: user.name, receiver_email: user.email, provider_id: provider.id, value: "100", shoppingCart: "[{\"detail\":null,\"price\":13,\"quantity\":1,\"item_id\":82,\"item_name\":\"Original Margarita \"}]")
+		gift = FactoryGirl.build(:gift, :cat => nil)
 		gift.should_not be_valid
 		gift.should have_at_least(1).error_on(:cat)
 	end
