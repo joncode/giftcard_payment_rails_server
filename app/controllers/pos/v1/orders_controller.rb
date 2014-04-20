263553ce85cb4ead341c82c358b82e7780d685cd
Jon Code <programcodex@gmail.com>
2014-04-19 20:12:38 -0400

    tweaks

--- app/controllers/pos/v1/orders_controller.rb
+++ app/controllers/pos/v1/orders_controller.rb
@@ -2,6 +2,7 @@ class Pos::V1::OrdersController < JsonController
     http_basic_authenticate_with name: NEXT_GEN_USER, password: NEXT_GEN_PASS
 
     def create
+        puts "------------- params = #{params}"
         order = Order.init_with_pos(create_params)
         # this method should rturn text to the machine and maybe status but not this introspection
         if order.id.present?
@@ -19,15 +20,3 @@ private
     end
 end
 
-
-
-        # gift   = @current_user.received.where(id: params[:id]).first
-        # return nil if data_not_found?(gift)
-        # order = Order.init_with_gift(gift, server_code)
-        # if order.save
-        #     success({ "order_number" => order.make_order_num , "total" => gift.total,  "server" => order.server_code })
-        # else
-        #     fail order
-        #     #status = :bad_request
-        # end
-        # respond(status)
