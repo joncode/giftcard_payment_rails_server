fef37186d58c95ee85cbd0a4024c9d64dfb5bbd6
Jon Code <programcodex@gmail.com>
2014-05-06 15:16:34 -0400

    1.2.4.3

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -34,8 +34,8 @@ FEEDBACK_EMAIL = "feedback#{AT_EMAIL}"
 
 NUMBER_ID = 649387
 
-VERSION_NUMBER  = "1.2.4.2"
-VERSION_UPDATED = "5/2/14"
+VERSION_NUMBER  = "1.2.4.3"
+VERSION_UPDATED = "5/6/14"
 
 
 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
