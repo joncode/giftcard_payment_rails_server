9a294c68281c19a434ec5619e00bf9d9894fa739
Jon Code <programcodex@gmail.com>
2014-06-11 16:17:19 -0400

    upading the boomerang cron

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -35,8 +35,8 @@ FEEDBACK_EMAIL = "feedback#{AT_EMAIL}"
 NUMBER_ID = 649387
 
 
-VERSION_NUMBER  = "1.2.5.10-2"
-VERSION_UPDATED = "6/9/14"
+VERSION_NUMBER  = "1.2.6-2"
+VERSION_UPDATED = "6/11/14"
 
 BLANK_AVATAR_URL = "http://res.cloudinary.com/drinkboard/image/upload/v1398470766/avatar_blank_cvblvd.png"
 
