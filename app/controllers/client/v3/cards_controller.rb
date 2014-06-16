11bce075c01a4617c2639193602bcb0687feb40e
Jon Code <programcodex@gmail.com>
2014-06-16 16:01:48 -0400

    getting user cards from token

--- app/controllers/client/v3/cards_controller.rb
+++ app/controllers/client/v3/cards_controller.rb
@@ -1,8 +1,9 @@
 class Client::V3::CardsController < MetalController
 
     def index
-    	puts "\n FAEK TOEK = #{request.headers["HTTP_X_AUTH_TOKEN"]} \n"
-        user = User.find(params[:user_id])
+    	token = request.headers["HTTP_X_AUTH_TOKEN"]
+    	puts "\n User Token = #{token} \n"
+        user = User.find_by(remember_token: token)
         cards_ary = Card.get_cards(user)
         cards_ary.each do |c_hsh|
             c_hsh["user_id"] = user.id
