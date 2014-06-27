852fec0feb012a0cffe7770a77915a31f922cb1e
iumehara <iumehara10@gmail.com>
2014-06-27 10:50:21 -0400

    1.2.7

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -34,8 +34,8 @@ FEEDBACK_EMAIL = "feedback#{AT_EMAIL}"
 
 NUMBER_ID = 649387
 
-VERSION_NUMBER  = "1.2.6.7"
-VERSION_UPDATED = "6/23/14"
+VERSION_NUMBER  = "1.2.7"
+VERSION_UPDATED = "6/27/14"
 
 
 BLANK_AVATAR_URL = "http://res.cloudinary.com/drinkboard/image/upload/v1398470766/avatar_blank_cvblvd.png"
