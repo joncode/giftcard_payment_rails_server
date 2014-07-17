81eaf336260e3252327a4bc9f834a1a23f5e47ef
iumehara <iumehara10@gmail.com>
2014-07-17 14:39:26 -0400

    adds at gift proto joins controller

---
+++ app/controllers/admt/v2/gift_proto_joins_controller.rb
@@ -0,0 +1,20 @@
+class Admt::V2::GiftProtoJoinsController < JsonController
+
+	before_action :authenticate_admin_tools
+	rescue_from JSON::ParserError, :with => :bad_request
+
+	def create
+		proto = Proto.find params[:id]
+		number = proto.giftables.count
+        if number == 0
+        	success "All gifts have already been created for gift prototype #{params[:id]}"
+        	status = 201
+        else
+			Resque.enqueue(ProtoGifterJob, proto.id)
+        	success "Request for #{number} gifts from #{proto.giver_name} received."
+        	status = 202
+        end
+    	respond(status)	end
+
+
+end
