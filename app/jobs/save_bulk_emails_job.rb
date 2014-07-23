f26d4c2545e86f8142742608ff9923a0525985bf
Jon Code <programcodex@gmail.com>
2014-07-23 19:27:36 -0400

    changing from where to find

--- app/jobs/save_bulk_emails_job.rb
+++ app/jobs/save_bulk_emails_job.rb
@@ -3,18 +3,19 @@ class SaveBulkEmailsJob
     @queue = :database
 
     def self.perform data
-
-    	bulk_emails = BulkEmail.where(provider_id: data["provider_id"], proto_id: data["proto_id"], processed: false)
-    	bulk_emails.each do |be_obj|
-    		begin
-	    		emails = JSON.parse be_obj.data
-	    		emails.each do |email|
-			    	social = SaveSocialImport.perform(email, data["provider_id"], data["proto_id"])
-	    		end
-	    		be_obj.update(processed: true)
-	    	rescue
-	    		puts "\n\n\n BULK EMAIL FAILS\n\n\n"
-	    	end
+		puts "\n SaveBulkEmailsJob\n"
+    	be_obj = BulkEmail.find(data)
+		begin
+    		emails = JSON.parse(be_obj.data)
+    		emails.each do |email|
+    			if email.kind_of?(Array)
+    				email = email[0]
+    			end
+		    	social = SaveSocialImport.perform(email, data["provider_id"], data["proto_id"])
+    		end
+    		be_obj.update(processed: true)
+    	rescue
+    		puts "\n\n\n BULK EMAIL FAILS\n\n\n"
     	end
     end
 
