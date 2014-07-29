ca21c02936d38dcc92f667637ccb77cd46b15f90
iumehara <iumehara10@gmail.com>
2014-07-29 11:41:37 -0400

    renames admin_users_socials table to at_users_socials

--- db/migrate/20140728151935_create_admin_users_socials.rb
+++
@@ -1,9 +0,0 @@
-class CreateAdminUsersSocials < ActiveRecord::Migration
-  def change
-    create_table :admin_users_socials do |t|
-      t.integer :admin_user_id
-      t.integer :social_id
-      t.timestamps
-    end
-  end
-end
