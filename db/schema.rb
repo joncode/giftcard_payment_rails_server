7b7174667536a48b4ee7514ebfcc652df21fde02
Jon Code <programcodex@gmail.com>
2014-08-06 15:53:28 -0400

    adding proto_id index to bulk emails

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140806162647) do
+ActiveRecord::Schema.define(version: 20140806195222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(version: 20140806162647) do
   end
 
   add_index "bulk_emails", ["at_user_id"], name: "index_bulk_emails_on_at_user_id", using: :btree
+  add_index "bulk_emails", ["proto_id"], name: "index_bulk_emails_on_proto_id", using: :btree
 
   create_table "campaign_items", force: true do |t|
     t.integer  "campaign_id"
