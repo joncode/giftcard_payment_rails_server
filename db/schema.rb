6ba1dbf94f9d368e5b1bacbb10a11f94d49da99a
iumehara <iumehara10@gmail.com>
2014-08-05 22:27:32 -0400

    adds at_user_id to bulk_email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140805163958) do
+ActiveRecord::Schema.define(version: 20140806022550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,7 +88,10 @@ ActiveRecord::Schema.define(version: 20140805163958) do
     t.integer  "provider_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "at_user_id"
   end
+
+  add_index "bulk_emails", ["at_user_id"], name: "index_bulk_emails_on_at_user_id", using: :btree
 
   create_table "campaign_items", force: true do |t|
     t.integer  "campaign_id"
