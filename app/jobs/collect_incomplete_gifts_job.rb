19db316102c1680f196355287c671db26b76a0e4
Jon Code <programcodex@gmail.com>
2014-08-13 15:01:39 -0400

    rmoveing the autoscaler

--- app/jobs/collect_incomplete_gifts_job.rb
+++ app/jobs/collect_incomplete_gifts_job.rb
@@ -1,7 +1,7 @@
 require 'resque/plugins/resque_heroku_autoscaler'
 
 class CollectIncompleteGiftsJob
-    extend Resque::Plugins::HerokuAutoscaler
+    #extend Resque::Plugins::HerokuAutoscaler
 
     @queue = :after_save
 
