698cee6aa59135ccbf414fefb3e032db1195b54d
Jon Code <programcodex@gmail.com>
2014-08-13 19:38:34 -0400

    changing the resque config file to go after_fork

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -3,15 +3,24 @@ Dir[File.join(Rails.root, 'app', 'jobs', '*.rb')].each { |file| require file }
 if Rails.env.production?
 	ENV["REDISTOGO_URL"] ||= "redis://redistogo:973cdc884e18482f5b324194e3b4cde1@barb.redistogo.com:9497/"
 	uri = URI.parse(ENV["REDISTOGO_URL"])
-	Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+	host = uri.host
+	port = uri.port
+	password = uri.password
 elsif Rails.env.staging?
 	ENV["REDISTOGO_URL"] ||= "redis://redistogo:7a26911511c6ef1c1b2f32fad240ae0a@squawfish.redistogo.com:9819/"
 	uri = URI.parse(ENV["REDISTOGO_URL"])
-	Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+	host = uri.host
+	port = uri.port
+	password = uri.password
 else
-	Resque.redis = Redis.new(:host => "localhost", :port => 6379)
+	host = "localhost"
+	port = 6379
+	password = nil
 end
 
-#-- Commented out 4/17/14 -- connection is reestablished in unicorn.rb
-# Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
+
+Resque.after_fork do
+  ActiveRecord::Base.establish_connection
+  Resque.redis = Redis.new(:host => host, :port => port, :password => password)
+end
 
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,5 @@
 worker_processes 3
-timeout 20
+timeout 19
 preload_app true
 
 before_fork do |server, worker|
@@ -9,14 +9,15 @@ before_fork do |server, worker|
     Process.kill 'QUIT', Process.pid
   end
 
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+  end
+
   if defined?(Resque)
     Resque.redis.quit
     Rails.logger.info('Disconnected from Redis')
   end
 
-  if defined?(ActiveRecord::Base)
-    ActiveRecord::Base.connection.disconnect!
-  end
 end
 
 after_fork do |server, worker|
@@ -25,21 +26,18 @@ after_fork do |server, worker|
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
   end
 
+  if defined?(ActiveRecord::Base)
+    config = ActiveRecord::Base.configurations[Rails.env] ||
+                Rails.application.config.database_configuration[Rails.env]
+    config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
+    config['pool']              = ENV['DB_POOL'] || 2
+    ActiveRecord::Base.establish_connection(config)
+  end
+
   if defined?(Resque)
     Resque.redis = ENV['REDISTOGO_URL']
     Rails.logger.info('Connected to Redis')
   end
 
-  #-- Updated 4/17/14 -- adds DB_POOl and DB_REAP_FREQ
-  #-- https://devcenter.heroku.com/articles/concurrency-and-database-connections
-  if defined?(ActiveRecord::Base)
-    config = ActiveRecord::Base.configurations[Rails.env] ||
-                Rails.application.config.database_configuration[Rails.env]
-    config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
-    config['pool']            =   ENV['DB_POOL'] || 2
-    ActiveRecord::Base.establish_connection(config)
-  end
 
-  # defined?(ActiveRecord::Base) and
-  #   ActiveRecord::Base.establish_connection
 end
