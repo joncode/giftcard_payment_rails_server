945a4148254c88e61026234a051a229d77f427a0
iumehara <iumehara10@gmail.com>
2014-09-16 14:23:02 -0400

    moves bulk social import tests from merchant tools to dbapp

--- app/services/save_social_import.rb
+++ app/services/save_social_import.rb
@@ -10,11 +10,10 @@ class SaveSocialImport
             #errors.add :email, "Row #{index+1}: #{social.network_id} is not a valid email"
             puts "#{social.network_id} is not a valid email - #{social.errors.full_messages}"
         else
-
             ProvidersSocial.create(provider_id: provider_id, social_id: social.id)
-        	if proto_id > 0
-        		ProtoJoin.find_or_create_by(receivable_type: "Social", receivable_id: social.id, gift_id: nil, proto_id: proto_id)
-        	end
+            if proto_id && proto_id > 0
+                ProtoJoin.find_or_create_by(receivable_type: "Social", receivable_id: social.id, gift_id: nil, proto_id: proto_id)
+            end
         	# number_good += 1
         end
         return social
---
+++ spec/jobs/save_bulk_emails_job_spec.rb
@@ -0,0 +1,158 @@
+require 'spec_helper'
+
+describe SaveBulkEmailsJob do
+
+	before(:each) do
+		@provider = FactoryGirl.create(:provider)
+	end
+
+	describe :save do
+
+		it "should create the providers_socials" do
+			merchant = FactoryGirl.create(:merchant)
+			provider = FactoryGirl.create(:provider, merchant_id: merchant.id)
+			proto    = FactoryGirl.create(:proto, provider_id: provider.id)
+
+			bulk_email = FactoryGirl.create :bulk_email,
+				data: "[\"ann@email.com\",\"bob@email.com\",\"cam@email.com\"]",
+				proto_id: proto.id,
+				provider_id: provider.id
+			SaveBulkEmailsJob.perform(bulk_email.id)
+			ProvidersSocial.count.should == 3
+			ProtoJoin.count.should == 3
+		end
+
+		it "should create the providers_social but only once" do
+			merchant = FactoryGirl.create(:merchant)
+			provider = FactoryGirl.create(:provider, merchant_id: merchant.id)
+			proto    = FactoryGirl.create(:proto, provider_id: provider.id)
+			bulk_email = FactoryGirl.create :bulk_email,
+				data: "[\"ann@email.com\",\"bob@email.com\",\"cam@email.com\"]",
+				proto_id: proto.id,
+				provider_id: provider.id
+			SaveBulkEmailsJob.perform(bulk_email.id)
+			s = Social.all
+			s.count.should == 3
+			providers_socials = ProvidersSocial.where(provider_id: provider.id)
+			providers_socials.count.should == 3
+
+			bulk_email2 = FactoryGirl.create :bulk_email,
+				data: "[\"ann@email.com\",\"bob@email.com\",\"cam@email.com\"]",
+				proto_id: proto.id,
+				provider_id: provider.id
+			SaveBulkEmailsJob.perform(bulk_email2.id)
+			s = Social.all
+			s.count.should == 3
+			providers_socials = ProvidersSocial.where(provider_id: provider.id)
+			providers_socials.count.should == 3
+		end
+
+		it "should not create multiple Socials for same network and network_id" do
+			merchant = FactoryGirl.create(:merchant)
+			provider = FactoryGirl.create(:provider, merchant_id: merchant.id)
+			proto    = FactoryGirl.create(:proto, provider_id: provider.id)
+
+			bulk_email = FactoryGirl.create :bulk_email,
+				data: "[\"ann@email.com\",\"ann@email.com\",\"cam@email.com\"]",
+				proto_id: proto.id,
+				provider_id: provider.id
+			SaveBulkEmailsJob.perform(bulk_email.id)
+			ProvidersSocial.count.should == 2
+			ProtoJoin.count.should == 2
+		end
+
+		it "should reject bad contacts and save good contacts" do
+			merchant = FactoryGirl.create(:merchant)
+			provider = FactoryGirl.create(:provider, merchant_id: merchant.id)
+			proto    = FactoryGirl.create(:proto, provider_id: provider.id)
+
+			bulk_email = FactoryGirl.create :bulk_email,
+				data: "[\"ann@email.com\",\"bobatemail.com\",\"cam@emailcom\"]",
+				proto_id: proto.id,
+				provider_id: provider.id
+			SaveBulkEmailsJob.perform(bulk_email.id)
+			Social.count.should == 1
+			ProvidersSocial.count.should == 1
+			ProtoJoin.count.should == 1
+		end
+
+		it "should save the emails as socials without making proto joins" do
+			merchant = FactoryGirl.create(:merchant)
+			provider = FactoryGirl.create(:provider, merchant_id: merchant.id)
+
+			bulk_email = FactoryGirl.create :bulk_email,
+				data: "[\"ann@email.com\",\"bob@email.com\",\"cam@email.com\"]",
+				proto_id: nil,
+				provider_id: provider.id
+			SaveBulkEmailsJob.perform(bulk_email.id)
+			Social.count.should == 3
+			ProvidersSocial.count.should == 3
+			ProtoJoin.count.should == 0
+		end
+
+		it "should save the emails as socials AND create the proto join" do
+			merchant = FactoryGirl.create(:merchant)
+			provider = FactoryGirl.create(:provider, merchant_id: merchant.id)
+			proto    = FactoryGirl.create(:proto, provider_id: provider.id)
+
+			bulk_email = FactoryGirl.create :bulk_email,
+				data: "[\"ann@email.com\",\"bob@email.com\",\"cam@email.com\"]",
+				proto_id: proto.id,
+				provider_id: provider.id
+			SaveBulkEmailsJob.perform(bulk_email.id)
+			Social.count.should == 3
+			ProvidersSocial.count.should == 3
+			ProtoJoin.count.should == 3
+		end
+
+		it "should not create proto joins when one exists without a gift id" do
+			proto = FactoryGirl.create(:proto)
+			["bob@email.com", "cam@email.com", "ann@email.com"].each do |email|
+				soci = Social.create(network: 'email', network_id: email)
+				ProtoJoin.create(proto_id: proto.id, receivable_id: soci.id, receivable_type: 'Social', gift_id: nil)
+			end
+			socials = Social.all
+			socials.count.should == 3
+			socials.each do |soc|
+				soc.proto_joins.count.should == 1
+			end
+
+			bulk_email = FactoryGirl.create :bulk_email,
+				data: "[\"ann@email.com\",\"bob@email.com\",\"cam@email.com\"]",
+				proto_id: proto.id
+
+			SaveBulkEmailsJob.perform(bulk_email.id)
+			socials = Social.all
+			socials.count.should == 3
+			socials.each do |soc|
+				soc.proto_joins.count.should == 1
+			end
+		end
+
+		it "should create proto joins when one exists with a gift id" do
+			proto = FactoryGirl.create(:proto)
+			["bob@email.com", "cam@email.com", "ann@email.com"].each_with_index do |email,index|
+				soci = Social.create(network: 'email', network_id: email)
+				ProtoJoin.create(proto_id: proto.id, receivable_id: soci.id, receivable_type: 'Social', gift_id: index + 1)
+			end
+			socials = Social.all
+			socials.count.should == 3
+			socials.each do |soc|
+				soc.proto_joins.count.should == 1
+			end
+			
+			bulk_email = FactoryGirl.create :bulk_email,
+				data: "[\"ann@email.com\",\"bob@email.com\",\"cam@email.com\"]",
+				proto_id: proto.id
+
+			SaveBulkEmailsJob.perform(bulk_email.id)
+			socials = Social.all
+			socials.count.should == 3
+			socials.each do |soc|
+				soc.proto_joins.count.should == 2
+			end
+		end
+
+	end
+
+end
