d504f33b9068b48220e58ac97b18e9df2f1d0f5d
iumehara <iumehara10@gmail.com>
2014-09-19 17:38:42 -0400

    updates city_list_web method from .each to .map

--- app/controllers/web/v3/regions_controller.rb
+++ app/controllers/web/v3/regions_controller.rb
@@ -3,9 +3,10 @@ class Web::V3::RegionsController < MetalController
     before_action :authenticate_web_general
 
     def index
-        city_list_web = CITY_LIST.each do |city|
-            city["token"] = city["name"].downcase.gsub(/ /, '_')
+        city_list_web = CITY_LIST.map do |city|
+            city["token"] = city["name"].downcase.gsub(/ /, '-')
             city["region_id"] = city.delete("city_id")
+            city
         end
         success city_list_web
         respond
