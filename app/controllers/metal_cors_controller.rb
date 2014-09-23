44a57a4f93c684b9dc0c878f43ad129c61f5b39c
iumehara <iumehara10@gmail.com>
2014-09-22 20:37:28 -0400

    adds content-type and accept to the cross_origin_allow_header

--- app/controllers/metal_cors_controller.rb
+++ app/controllers/metal_cors_controller.rb
@@ -15,6 +15,8 @@ private
         headers['Access-Control-Allow-Methods']  = 'POST, PUT, DELETE, GET, OPTIONS'
         headers['Access-Control-Request-Method'] = '*'
         headers['Access-Control-Allow-Headers']  = 'Origin, X-Requested-With, Content-Type, Accept, TKN, Mdot-Version, Android-Version'
+        headers['Content-Type']                  = "application/json"
+        headers['Accept']                        = "application/json"
     end
 
 end
