d35bc48129ac27ac1c8a61d83b0ce6e3f66f81f4
Jon Code <programcodex@gmail.com>
2014-09-23 17:25:25 -0400

    --amend gs

--- app/controllers/metal_cors_controller.rb
+++ app/controllers/metal_cors_controller.rb
@@ -47,7 +47,7 @@ private
         headers['Access-Control-Allow-Origin']   = "*"
         headers['Access-Control-Allow-Methods']  = 'POST, PUT, DELETE, GET, OPTIONS'
         headers['Access-Control-Request-Method'] = '*'
-        headers['Access-Control-Allow-Headers']  = 'Origin, X-Requested-With, Content-Type, Accept, X_AUTH_TOKEN, Mdot-Version, Android-Version'
+        headers['Access-Control-Allow-Headers']  = 'Origin, X-Requested-With, Content-Type, Accept, X-AUTH-TOKEN, Mdot-Version, Android-Version'
     end
 
 end
