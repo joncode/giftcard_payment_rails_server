ead8925e334fe3c50eb0fa62b9f0d0afe8cd0cc2
Jon Code <programcodex@gmail.com>
2014-09-23 17:23:13 -0400

    fixing the CORS path

--- app/controllers/metal_cors_controller.rb
+++ app/controllers/metal_cors_controller.rb
@@ -1,9 +1,9 @@
 class MetalCorsController < MetalController
 
 	before_action :print_params
-    after_filter         :cross_origin_allow_header
-    after_action :print_params
-    
+    after_action  :cross_origin_allow_header
+    after_action  :print_params
+
 
 protected
 
@@ -47,7 +47,7 @@ private
         headers['Access-Control-Allow-Origin']   = "*"
         headers['Access-Control-Allow-Methods']  = 'POST, PUT, DELETE, GET, OPTIONS'
         headers['Access-Control-Request-Method'] = '*'
-        headers['Access-Control-Allow-Headers']  = 'Origin, X-Requested-With, Content-Type, Accept, TKN, Mdot-Version, Android-Version'
+        headers['Access-Control-Allow-Headers']  = 'Origin, X-Requested-With, Content-Type, Accept, X_AUTH_TOKEN, Mdot-Version, Android-Version'
     end
 
 end
