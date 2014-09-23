116691886f4f8ed1660920da5bf2c4dff139d085
Jon Code <programcodex@gmail.com>
2014-09-23 19:01:26 -0400

    --ammend

--- app/controllers/metal_cors_controller.rb
+++ app/controllers/metal_cors_controller.rb
@@ -1,7 +1,7 @@
 class MetalCorsController < MetalController
 
 	# before_action :print_params
-    after_action  :cross_origin_allow_header
+    after_filter :cross_origin_allow_header
     #after_action  :print_params
 
 
