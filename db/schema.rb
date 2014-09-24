7168969d99f80ee16e5ad8d9ff207e9a6db00bf6
iumehara <iumehara10@gmail.com>
2014-09-24 18:10:35 -0400

    adds daily_stat model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140911202244) do
+ActiveRecord::Schema.define(version: 20140924191849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -161,6 +161,15 @@ ActiveRecord::Schema.define(version: 20140911202244) do
     t.integer  "owner_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "daily_stats", force: true do |t|
+    t.string   "dash_day_old"
+    t.string   "dash_week_old"
+    t.string   "dash_month_old"
+    t.string   "dash_total"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "debts", force: true do |t|
