79c398a3af76926afcb06cfab99fddbb2b4657c0
Jon Code <programcodex@gmail.com>
2014-10-13 05:13:32 -0400

    upgrading ruby and gems

--- app/controllers/web/v3/regions_controller.rb
+++ app/controllers/web/v3/regions_controller.rb
@@ -5,7 +5,7 @@ class Web::V3::RegionsController < MetalCorsController
     def index
         city_list = CITY_LIST
         city_list.each  do |c|
-            c["token"] = c["name"].downcase.gsub(' ', '-')
+            c["token"]     = c["name"].downcase.gsub(' ', '-')
             c["region_id"] = c["city_id"]
         end
         success city_list
