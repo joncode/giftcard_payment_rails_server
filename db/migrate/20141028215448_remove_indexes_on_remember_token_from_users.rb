9cac8828f9882ee984dea645685b6a222ff2ec69
Jon Code <programcodex@gmail.com>
2014-10-28 17:58:30 -0400

    removign indexes schema

--- db/migrate/20141028215448_remove_indexes_on_remember_token_from_users.rb
+++ db/migrate/20141028215448_remove_indexes_on_remember_token_from_users.rb
@@ -1,6 +1,11 @@
 class RemoveIndexesOnRememberTokenFromUsers < ActiveRecord::Migration
-  def change
+  def up
   	remove_index :users, ["active", "perm_deactive", "remember_token"]
   	remove_index :users, ["remember_token"]
+  end
+
+  def down
+  	add_index :users, ["active", "perm_deactive", "remember_token"]
+  	add_index :users, ["remember_token"]
   end
 end
