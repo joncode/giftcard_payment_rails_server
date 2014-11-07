817fe877a6693a255410bedde20e4a17001534a7
Jon Code <programcodex@gmail.com>
2014-11-06 19:38:55 -0500

    commenting out unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,49 +1,49 @@
-worker_processes 3
-timeout 19
-preload_app true
+# worker_processes 3
+# timeout 19
+# preload_app true
 
-before_fork do |server, worker|
-  puts "Before Fork" # - #{server.inspect}\n #{worker.inspect}"
+# before_fork do |server, worker|
+#   puts "Before Fork" # - #{server.inspect}\n #{worker.inspect}"
 
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
-  end
+#   Signal.trap 'TERM' do
+#     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+#     Process.kill 'QUIT', Process.pid
+#   end
 
-  if defined?(Resque)
-    Resque.redis.quit
-    Rails.logger.info('Disconnected from Redis')
-  end
+#   if defined?(Resque)
+#     Resque.redis.quit
+#     Rails.logger.info('Disconnected from Redis')
+#   end
 
-  # defined?(Redis) and
-  #   Redis.current.quit
-  if defined?(ActiveRecord::Base)
-    ActiveRecord::Base.connection.disconnect!
-  end
-end
+#   # defined?(Redis) and
+#   #   Redis.current.quit
+#   if defined?(ActiveRecord::Base)
+#     ActiveRecord::Base.connection.disconnect!
+#   end
+# end
 
-after_fork do |server, worker|
-  puts "After Fork" # - #{server.inspect} \n#{worker.inspect}"
+# after_fork do |server, worker|
+#   puts "After Fork" # - #{server.inspect} \n#{worker.inspect}"
 
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
-  end
+#   Signal.trap 'TERM' do
+#     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
+#   end
 
-  if defined?(Resque)
-    Resque.redis = ENV['REDISTOGO_URL']
-    Rails.logger.info('Connected to Redis')
-  end
+#   if defined?(Resque)
+#     Resque.redis = ENV['REDISTOGO_URL']
+#     Rails.logger.info('Connected to Redis')
+#   end
 
-  if defined?(ActiveRecord::Base)
-    config = ActiveRecord::Base.configurations[Rails.env] ||
-                Rails.application.config.database_configuration[Rails.env]
-    config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
-    config['pool']              = ENV['DB_POOL'] || 2
-    ActiveRecord::Base.establish_connection(config)
-  end
+#   if defined?(ActiveRecord::Base)
+#     config = ActiveRecord::Base.configurations[Rails.env] ||
+#                 Rails.application.config.database_configuration[Rails.env]
+#     config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
+#     config['pool']              = ENV['DB_POOL'] || 2
+#     ActiveRecord::Base.establish_connection(config)
+#   end
 
-  # defined?(Redis) and
-  #   $redis = Redis.current = Redis.new( url: ENV['REDISTOGO_URL'] )
+#   # defined?(Redis) and
+#   #   $redis = Redis.current = Redis.new( url: ENV['REDISTOGO_URL'] )
 
 
-end
+# end
