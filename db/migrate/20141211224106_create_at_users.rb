ab4aa785cc2cc864e74cc96e6990c2fd35da965f
Jon Code <programcodex@gmail.com>
2014-12-11 18:20:21 -0500

    fix incorrect migration users

--- db/migrate/20141211224106_create_at_users.rb
+++ db/migrate/20141211224106_create_at_users.rb
@@ -1,6 +1,6 @@
 class CreateAtUsers < ActiveRecord::Migration
   def change
-	  create_table "users", force: true do |t|
+	  create_table "at_users", force: true do |t|
 	    t.string   "first_name"
 	    t.string   "last_name"
 	    t.string   "email"
