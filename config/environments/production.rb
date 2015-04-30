f6954c135f9d02bad6a29d89966f145f3088cd4d
Jon Code <programcodex@gmail.com>
2015-04-30 14:09:27 -0400

    setting rails log level off debug so no db calls in production logs :(

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ Drinkboard::Application.configure do
 
   # See everything in the log (default is :info)
 
-  config.log_level = :debug
+  config.log_level = :info
 
 
   # Prepend all log lines with the following tags
