fc9ae2ef3e499d8d9c212b49af4ac44ef4ea9efd
Jon Code <programcodex@gmail.com>
2015-06-04 02:06:31 -0400

    better seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ cities =  [
   {"name"=>"San Francisco", "detail"=>"California", "photo"=>"d|v1378747548/san_francisco_hv2bsc.jpg"},
   {"name"=>"Santa Barbara", "detail"=>"California", "photo"=>"d|v1393292171/santa_barbara_lqln3n.jpg"},
   {"name"=>"Philadelphia", "detail"=>"Pennsylvania", active: false},
-  {"name"=>"Long Beach", "detail"=>"California", active: false},
+  {"name"=>"Long Beach", "detail"=>"California", 'photo'=>'d|v1416615199/LongBeach_jtrb0s.jpg', active: false},
   {"name"=>"Newport Beach", "detail"=>"California", "photo"=>"d|v1416615229/newportbeach_bwwmrq.jpg"},
   {"name"=>"Elkhart Lake", "detail"=>"Wisconsin", "photo"=>"d|v1418237673/elkheart_tplhzq.jpg"},
   {"name"=>"COCHON 555 US TOUR", "detail"=>"Nationwide", "photo"=>"d|v1417972995/cochon_hr8ixy.png"},
