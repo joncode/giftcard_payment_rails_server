ea703afef5de94919d42bb7a5dfbd7e8cf989e88
Jon Code <programcodex@gmail.com>
2015-06-10 17:39:32 -0400

    devise db migrations

--- db/migrate/20150601202736_add_devise_to_mt_users.rb
+++ db/migrate/20150601202736_add_devise_to_mt_users.rb
@@ -40,8 +40,14 @@ class AddDeviseToMtUsers < ActiveRecord::Migration
   end
 
   def self.down
-    # By default, we don't want to make any assumption about how to roll back a migration when your
-    # model already existed. Please edit below which fields you would like to remove in this migration.
-    raise ActiveRecord::IrreversibleMigration
+    remove_column :mt_users, :encrypted_password
+    remove_column :mt_users, :reset_password_sent_at
+    remove_column :mt_users, :remember_created_at
+    remove_column :mt_users, :sign_in_count
+    remove_column :mt_users, :current_sign_in_at
+    remove_column :mt_users, :last_sign_in_at
+    remove_column :mt_users, :current_sign_in_ip
+    remove_column :mt_users, :last_sign_in_ip
   end
 end
+
