696ced1ec95550b8f2df758a97a38dc8ffa72e6f
Jon Code <programcodex@gmail.com>
2015-07-06 14:27:33 -0400

    setting up datbase cleaner

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,26 +1,26 @@
 RSpec.configure do |config|
 
-  config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
-  end
+	config.before(:suite) do
+		DatabaseCleaner.clean_with(:truncation)
+	end
 
-  config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
-  end
+	config.before(:each) do
+		DatabaseCleaner.strategy = :transaction
+	end
 
-  config.before(:each, :js => true) do
-    DatabaseCleaner.strategy = :truncation
-  end
+	config.before(:each, :js => true) do
+		DatabaseCleaner.strategy = :truncation
+	end
 
-  config.before(:each) do
-    DatabaseCleaner.start
-    if Region.city.nil? || Region.city.count == 0
-        load "#{Rails.root}/db/seeds.rb"
-    end
-  end
+	config.before(:each) do
+		DatabaseCleaner.start
+		if Region.city.nil? || Region.city.count == 0
+				load "#{Rails.root}/db/seeds.rb"
+		end
+	end
 
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
+	config.after(:each) do
+		DatabaseCleaner.clean
+	end
 
 end
