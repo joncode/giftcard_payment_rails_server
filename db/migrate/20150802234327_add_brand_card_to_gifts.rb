83acb2141b04fab2ca125cf5f75d95d6bea09d23
Jon Code <programcodex@gmail.com>
2015-08-02 21:06:03 -0400

    adding brand card to campaign items and gifts

--- db/migrate/20150802234327_add_brand_card_to_gifts.rb
+++ db/migrate/20150802234327_add_brand_card_to_gifts.rb
@@ -1,6 +1,6 @@
 class AddBrandCardToGifts < ActiveRecord::Migration
   def change
-  	add_column :gifts, :brand_card, :boolean, default: false
   	add_column :campaign_items, :brand_card, :boolean, default: false
+  	add_column :gifts, :brand_card, :boolean, default: false
   end
 end
