210cdcabb9ac86788624dcdbe24c908b6e05649a
Jon Code <programcodex@gmail.com>
2015-09-25 13:58:42 -0700

    turning off register push job

--- app/jobs/register_push_job.rb
+++ app/jobs/register_push_job.rb
@@ -7,9 +7,9 @@ class RegisterPushJob
     @queue = :push
 
     def self.perform pn_token_id
-        pn_token    = PnToken.find pn_token_id
-        puts "registering PN Token for #{pn_token.user.name}"
-        self.ua_register(pn_token.pn_token, pn_token.ua_alias, pn_token.user_id, pn_token.platform)
+        # pn_token    = PnToken.find pn_token_id
+        # puts "registering PN Token for #{pn_token.user.name}"
+        # self.ua_register(pn_token.pn_token, pn_token.ua_alias, pn_token.user_id, pn_token.platform)
     end
 
 end
