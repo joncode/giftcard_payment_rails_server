17ecc4d5e2e043a50c7fa54f976baa9ac768245a
Jon Code <programcodex@gmail.com>
2015-09-28 16:15:18 -0700

    adding tighter expiration

--- lib/expiration.rb
+++ lib/expiration.rb
@@ -6,7 +6,7 @@ module Expiration
         Gift.where(status: ["incomplete", "open", "notified"]).where.not(expires_at: nil).find_each do |gift|
             date = Time.now.utc.to_date
 
-            if (date > gift.expires_at.to_date)
+            if (date >= gift.expires_at.to_date)
                 gift.update(status: "expired", redeemed_at: Time.now.utc)
                 "-------------  Expired gift ID = #{gift.id}  -------------"
                 expired_gifts += 1
