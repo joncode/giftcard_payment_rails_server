065a2e76531fd038858c7a0dd66801930740f199
Jon Code <programcodex@gmail.com>
2015-10-04 18:05:22 -0700

    demo gift scheduler

--- lib/demo_gifts.rb
+++ lib/demo_gifts.rb
@@ -1,6 +1,7 @@
 module DemoGifts
 
 	def self.perform
+		puts "DEMOGIFT scheduler"
 		[ 347844, 347845, 347846, 347847, 347848].each do | gift_id |
 			gift = Gift.find(gift_id)
 			gift.update(status: 'notified' , redeemed_at: nil, token: nil, order_num: nil)
