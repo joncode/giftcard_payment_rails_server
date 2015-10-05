008165b5481a60c150dfdc33a5c48cd5c3660efb
Jon Code <programcodex@gmail.com>
2015-10-04 20:27:49 -0700

    onlyh setting cache for users

--- app/jobs/GiftAfterSaveJob.rb
+++ app/jobs/GiftAfterSaveJob.rb
@@ -11,12 +11,14 @@ class GiftAfterSaveJob
     		gift = Gift.includes(:giver).includes(:receiver).find(gift_or_gift_id)
     	end
         [gift.giver, gift.receiver].each do |person|
-            client_id = gift.client_id || client_id
-            gifts = Gift.get_user_activity_in_client(person, client_id)
-            puts "setting cache for #{client_id} - #{gift.id}"
-            RedisWrap.set_user_gifts(client_id, person.id, gifts.serialize_objs(:web))
-            if gift.giver_id == gift.receiver_id
-                break
+            if person.kind_of?(User)
+                client_id = gift.client_id || client_id
+                gifts = Gift.get_user_activity_in_client(person, client_id)
+                puts "REDISWRAP setting cache for #{client_id} - #{gift.id}"
+                RedisWrap.set_user_gifts(client_id, person.id, gifts.serialize_objs(:web))
+                if gift.giver_id == gift.receiver_id
+                    break
+                end
             end
         end
 
