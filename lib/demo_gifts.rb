123ede0d6db514cf2345cef9330678086250c3d0
Jon Code <programcodex@gmail.com>
2015-10-05 10:56:01 -0700

    leaving token on unredeemed gift

--- lib/demo_gifts.rb
+++ lib/demo_gifts.rb
@@ -5,7 +5,7 @@ module DemoGifts
 		[ 347844, 347845, 347846, 347847, 347848].each do | gift_id |
 			gift = Gift.find(gift_id)
 			if gift.status == 'redeemed'
-				gift.update(status: 'notified' , redeemed_at: nil, token: nil, order_num: nil)
+				gift.update(status: 'notified' , redeemed_at: nil, order_num: nil)
 			end
 			gift.notify(true)
 		end
