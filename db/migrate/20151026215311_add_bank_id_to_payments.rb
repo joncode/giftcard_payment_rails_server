f1ded0b368f05f22ee21b168ee31dff5b7e4b9e7
Jon Code <programcodex@gmail.com>
2015-10-26 18:44:02 -0400

    removing unique constraint

--- db/migrate/20151026215311_add_bank_id_to_payments.rb
+++ db/migrate/20151026215311_add_bank_id_to_payments.rb
@@ -1,6 +1,6 @@
 class AddBankIdToPayments < ActiveRecord::Migration
   def change
   	add_column :payments, :bank_id, :integer
-   	add_index :payments, [:bank_id, :start_date], unique: true
+   	add_index :payments, [:bank_id, :start_date]
   end
 end
