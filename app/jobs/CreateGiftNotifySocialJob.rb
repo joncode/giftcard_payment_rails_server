ac3f1966bfa746095a12c27a76a68b22c50d038c
Jon Code <programcodex@gmail.com>
2015-11-09 20:52:44 -0800

    notify social via gift

--- app/jobs/CreateGiftNotifySocialJob.rb
+++ app/jobs/CreateGiftNotifySocialJob.rb
@@ -11,9 +11,16 @@ class CreateGiftNotifySocial
             oauth = gift.oauth
             cart     = JSON.parse gift.shoppingCart
             post_hsh = { "merchant"  => gift.provider_name, "title" => cart[0]["item_name"], "url" => "#{PUBLIC_URL}/signup/acceptgift?id=#{gift.obscured_id}" }
-            social_proxy = SocialProxy.new(oauth.to_proxy)
-            social_proxy.create_post(post_hsh)
-            puts "------ #{social_proxy.msg}"
+            # social_proxy = SocialProxy.new(oauth.to_proxy)
+            # social_proxy.create_post(post_hsh)
+            # puts "------ #{social_proxy.msg}"
+
+            giver = gift.giver
+            oauth_obj = giver.current_oauth
+            graph = Koala::Facebook::API.new(oauth_obj.token, FACEBOOK_APP_SECRET)
+            post_id_hsh = graph.put_wall_post( "You've Received a Gift!", post_hsh)
+            puts "POSTED TO FACEBOOK WALL CreateGiftNotifySocial#{post_id_hsh}\n"
+
         end
     end
 
