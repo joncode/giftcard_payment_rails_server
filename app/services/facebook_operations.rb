dc107d3d6aa7b36aa6f52bd63744e04a9824e182
Jon Gutwillig <programcodex@gmail.com>
2015-11-10 20:40:04 -0800

    moving the message to name first

--- app/services/facebook_operations.rb
+++ app/services/facebook_operations.rb
@@ -15,7 +15,7 @@ class FacebookOperations
         end
 		post_id_hsh = graph.graph_call("v2.5/me/#{search_query}:send", { tags: "#{gift.facebook_id}",
 			gift: "#{PUBLIC_URL}/signup/acceptgift/#{gift.obscured_id}",
-			message: "#{gift.message} @[#{gift.facebook_id}] #{Time.now}",
+			message: " @[#{gift.facebook_id}], #{gift.message}",
 			privacy: { 'value' => 'EVERYONE'},
 			'fb:explicitly_shared' => 'true'}, 'post')
 		puts "POSTED TO FACEBOOK WALL graph_call #{post_id_hsh}\n"
