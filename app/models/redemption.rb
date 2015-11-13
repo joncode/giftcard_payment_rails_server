85f6e08ea78ee8bfe9485895187ed429e93915dc
Jon Gutwillig <programcodex@gmail.com>
2015-11-12 23:35:38 -0800

    removing validator from qa

--- app/models/redemption.rb
+++ app/models/redemption.rb
@@ -4,7 +4,9 @@ class Redemption < ActiveRecord::Base
 
 #   -------------
 
-    # validates_with RedemptionTotalValueValidator
+	if Rails.env.staging?
+	    validates_with RedemptionTotalValueValidator
+	end
 
 #   -------------
 
