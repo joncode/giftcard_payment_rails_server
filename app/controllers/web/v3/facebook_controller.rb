9f1acd4462ca4b1600d2c73a5c0cd2553bf8aeab
Jon Gutwillig <programcodex@gmail.com>
2015-11-12 18:45:48 -0800

    revmoing non-approved permissions from production

--- app/controllers/web/v3/facebook_controller.rb
+++ app/controllers/web/v3/facebook_controller.rb
@@ -79,7 +79,12 @@ class Web::V3::FacebookController < MetalCorsController
 
     def oauth_init
         oauth = Koala::Facebook::OAuth.new(FACEBOOK_APP_ID, FACEBOOK_APP_SECRET, callback_url_generator(generate_token(params)))
-        redirect_url = oauth.url_for_oauth_code(scope: ['taggable_friends','user_posts','public_profile', 'user_friends', 'email', 'user_birthday', 'publish_actions', 'user_location'])
+        if Rails.env.production?
+            scope_ary = ['public_profile', 'user_friends', 'email', 'user_birthday', 'publish_actions', 'user_location']
+        else
+            scope_ary = ['taggable_friends','user_posts','public_profile', 'user_friends', 'email', 'user_birthday', 'publish_actions', 'user_location']
+        end
+        redirect_url = oauth.url_for_oauth_code(scope: scope_ary )
         # success redirect_url
         # respond(:found)
         redirect_to redirect_url
