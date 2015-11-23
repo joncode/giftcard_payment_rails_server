686e4adc9627fafaaf4456954eddb9fe8c35e7e1
Jon Gutwillig <programcodex@gmail.com>
2015-11-23 15:40:46 -0800

    re-adding merchant trigger and function to this migration stack from MT

--- app/jobs/GiftCreatedEvent.rb
+++ app/jobs/GiftCreatedEvent.rb
@@ -4,8 +4,12 @@ class GiftCreatedEvent
     def self.perform gift_id
     	puts "\n gift #{gift_id} is being GiftCreatedEvent.job\n"
     	gift = Gift.find gift_id
-        FacebookOps.notify_receiver_from_giver(gift)
     	Accountant.merchant(gift)
+    	begin
+	        FacebookOps.notify_receiver_from_giver(gift)
+    	rescue
+
+    	end
     	PointsForSaleJob.perform gift_id
     end
 end
---
+++ db/migrate/20151123233331_merchants_full_text_search.rb
@@ -0,0 +1,56 @@
+class MerchantsFullTextSearch < ActiveRecord::Migration
+  def change
+    # add_column :merchants, :ftmeta, :tsvector
+
+
+    reversible do |dir|
+      dir.up do
+        # Sets the ftmeta column with the tsvector data
+        execute <<-SQL
+          CREATE FUNCTION update_merchants_ftmeta() RETURNS trigger AS $$
+          begin
+            new.ftmeta :=
+              setweight(to_tsvector(cast(new.id as text)), 'A') ||
+              setweight(to_tsvector(concat(new.name)), 'B') ||
+              setweight(to_tsvector(concat(new.phone)), 'B') ||
+              setweight(to_tsvector(concat(new.email)), 'B') ||
+              setweight(to_tsvector(concat(new.address)), 'C') ||
+              setweight(to_tsvector(concat(new.city_name)), 'C') ||
+              setweight(to_tsvector(concat(new.state)), 'C') ||
+              setweight(to_tsvector(concat(new.zip)), 'C');
+            return new;
+          end
+          $$ LANGUAGE plpgsql
+        SQL
+
+        # A trigger to update the ftmeta column on update/insert.
+        execute <<-SQL
+            CREATE TRIGGER update_merchants_ftmeta_trigger
+                    BEFORE INSERT OR UPDATE
+                        ON merchants
+                  FOR EACH ROW
+          EXECUTE PROCEDURE update_merchants_ftmeta()
+        SQL
+
+        # Touch each record so that the trigger creates our new metadata
+        execute <<-SQL
+          UPDATE merchants SET id=id;
+        SQL
+
+        # execute <<-SQL
+          # CREATE INDEX merchants_ftsmeta_idx ON merchants USING gin(ftmeta)
+        # SQL
+
+      end
+      dir.down do
+        execute <<-SQL
+          DROP TRIGGER update_merchants_ftmeta_trigger ON merchants
+        SQL
+
+        execute <<-SQL
+          DROP FUNCTION update_merchants_ftmeta()
+        SQL
+      end
+    end
+  end
+end
