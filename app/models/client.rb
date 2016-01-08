a20f45b61154343d953fbec30b01170812f90a7d
Jon Gutwillig <programcodex@gmail.com>
2016-01-07 17:51:08 -0800

    adding data type enum

--- app/models/client.rb
+++ app/models/client.rb
@@ -16,7 +16,7 @@ class Client < ActiveRecord::Base
 
 	enum ecosystem: [ :full, :client, :partner ]
 	enum platform: [ :ios, :android, :menu_widget, :kiosk, :landing_page, :menu_facebook ]
-
+	enum data_type: [ :merchant, :region, :overview, :menu ]
 
 # return Merchant.find_by_sql("SELECT merchants.* FROM contents , merchants WHERE contents.client_id IS NULL AND contents.partner_type = 'Affiliate' AND contents.partner_id = 29
 	# AND contents.content_type = 'Merchant' AND merchants.id = contents.content_id")
