7ff0606d0b09d062af1519a7ad60e4cb4b2ace91
Jon Gutwillig <programcodex@gmail.com>
2016-01-13 13:05:10 -0800

    removing shortenUrl code

--- app/helpers/shorten_photo_url_helper.rb
+++ app/helpers/shorten_photo_url_helper.rb
@@ -1,15 +1,15 @@
 module ShortenPhotoUrlHelper
 
     def shorten_photo_url photo_url
-        if photo_url.kind_of?(String) && photo_url.include?("http://res.cloudinary.com/drinkboard/image/upload/")
-            photo_url.gsub!("http://res.cloudinary.com/drinkboard/image/upload/", "d|")
-        end
+        # if photo_url.kind_of?(String) && photo_url.include?("http://res.cloudinary.com/drinkboard/image/upload/")
+        #     photo_url.gsub!("http://res.cloudinary.com/drinkboard/image/upload/", "d|")
+        # end
         return photo_url
     end
 
     def unshorten_photo_url photo_url
         if photo_url.kind_of?(String) && photo_url.include?("d|")
-            photo_url.gsub!("d|", "http://res.cloudinary.com/drinkboard/image/upload/")
+            photo_url.gsub!("d|", "https://res.cloudinary.com/drinkboard/image/upload/")
         end
         return photo_url
     end
