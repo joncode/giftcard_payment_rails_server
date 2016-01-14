c2792a62c0fc5935420cbbeee9b5212db65c72a0
Jon Gutwillig <programcodex@gmail.com>
2016-01-13 17:20:44 -0800

    adding basic auth good passwords

--- app/controllers/react_controller.rb
+++ app/controllers/react_controller.rb
@@ -1,6 +1,6 @@
 class ReactController < ApplicationController
 
-	http_basic_authenticate_with name: "dhh", password: "secret", only: :index
+	http_basic_authenticate_with name: "developer", password: "gifted", only: :index
 
 	def  index
 		puts "Developer Tools #index"
