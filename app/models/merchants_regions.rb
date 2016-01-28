c63d4b456ab7bd9c1bd8ac6ace2dd844c00452a9
Jon Gutwillig <programcodex@gmail.com>
2016-01-28 14:23:37 -0800

    adding merchants regions list with the appropriate cache busting

--- app/models/merchants_regions.rb
+++ app/models/merchants_regions.rb
@@ -1,4 +1,13 @@
 class MerchantsRegions < ActiveRecord::Base
 
+	def self.create(merchant_id: m_id, region_id: r_id)
+		resp = super(merchant_id: merchant_id, region_id: region_id)
+		if !resp.errors.nil?
+			RedisWrap.clear_merchants_caches(region_id)
+			WwwHttpService.clear_merchant_cache
+		end
+		resp
+	end
+
 
 end
