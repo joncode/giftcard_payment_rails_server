3cc0cfc235813e4794206556ffd4fd2fb03b123e
Jon Gutwillig <programcodex@gmail.com>
2016-01-28 14:56:38 -0800

    do not build caches with []

--- app/services/redis_cache_control.rb
+++ app/services/redis_cache_control.rb
@@ -17,7 +17,7 @@ class RedisCacheControl
 		def rebuild_menus
 			Merchant.all.each do |merchant|
 	            menu_responses = { "menu" =>  merchant.menu_string, "loc_id" => merchant.id }
-	            RedisWrap.set_menu(merchant.menu_id, menu_responses)
+	            RedisWrap.set_menu(merchant.menu_id, menu_responses) unless (menu_responses == [])
 	        end
 		end
 
@@ -26,7 +26,7 @@ class RedisCacheControl
 	            arg_scope = proc { Merchant.where(active: true).where(paused: false).order("name ASC") }
 	            merchants = client.contents(:merchants, &arg_scope)
 	            merchants_serialized = merchants.serialize_objs(:web)
-	            RedisWrap.set_merchants(client.id, merchants_serialized)
+	            RedisWrap.set_merchants(client.id, merchants_serialized) unless (merchants_serialized == [])
 	        end
 		end
 
@@ -35,7 +35,7 @@ class RedisCacheControl
 			Client.all.each do |client|
 	            arg_scope = proc { Region.index }
 		        cities_serialized = client.contents(:regions, &arg_scope).map(&:old_city_json)
-	            RedisWrap.set_cities(client.id, cities_serialized)
+	            RedisWrap.set_cities(client.id, cities_serialized) unless (cities_serialized == [])
 	        end
 		end
 
@@ -49,7 +49,7 @@ class RedisCacheControl
 	                    	merchants = merchants.select{ |m| m.city_id == region_id }
 	                	end
 	                	merchants_serialized = merchants.serialize_objs(:web)
-		                RedisWrap.set_region_merchants(client.id, region_id, merchants_serialized)
+		                RedisWrap.set_region_merchants(client.id, region_id, merchants_serialized) unless (merchants_serialized == [])
 		            end
 		        end
 			end
