ba11f57a4e89bdd7fd7c80775e7c6adf9927688e
Jon Gutwillig <programcodex@gmail.com>
2016-02-24 16:57:03 -0800

    removing symbols and going to strings

--- app/jobs/PushJob.rb
+++ app/jobs/PushJob.rb
@@ -3,11 +3,11 @@ class PushJob
 
     @queue = :push
 
-    def self.perform gift_id, push_type=:gift_receiver_notification
+    def self.perform gift_id, push_type='gift_receiver_notification'
         gift = Gift.find gift_id
 
         case push_type
-        when :gift_receiver_notification
+        when 'gift_receiver_notification'
                 # notify receiver of gift push
                 # to gift receiver -> 'you have received a gift'
             push_receiver = gift.receiver
@@ -18,19 +18,19 @@ class PushJob
                 alert = "#{gift.giver_name} sent you a gift at #{gift.provider_name}!"
             end
 
-        when :gift_receiver_created_account
+        when 'gift_receiver_created_account'
                 # to gift giver -> 'gift is received - receiver created an account'
             push_receiver = gift.giver
 
             alert = "Thank You! #{gift.receiver_name} got the app and your gift!"
 
-        when :gift_received_thank_you
+        when 'gift_received_thank_you'
                 # to gift giver -> 'user (push_receiver) has looked at their gift'
             push_receiver = gift.giver
 
             alert = "#{gift.receiver_name} opened your gift at #{gift.provider_name}!"
 
-        when :gift_delivered
+        when 'gift_delivered'
             push_receiver = gift.giver
 
             alert = "Your gift has been delivered to #{gift.receiver_name} at #{gift.provider_name}!"
--- app/services/relay.rb
+++ app/services/relay.rb
@@ -6,7 +6,7 @@ class Relay
 
 			if gift.receiver_id
                 puts "\nNotify Receiver via Push #{gift.receiver_name}"
-				Resque.enqueue(PushJob, gift.id, :gift_receiver_notification)
+				Resque.enqueue(PushJob, gift.id, 'gift_receiver_notification')
 			end
 
 		end
@@ -17,17 +17,17 @@ class Relay
 
         def send_push_thank_you gift
             if gift.notified_at >= (DateTime.now.utc - 1.minute)
-                Resque.enqueue(PushJob, gift.id, :gift_received_thank_you)
+                Resque.enqueue(PushJob, gift.id, 'gift_received_thank_you')
             end
         end
 
         def send_gift_delivered gift
-            Resque.enqueue(PushJob, gift.id, :gift_delivered)
+            Resque.enqueue(PushJob, gift.id, 'gift_delivered')
         end
 
         def send_push_incomplete gift
             if gift.giver_type == "User"
-                Resque.enqueue(PushJob, gift.id, :gift_receiver_created_account)
+                Resque.enqueue(PushJob, gift.id, 'gift_receiver_created_account')
                 # email_gift_collected(g)
             end
         end
