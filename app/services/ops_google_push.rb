9dedbaf9a4a9cfe4ec40f4b278c3b5cd9514c2df
Jon Gutwillig <programcodex@gmail.com>
2016-05-26 21:57:20 -0700

    adding multiple canonical push tokens support for GCM

--- app/services/ops_google_push.rb
+++ app/services/ops_google_push.rb
@@ -4,8 +4,19 @@ class OpsGooglePush
 
 	class << self
 
-		def send_push pn_token, alert
-			gcm = GCM.new(GCM_API_KEY)
+		def send_push pn_token_or_array, alert
+			unless pn_token_or_array.kind_of?(Array)
+				pn_token_or_array = [pn_token_or_array]
+			end
+
+			registration_ids = []
+			pn_token_or_array.each do |pn_token|
+				if pn_token.canonical_id.present?
+					registration_ids << pn_token.canonical_id
+				else
+					registration_ids << pn_token.pn_token
+				end
+			end
 
 			if alert.kind_of?(String)
 				msg =  { data: { message: alert, title: 'ItsOnMe App' } }
@@ -13,22 +24,21 @@ class OpsGooglePush
 				msg = { data: alert }
 			end
 
-			if pn_token.canonical_id.present?
-				registration_ids = [pn_token.canonical_id]
-			else
-				registration_ids = [pn_token.pn_token] # an array of one or more client registration tokens
-			end
+			gcm = GCM.new(GCM_API_KEY)
 			r = gcm.send(registration_ids, msg)
 			puts "SENDING PUSH GCM #{r.inspect}"
-			update_canonical_id r, pn_token
+			update_canonical_id r, pn_token_or_array
 			r
 		end
 
-
-		def update_canonical_id r, pn_token
-			if r[:canonical_ids][0].present? && (r[:canonical_ids][0][:new] != pn_token.pn_token)
-				if pn_token.canonical_id.nil?
-					pn_token.update(canonical_id: r[:canonical_ids][0][:new])
+		def update_canonical_id r, pn_tokens
+			r[:canonical_ids].each do |canon_hsh|
+				pn_tokens.each do |pn_token|
+					if pn_token.canonical_id.nil?
+						if canon_hsh[:old] == pn_token.pn_token
+							pn_token.update(canonical_id: canon_hsh[:new])
+						end
+					end
 				end
 			end
 		end
--- app/services/urban_airship_wrap.rb
+++ app/services/urban_airship_wrap.rb
@@ -1,24 +1,24 @@
+'APA91bGrDojis1jdvoC2frtAbmet44V-IQnvsM86FAtjRS5yEHBZDv0AOtIEXlrpHz8-pEex30CGeGyuXgXlssqCHbCXuO1HG9IX0b6LXZ0fYaUOrmmEJzJ36kR2Ha2qNDGhz_W3YaW1'
+
 module UrbanAirshipWrap
 
     def send_push(user, alert, gift_id)
         puts "SENDING PUSH NOTE for GIFT ID = #{gift_id} | PUSH_RECEIVER ID = #{user.id} | #{alert}"
         pnts = user.pn_tokens
         resp = []
+        googe_push_tokens = []
         pnts.each do |pn_token_obj|
             begin
                 if pn_token_obj.platform == 'ios'
-                    push = UA_CLIENT.create_push
-                    push.audience = UA.device_token(pn_token_obj.pn_token)
-                    push.notification = UA.notification(alert: alert)
-                    push.device_types = UA.all
-                    r = push.send_push
+                    r = send_push_with_urban_airship(pn_token_obj, alert)
                 elsif pn_token_obj.platform == 'android'
                     # is the token for the new APP ?
-                    r = OpsGooglePush.send_push(pn_token_obj, { message: alert,
-                            title: 'New ItsOnMe!',
-                            action: 'VIEW_GIFT',
-                            args: { gift_id: gift_id }
-                        })
+                    if pn_token_obj.pn_token.length > 40
+                        # new google push
+                        googe_push_tokens << pn_token_obj
+                    else
+                        r = send_push_with_urban_airship(pn_token_obj, alert)
+                    end
                 end
                 puts "PUSH SUCCEEDED |#{pn_token_obj.id}| - #{r.inspect}"
                 resp << r
@@ -27,8 +27,30 @@ module UrbanAirshipWrap
             end
         end
 
+        if googe_push_tokens.count > 0
+            r = OpsGooglePush.send_push(googe_push_tokens, { message: alert,
+                    title: 'New ItsOnMe!',
+                    action: 'VIEW_GIFT',
+                    args: { gift_id: gift_id }
+                })
+            puts "PUSH SUCCEEDED |#{pn_token_obj.id}| - #{r.inspect}"
+            resp << r
+        end
+
         puts "APNS push sent via TOKENS! #{resp.inspect}"
         Ditto.send_push_create(resp[0], gift_id, 'Gift')
+    end
+
+    def send_push_with_urban_airship(pn_token_obj, alert)
+        push = UA_CLIENT.create_push
+        if pn_token_obj.platform == 'ios'
+            push.audience = UA.device_token(pn_token_obj.pn_token)
+        else
+            push.audience = UA.apid(pn_token_obj.pn_token)
+        end
+        push.notification = UA.notification(alert: alert)
+        push.device_types = UA.all
+        push.send_push
     end
 
     def ua_register pn_token, user_alias, user_id, platform=nil
