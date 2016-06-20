935bb89f0e603afd8b2a31f67517d6036a10755a
Jon Gutwillig <programcodex@gmail.com>
2016-06-20 14:03:21 -0700

    adding a skip for bad tokens

--- app/jobs/FacebookCronJon.rb
+++ app/jobs/FacebookCronJon.rb
@@ -5,6 +5,7 @@ class FacebookCronJob
 		os = Oauth.all
 		facebook_oauth ||= Koala::Facebook::OAuth.new(FACEBOOK_APP_ID, FACEBOOK_APP_SECRET)
 		os.each do |o|
+			good = true
 			begin
 				graph = Koala::Facebook::API.new(o.token, FACEBOOK_APP_SECRET)
 				p = graph.get_object("me")
@@ -15,10 +16,11 @@ class FacebookCronJob
 					p = graph.get_object("me")
 				rescue
 					puts "TOKEN IS BROKEN #{o.id}"
+					good = false
 				end
 			end
 			puts p.inspect
-			resp = facebook_oauth.exchange_access_token_info o.token
+			resp = facebook_oauth.exchange_access_token_info o.token if good
 			puts resp.inspect
 		end
 	end
