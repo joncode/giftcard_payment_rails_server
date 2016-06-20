bb3369c4a5ac74e40118888fafbd402bc582c797
Jon Gutwillig <programcodex@gmail.com>
2016-06-20 13:56:50 -0700

    adding the rescue for wrong app id stuff

--- app/jobs/FacebookCronJon.rb
+++ app/jobs/FacebookCronJon.rb
@@ -1,11 +1,16 @@
 class FacebookCronJob
 
 
-	def check_tokens
+	def self.check_tokens
 		os = Oauth.all
 		facebook_oauth ||= Koala::Facebook::OAuth.new(FACEBOOK_APP_ID, FACEBOOK_APP_SECRET)
 		os.each do |o|
-			graph = Koala::Facebook::API.new(o.token, FACEBOOK_APP_SECRET)
+			begin
+				graph = Koala::Facebook::API.new(o.token, FACEBOOK_APP_SECRET)
+			rescue
+				puts "NO SECRET #{o.id}"
+				graph = Koala::Facebook::API.new(o.token)
+			end
 			p = graph.get_object("me")
 			puts p.inspect
 			resp = facebook_oauth.exchange_access_token_info o.token
