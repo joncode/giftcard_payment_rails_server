e8fd928100a2ea113acdb8d51069b059da467136
Jon Gutwillig <programcodex@gmail.com>
2016-06-20 14:01:30 -0700

    adding log to bad tokens

--- app/jobs/FacebookCronJon.rb
+++ app/jobs/FacebookCronJon.rb
@@ -9,9 +9,13 @@ class FacebookCronJob
 				graph = Koala::Facebook::API.new(o.token, FACEBOOK_APP_SECRET)
 				p = graph.get_object("me")
 			rescue
-				puts "NO SECRET #{o.id}"
-				graph = Koala::Facebook::API.new(o.token)
-				p = graph.get_object("me")
+				begin
+					puts "NO SECRET #{o.id}"
+					graph = Koala::Facebook::API.new(o.token)
+					p = graph.get_object("me")
+				rescue
+					puts "TOKEN IS BROKEN #{o.id}"
+				end
 			end
 			puts p.inspect
 			resp = facebook_oauth.exchange_access_token_info o.token
