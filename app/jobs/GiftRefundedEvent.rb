3ae5e16f2768d826c767d4224f5eab8f5cdb590c
Jon Gutwillig <programcodex@gmail.com>
2016-07-03 13:39:35 -0700

    status

--- app/jobs/GiftRefundedEvent.rb
+++ app/jobs/GiftRefundedEvent.rb
@@ -5,13 +5,13 @@ class GiftRefundedEvent
     	puts "\n gift #{gift_id} is in GiftRefundedEvent.job\n"
     	gift = Gift.find gift_id
 
-    	if gift.status == 'refund_live'
+    	if gift.status == 'refund_cancel'
 	    	# if gift is refund_cancel
 	    		# refund the gift registers
 	    	registers = gift.registers
 			registers.each {|r| r.reverse_charge }
 
-    	elsif gift.status == 'refund_cancel'
+    	elsif gift.status == 'refund_live'
 	    	# if gift is refund_live
 	    		# leave the registers
 
