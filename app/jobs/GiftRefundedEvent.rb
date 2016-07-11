d8fbf8057c26cd4e7b98dabc7288d96668e56789
Jon Gutwillig <programcodex@gmail.com>
2016-07-11 15:46:28 -0700

    adding fixeds to refund to find the zero amount bug

--- app/jobs/GiftRefundedEvent.rb
+++ app/jobs/GiftRefundedEvent.rb
@@ -3,13 +3,16 @@ class GiftRefundedEvent
 
     def self.perform gift_id
     	puts "\n gift #{gift_id} is in GiftRefundedEvent.job\n"
-    	gift = Gift.find gift_id
+    	gift = Gift.unscoped.find gift_id
 
     	if gift.pay_stat == 'refund_cancel'
 	    	# if gift is refund_cancel
 	    		# refund the gift registers
 	    	registers = gift.registers
-			registers.each {|r| r.reverse_charge }
+			registers.each do |r|
+                reg = r.reverse_charge
+                puts "Here is the register #{reg.inspect}"
+            end
 
     	elsif gift.pay_stat == 'refund_comp'
 	    	# if gift is refund_live
