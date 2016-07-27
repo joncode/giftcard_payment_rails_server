bdf1608c07b99ba0656120813ac39b780940b808
Jon Gutwillig <programcodex@gmail.com>
2016-07-26 20:38:23 -0700

    adding states constants

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -121,5 +121,76 @@ GIFT_CAT = { 100 => "Admin", 101 => "AdmRegift", 107 => "AdmBoom", 150 => "AdmCa
 
 REDEMPTION_HSH = {1 => "V1" , 2 => "V2",  3 => "Pos", 4 => 'Paper' }
 
+# LOCATION DATA
+
+CANADIAN_HSH = {
+    "AB" => "Alberta",
+    "BC" => "British Columbia",
+    "MB" => "Manitoba",
+    "NB" => "New Burnswick",
+    "NL" => "Newfoundland and Labrador",
+    "NS" => "Nova Scotia",
+    "NT" => "Northwest Territories",
+    "NU" => "Nunavut",
+    "ON" => "Ontario",
+    "PE" => "Prince Edward Island",
+    "QC" => "Quebec",
+    "SK" => "Saskatchewan",
+    "YT" => "Yukon"
+}
+
+USA_HSH = {
+    "AL" => "Alabama",
+    "AK" => "Alaska",
+    "AZ" => "Arizona",
+    "AR" => "Arkansas",
+    "CA" => "California",
+    "CO" => "Colorado",
+    "CT" => "Connecticut",
+    "DE" => "Delaware",
+    "DC" => "District of Columbia",
+    "FL" => "Florida",
+    "GA" => "Georgia",
+    "HI" => "Hawaii",
+    "ID" => "Idaho",
+    "IL" => "Illinois",
+    "IN" => "Indiana",
+    "IA" => "Iowa",
+    "KS" => "Kansas",
+    "KY" => "Kentucky",
+    "LA" => "Louisiana",
+    "ME" => "Maine",
+    "MD" => "Maryland",
+    "MA" => "Massachusetts",
+    "MI" => "Michigan",
+    "MN" => "Minnesota",
+    "MS" => "Mississippi",
+    "MO" => "Missouri",
+    "MT" => "Montana",
+    "NE" => "Nebraska",
+    "NV" => "Nevada",
+    "NH" => "New Hampshire",
+    "NJ" => "New Jersey",
+    "NM" => "New Mexico",
+    "NY" => "New York",
+    "NC" => "North Carolina",
+    "ND" => "North Dakota",
+    "OH" => "Ohio",
+    "OK" => "Oklahoma",
+    "OR" => "Oregon",
+    "PA" => "Pennsylvania",
+    "RI" => "Rhode Island",
+    "SC" => "South Carolina",
+    "SD" => "South Dakota",
+    "TN" => "Tennessee",
+    "TX" => "Texas",
+    "UT" => "Utah",
+    "VT" => "Vermont",
+    "VA" => "Virginia",
+    "WA" => "Washington",
+    "WV" => "West Virginia",
+    "WI" => "Wisconsin",
+    "WY" => "Wyoming"
+}
 
 
