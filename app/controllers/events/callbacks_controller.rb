6c224b39155935c85d8217cd5c3f66eecfeda5c9
Jon Gutwillig <programcodex@gmail.com>
2016-07-28 17:17:40 -0700

    moving to oldschoool way

--- app/controllers/events/callbacks_controller.rb
+++ app/controllers/events/callbacks_controller.rb
@@ -1,5 +1,5 @@
 class Events::CallbacksController < MetalCorsController
-	respond_to :json
+	# respond_to :json
 	include MtSmsRedeem
 
 	def receive_sms
@@ -10,11 +10,11 @@ class Events::CallbacksController < MetalCorsController
 
 		dispatch_message(from_number, msg, params)
 
-		respond_with :ok
+		# respond_with :ok
 
-		# respond_to do |format|
-	 #     	format.json
-	 #    end
+		respond_to do |format|
+	     	format.json
+	    end
 	end
 
 	def dispatch_message from, msg, req
