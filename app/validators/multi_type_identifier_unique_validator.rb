a5ada1c0a5d4b36338dd8e62e1293ef937095da8
Jon Gutwillig <programcodex@gmail.com>
2016-08-11 18:42:12 -0700

    making 1 validation failed msg enough

--- app/validators/multi_type_identifier_unique_validator.rb
+++ app/validators/multi_type_identifier_unique_validator.rb
@@ -6,7 +6,8 @@ class MultiTypeIdentifierUniqueValidator < ActiveModel::Validator
 			UserSocial.where(identifier: record.identifier.to_s).each do |us|
 				if us.type_of == record_type && us.id != record.id
 					puts "not passing user social validations because #{us.inspect} == #{record.inspect}"
-					return record.errors[record_type.to_sym] << "#{record.identifier} already has an account. Use that account or email support@itson.me for help."
+					record.errors[record_type.to_sym] << "#{record.identifier} already has an account. Use that account or email support@itson.me for help."
+					break
 				end
 			end
 		end
