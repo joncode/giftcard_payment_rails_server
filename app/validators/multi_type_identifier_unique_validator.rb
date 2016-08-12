ecd9659dd70539c26ddb566194ffdfbff7bd4a92
Jon Gutwillig <programcodex@gmail.com>
2016-08-11 18:41:26 -0700

    fixing the user social validation msg

--- app/validators/multi_type_identifier_unique_validator.rb
+++ app/validators/multi_type_identifier_unique_validator.rb
@@ -6,7 +6,7 @@ class MultiTypeIdentifierUniqueValidator < ActiveModel::Validator
 			UserSocial.where(identifier: record.identifier.to_s).each do |us|
 				if us.type_of == record_type && us.id != record.id
 					puts "not passing user social validations because #{us.inspect} == #{record.inspect}"
-					return record.errors[record_type.to_sym] << "is already in use. Please email support@itson.me for assistance if this is in error"
+					return record.errors[record_type.to_sym] << "#{record.identifier} already has an account. Use that account or email support@itson.me for help."
 				end
 			end
 		end
