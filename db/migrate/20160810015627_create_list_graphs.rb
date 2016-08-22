2bc5fa4ccc03f5538801eb85d6c41eccf104d0c5
Jon Gutwillig <programcodex@gmail.com>
2016-08-22 12:48:47 -0700

    changing list target to list item

--- db/migrate/20160810015627_create_list_graphs.rb
+++ db/migrate/20160810015627_create_list_graphs.rb
@@ -2,8 +2,8 @@ class CreateListGraphs < ActiveRecord::Migration
   def change
     create_table :list_graphs do |t|
       t.integer :list_id
-      t.integer :target_id
-      t.string :target_type
+      t.integer :item_id
+      t.string :item_type
       t.integer :position
       t.boolean :active, default: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160810015639) do
+ActiveRecord::Schema.define(version: 20160801051819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -576,16 +576,6 @@ ActiveRecord::Schema.define(version: 20160810015639) do
 
   add_index "landing_pages", ["link"], name: "index_landing_pages_on_link", using: :btree
 
-  create_table "list_graphs", force: :cascade do |t|
-    t.integer  "list_id"
-    t.integer  "target_id"
-    t.string   "target_type"
-    t.integer  "position"
-    t.boolean  "active",      default: true
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-  end
-
   create_table "list_items", force: :cascade do |t|
     t.integer  "owner_id"
     t.string   "owner_type"
@@ -602,23 +592,6 @@ ActiveRecord::Schema.define(version: 20160810015639) do
     t.integer  "position",   default: 0
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
-  end
-
-  create_table "lists", force: :cascade do |t|
-    t.string   "owner_type"
-    t.integer  "owner_id"
-    t.string   "token"
-    t.boolean  "active",      default: true
-    t.string   "template"
-    t.string   "name"
-    t.string   "zinger"
-    t.text     "detail"
-    t.string   "photo"
-    t.string   "logo"
-    t.integer  "total_items"
-    t.string   "item_types"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
   end
 
   create_table "menu_items", force: :cascade do |t|
