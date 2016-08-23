32028933c6b909e28fe64765884a586ca63327db
Jon Gutwillig <programcodex@gmail.com>
2016-08-23 14:11:06 -0700

    migration wil automatically make the menu item tokens

--- db/migrate/20160823040305_add_token_to_menu_items.rb
+++ db/migrate/20160823040305_add_token_to_menu_items.rb
@@ -1,5 +1,19 @@
 class AddTokenToMenuItems < ActiveRecord::Migration
-  def change
-  	add_column :menu_items, :token, :string
-  end
+	def up
+	  	add_column :menu_items, :token, :string
+	  	generate_tokens
+	end
+
+	def down
+		remove_column :menu_items, :token
+	end
+
+	def generate_tokens
+        MenuItem.find_in_batches do |group|
+            group.each do |item|
+            	item.save
+            end
+        end
+	end
+
 end
