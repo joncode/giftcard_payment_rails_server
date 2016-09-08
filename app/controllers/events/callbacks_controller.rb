240f47962ea58f83f5fa7cc1dd0028cc8fff8b5e
Jon Gutwillig <programcodex@gmail.com>
2016-09-08 02:17:03 -0700

    adding the weere here to help customer message

--- app/controllers/events/callbacks_controller.rb
+++ app/controllers/events/callbacks_controller.rb
@@ -15,7 +15,7 @@ class Events::CallbacksController < MetalCorsController
 
 		dispatch_message(from_number, params, msg, from_number)
 
-		@app_response = "You've reached ItsOnMe!  One of our people will be texting you shortly.  Thank you for your patience :)"
+		@app_response = "You've reached ItsOnMe!  One of our people will be texting you shortly.  Thank you, we're here to help :)"
 		respond
 	end
 
