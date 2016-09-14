b4cb05c076022b6d0df03a8335317b32561167e3
Jon Gutwillig <programcodex@gmail.com>
2016-09-13 20:18:29 -0700

    new signature for redis

--- app/jobs/GiftNotificationJob.rb
+++ app/jobs/GiftNotificationJob.rb
@@ -2,19 +2,19 @@ class GiftNotificationJob
 
 	@queue = :push
 
-    def self.perform args
-        puts "\nGiftNotificationJob - perform- #{args.inspect}\n"
-    	return if (args[:gift_id].to_i == 0)
-        return if !(['invoice', 'receiver'].include?(args[:type]))
-    	gift = Gift.find args[:gift_id]
-    	note_type = args[:type]
+    def self.perform alert_type, gift_id
+        puts "GiftNotificationJob -#{alert_type.inspect}- #{gift_id}"
 
-    	if note_type == 'invoice'
+    	gift = Gift.find gift_id
+
+    	if alert_type == 'invoice'
     		gift.invoice_giver
-    	elsif note_type == 'receiver'
+    	elsif alert_type == 'receiver'
     		gift.notify_via_text
     		gift.send_receiver_notification
-    	end
+    	else
+            puts "500 Internal - BAD ALERT TYPE RECEIVED - #{alert_type} - GiftNotificationJob"
+        end
     end
 
 
