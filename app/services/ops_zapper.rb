ef610db18a0b80215665f941ab602d78cc9d8a09
Jon Gutwillig <programcodex@gmail.com>
2016-09-19 12:22:22 -0700

    adding log level to show params

--- app/services/ops_zapper.rb
+++ app/services/ops_zapper.rb
@@ -69,13 +69,13 @@ class OpsZapper
 		response_from_code
 	end
 
+#   -------------
+
 	def self.strip_qr_code qr_code
 		x = Base64.encode64(qr_code)
 		x.chomp! if (x[-1..-1] == "\n")
 		x
 	end
-
-#   -------------
 
 	def self.make_request_hsh gift, qr_code, value, redemption_id=nil
 		{
