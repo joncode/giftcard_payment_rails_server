9aec9df7416a0d4262a16e5d29f8cac077f6ebf9
Jon Gutwillig <programcodex@gmail.com>
2016-10-10 14:28:22 -0700

    adding this tho it needs to be changed in psql

--- db/migrate/20160918222802_add_status_to_redemptions.rb
+++ db/migrate/20160918222802_add_status_to_redemptions.rb
@@ -1,5 +1,5 @@
 class AddStatusToRedemptions < ActiveRecord::Migration
   def change
-  	add_column :redemptions, :status, :string, default: 'done'
+  	add_column :redemptions, :status, :string, default: 'pending'
   end
 end
