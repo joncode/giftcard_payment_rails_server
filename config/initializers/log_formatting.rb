c4a2e2dc9b9e41e4d449e66f3d5abf58e69404e8
Jon Gutwillig <programcodex@gmail.com>
2016-10-13 16:34:15 -0700

    adding uuid into logs

--- config/initializers/log_formatting.rb
+++ config/initializers/log_formatting.rb
@@ -23,6 +23,8 @@ class Logger::Formatter
 		formatted_time = time.strftime("%Y-%m-%d %H:%M:%S.") << time.usec.to_s[0..2].rjust(3)
 		color = SEVERITY_TO_COLOR_MAP[severity]
 
-		"\033[0;37m#{formatted_time}\033[0m [\033[#{color}m#{formatted_severity}\033[0m] #{msg.strip} (pid:#{$$})\n"
+		request_id = env['HTTP_X_REQUEST_ID'] || 'NRI'
+
+		"\033[0;37m#{formatted_time}\033[0m [\033[#{color}m|#{request_id}|pid:#{$$}|#{formatted_severity}\033[0m] #{msg.strip} (pid:#{$$})\n"
 	end
 end
