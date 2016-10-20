e14d8cbd6c960f029b0b6adb20f0f08be5b91822
Jon Gutwillig <programcodex@gmail.com>
2016-10-20 16:22:47 -0700

    rolling back invite link for now

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -101,8 +101,8 @@ class Gift < ActiveRecord::Base
 #   -------------
 
     def invite_link
-        # "#{PUBLIC_URL}/signup/acceptgift?id=#{self.obscured_id}"
-        "#{PUBLIC_URL}/hi/#{self.hex_id}"
+        "#{PUBLIC_URL}/signup/acceptgift?id=#{self.obscured_id}"
+        # "#{PUBLIC_URL}/hi/#{self.hex_id}"
     end
 
     def paper_id
