894d7b9b81305be0d25e4cab4d2ef5c542d9f4d3
Jon Gutwillig <programcodex@gmail.com>
2016-10-20 23:19:57 -0700

    adding user push job onto queue proper via ID

--- app/jobs/PushUserJob.rb
+++ app/jobs/PushUserJob.rb
@@ -3,7 +3,13 @@ class PushUserJob
 
     @queue = :push
 
-    def self.perform user, alert
+    def self.perform user_or_id, alert
+
+    	if user_or_id.kind_of?(User)
+    		user = user_or_id
+    	else
+    		user = User.find user_or_id
+    	end
 
         puts "SENDING AT PUSH NOTE for USER ID = #{user.id} | #{alert}"
         ditto = send_push(user, alert)
