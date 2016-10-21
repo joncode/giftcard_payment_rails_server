440c332e6889ce3fe355deddc4f13fe2142fe78d
Jon Gutwillig <programcodex@gmail.com>
2016-10-20 19:27:38 -0700

    trying a format payload basic

--- app/services/ops_push_apple.rb
+++ app/services/ops_push_apple.rb
@@ -6,13 +6,14 @@ class OpsPushApple
 
 	        puts "SEND APNS push |#{pnt.id}| - #{alert}"
 
-	        # payload = format_payload(alert, gift_id)
-	        n = APNS::Notification.new(pnt.pn_token, alert: alert)
+	        payload = format_payload(alert, gift_id)
+	        n = APNS::Notification.new(pnt.pn_token, payload)
 	        APNS.send_notifications([n])
 
 		end
 
 		def format_payload alert, gift_id
+			return { alert: alert }
 	        if alert.to_s.match(/has been delivered/)
 	            payload = { body: alert,
 	                    title: 'ItsOnMe Gift Delivered!',
