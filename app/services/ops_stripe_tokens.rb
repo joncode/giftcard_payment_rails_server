5f7eb825d21d1503d18203a91be14e74a3072b0b
Jon Gutwillig <programcodex@gmail.com>
2016-11-28 17:55:53 -0500

    adding strip token card creator

---
+++ app/services/ops_stripe_tokens.rb
@@ -0,0 +1,37 @@
+require 'stripe'
+
+class OpsStripeCard
+	include MoneyHelper
+	include OpsStripeHelper
+
+	attr_reader :response, :success, :request_id, :error, :error_message, :error_code, :error_key,
+		:http_status, :customer_name, :token, :card_id, :ccy
+
+	def initialize args
+		Stripe.api_key = STRIPE_SECRET
+		@token = args['stripe_id']
+		@customer_id = args['stripe_user_id']
+		@customer_name = args['origin']
+		@card_id = args['id']
+		@amount = args['amount'].to_i || 0
+		@ccy = args['ccy']
+	end
+
+	def create_subscription
+		return nil if @token.nil? || @customer_name.nil? || @card_id.nil?
+		@response = Stripe::Customer.create(
+			:source => @token,
+			:description => "#{@customer_name} - #{@card_id}"
+		)
+	end
+
+	def charge_token
+		return nil if @customer_id.nil? || @ccy.nil? || @amount == 0
+		@response = Stripe::Charge.create(
+			:amount   => @amount,
+			:currency => @ccy,
+			:customer => @customer_id
+		)
+	end
+
+end
