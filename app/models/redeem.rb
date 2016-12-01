759f1381d21bbf4c108041894bd9c054cf86b258
Jon Gutwillig <programcodex@gmail.com>
2016-11-30 23:42:38 -0400

    removing the available feature of startign a new redemption

--- app/models/redeem.rb
+++ app/models/redeem.rb
@@ -440,9 +440,9 @@ class Redeem
 		if amount > value_amt
 			return { 'success' => false, "response_code" => "NOT_REDEEMABLE",
 				"response_text" => "The amount (#{display_money(cents: amount, ccy: gift.ccy)}) received is more than the remaining balance on the gift of #{ display_money(cents: value_amt, ccy: gift.ccy) }" }
-		elsif amount > available_amt
-			return { 'success' => false, "response_code" => "NOT_REDEEMABLE",
-				"response_text" => "Due to pending redemptions, the amount you entered is more than current available balance #{display_money(cents: available_amt, ccy: gift.ccy)} " }
+		# elsif amount > available_amt
+		# 	return { 'success' => false, "response_code" => "NOT_REDEEMABLE",
+		# 		"response_text" => "Due to pending redemptions, the amount you entered is more than current available balance #{display_money(cents: available_amt, ccy: gift.ccy)} " }
 		end
 
 		  # -------------
