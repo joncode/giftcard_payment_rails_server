6961bc395fffbe8c5d4813c64dd486f0b49822f6
Jon Gutwillig <programcodex@gmail.com>
2016-12-07 13:20:18 -0800

    adding the commented out migration

--- db/migrate/20161005004404_add_start_req_to_redemptions.rb
+++ db/migrate/20161005004404_add_start_req_to_redemptions.rb
@@ -1,8 +1,8 @@
 class AddStartReqToRedemptions < ActiveRecord::Migration
   def change
-  	add_column :redemptions, :start_req, :json
-  	add_column :redemptions, :start_res, :json
-  	add_column :redemptions, :request_at, :datetime
-  	add_column :redemptions, :response_at, :datetime
+  	# add_column :redemptions, :start_req, :json
+  	# add_column :redemptions, :start_res, :json
+  	# add_column :redemptions, :request_at, :datetime
+  	# add_column :redemptions, :response_at, :datetime
   end
 end
