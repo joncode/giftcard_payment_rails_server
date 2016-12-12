4094f1c8ca6e63c50aa3df79344382f2d77b232c
Jon Gutwillig <programcodex@gmail.com>
2016-12-12 14:42:07 -0800

    loading all the rails folders inside :app not just :jobs

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,4 +1,4 @@
-Dir[File.join(Rails.root, 'app', 'jobs', '*.rb')].each { |file| require file }
+Dir[File.join(Rails.root, 'app', '*', '*.rb')].each { |file| require file }
 
 if Rails.env.production?
 	ENV["REDISTOGO_URL"] ||= "redis://redistogo:973cdc884e18482f5b324194e3b4cde1@barb.redistogo.com:9497/"
