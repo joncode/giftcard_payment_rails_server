ecbd80c55161f1a847112d754f4be7adaa5152d3
Jon Gutwillig <programcodex@gmail.com>
2016-12-21 14:45:41 -0800

    putting up some mock data

--- app/controllers/web/v3/clover_controller.rb
+++ app/controllers/web/v3/clover_controller.rb
@@ -2,11 +2,24 @@ class Web::V3::CloverController < MetalCorsController
 
 	before_action :authentication_no_token
 
-	def redeem
+	def initialize
+		success({
+				status: 1,
+				code: 'INITIALIZED'
+				support_phone_number: '1-310-235-3835',
+				application_key: '9ih2ihf2i03h0i2jd23ijd20idje2fw1ihf1i',
+				message: 'ItsOnMe App Initialized - ready to redeem gift cards!'
+			})
 		respond
 	end
 
-	def initialize
+	def redeem
+		success({
+					applied_amount: 0,
+					code: "NOT_FOUND",
+					transaction_reference: 'rd_ccd31145',
+					message: 'Gift not found for ID RD-8482-AD45'
+				})
 		respond
 	end
 
