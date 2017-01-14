e10003c7dbc84114aa7db4e172210b281ba0f8fb
Jon Gutwillig <programcodex@gmail.com>
2017-01-13 19:03:16 -0800

    not making a signup without info

--- app/services/ops_clover.rb
+++ app/services/ops_clover.rb
@@ -150,7 +150,7 @@ class OpsClover
 	end
 
 	def make_signup
-		if @signup.nil?
+		if @signup.nil? && @merchant_name.present? && @merchant_email.persent?
 			@signup = MerchantSignup.new_clover @args
 			if @signup.save
 				# 2. make a clover client and connect to the merchant signup
