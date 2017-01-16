152314a0e69824bbab1e5c69f515cdbfce2c6068
Jon Gutwillig <programcodex@gmail.com>
2017-01-16 12:52:22 -0800

    forensic updates

--- app/accounting/forensic.rb
+++ app/accounting/forensic.rb
@@ -1,6 +1,30 @@
 class Forensic
 
+	def self.gifts
+		Gift.includes(:registers).all.find_each do |g|
 
+			regs = gift.registers
+
+			total = Register.sum_gift(regs)
+
+		end
+	end
+
+	def self.registers
+		c = []
+
+		Register.all.find_each do |r|
+			errored = nil
+
+			if r.ccy.nil?
+				errored = true
+				c << r
+			end
+
+
+
+		end
+	end
 
 
 #   -------------
@@ -40,6 +64,11 @@ class Forensic
 				wccy << s if !g.nil? && g.ccy != s.ccy
 			end
 		end
+		# err is for when revenue != revenue_cents -- this no longer matters because revenue is not used
+		# none is when sale revenue_cents is nil -
+		# ccy is when sales is not usd but not converted to usd
+		# miss is when ccy is missing
+		# wrong is when gift ccy != sale ccy
 		{ err: e, none: n, ccy: c, miss: m, wrong: wccy }
 	end
 
