040708f8553a3ab2e9f6c2f1fdd64bedfa716525
Jon Gutwillig <programcodex@gmail.com>
2017-01-16 16:45:23 -0800

    adding headrs inspect after keys broke

--- app/controllers/metal_cors_controller.rb
+++ app/controllers/metal_cors_controller.rb
@@ -21,7 +21,7 @@ protected
 
     def authentication_clover
         puts "\nCLOVER AUTH - #{request.headers["HTTP_X_AUTH_TOKEN"]} - #{request.headers['HTTP_X_APPLICATION_KEY']}\n"
-        puts "Here are the header keys #{request.headers.keys}"
+        puts "Here are the header keys #{request.headers.inspect}"
         successful = authenticate_general
     end
 
