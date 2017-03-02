2a5621643470624d366e4dc464968e5d424aa5d2
Jon Gutwillig <programcodex@gmail.com>
2017-03-02 14:32:24 -0800

    testing for stripe keys in params

--- app/services/ops_iom_api_pos.rb
+++ app/services/ops_iom_api_pos.rb
@@ -25,11 +25,11 @@ OpsIomApiPos = Struct.new(:redemption, :gift, :server) do
     	if success?
     		status = redemption.status
     		redemption.status = 'done'
-			h = { "success" => success?, "response_code" => 'APPLIED', "response_text"=>{"amount_applied" => applied_value, 'msg' => redemption.msg }, api: { system: 'OpsIomApiPos'} }
+			h = { "success" => success?, "response_code" => 'APPLIED', "response_text"=> { "amount_applied" => applied_value, 'msg' => redemption.msg }, 'api' => { 'system' => 'OpsIomApiPos'} }
 			redemption.status = status
 			h
 		else
-			{ "success" => success?, "response_code" => "ERROR", "response_text"=>{"amount_applied" => 0, 'msg' => 'POS system unavailable' }, api: { system: 'OpsIomApiPos'} }
+			{ "success" => success?, "response_code" => "ERROR", "response_text"=> { "amount_applied" => 0, 'msg' => 'POS system unavailable' }, 'api' => { 'system' => 'OpsIomApiPos'} }
 		end
 	end
 
