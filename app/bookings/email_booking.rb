30d353c71976060cfb4382d5796ebfa748f1bdeb
Jon Gutwillig <programcodex@gmail.com>
2017-05-16 19:14:59 -0700

    adding set bcc

--- app/bookings/email_booking.rb
+++ app/bookings/email_booking.rb
@@ -20,8 +20,8 @@ class EmailBooking < EmailAbstract
 		@template = template
 		@subject = email_data_hsh['subject']
 		@body = email_data_hsh['html']
-		set_bcc
 		@to_emails  = [{"email" => email_data_hsh['email'], "name" => email_data_hsh['name'] }]
+		set_bcc
 		set_email_message_data
 		set_vars
 	end
