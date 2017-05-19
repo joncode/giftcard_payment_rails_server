a70d7ec85dba1b9bb3284c2702e64f692be46311
Jon Gutwillig <programcodex@gmail.com>
2017-05-18 17:20:32 -0700

    leaving off record call

--- app/validators/booking_date_validator.rb
+++ app/validators/booking_date_validator.rb
@@ -3,7 +3,7 @@ class BookingDateValidator < ActiveModel::Validator
     def validate(record)
         book = record.book
         if book
-            earliest_booking_date = record.book.book_by
+            earliest_booking_date = book.book_by
             if record.date1 && (record.date1 < earliest_booking_date)
                 return record.errors[:primary_date] << "Booking creation was not successful. Date #{record.date1_to_s} is not far enough in advance. Must be after #{earliest_booking_date.to_formatted_s(:only_date)}"
             end
