83acfe5a915fdc90c81a961b8144969cfee64f53
Jon Gutwillig <programcodex@gmail.com>
2017-07-24 17:08:40 -0700

    updating book to new db schema

--- app/bookings/book.rb
+++ app/bookings/book.rb
@@ -22,17 +22,17 @@ class Book < ActiveRecord::Base
 		end
 	end
 
-    auto_strip_attributes :name, :zinger, :detail, :notes
+    auto_strip_attributes :name, :zinger, :detail, :notes,
+	    :member1, :member2, :member3, :member4,
+		:member1_name, :member2_name, :member3_name, :member4_name,
+    	:photo1, :photo2, :photo3, :photo4,
+		:photo1_name, :photo2_name, :photo3_name, :photo4_name,
+		:price1_name, :price2_name
 
-	validates_presence_of :name, :merchant_id, :advance_days
+	validates_presence_of :name, :merchant_id, :advance_days, :min_ppl, :max_ppl, :price1
 
 	belongs_to :merchant
-	attr_accessor :price_dollars, :price_wine_dollars, :chef, :sommelier, :general_manager, :other,
-		:photo1, :photo2, :photo3, :photo4
 
-# ---------------
-
-	before_save :tighten_up_json
 
 # ---------------
 
@@ -76,11 +76,19 @@ class Book < ActiveRecord::Base
 
 # ---------------
 
+	def merchant_name
+		return self.merchant ? self.merchant.name : 'No Merchant'
+	end
+
 	def merchant_list_serialize
 		m = self.merchant
 		return { name: 'Merchant' } if m.nil?
 		m
 	end
+
+    def get_photo
+    	self.photo1 || self.photo2 || self.photo3 || self.photo4
+    end
 
     def basic_serialize
 		{
@@ -94,17 +102,10 @@ class Book < ActiveRecord::Base
 	    	href: itsonme_url, api_url: api_url, shop_url: shop_url,
 	        	# LIST PRESENTATION DATA
 	    	name: self.name, zinger: self.zinger, detail: self.detail, notes: self.notes,
-	        photo: self.get_photo, ccy: self.ccy, price: self.price, price_wine: self.price_wine,
+	        photo: self.get_photo, ccy: self.ccy,
 	        advance_days: self.advance_days, min_ppl: self.min_ppl, max_ppl: self.max_ppl,
 			duration_minutes: duration, duration_desc: duration_desc
    		}
-    end
-
-    def prices_serialize
-    	[
-    		{ price: self.price, ccy: self.ccy, title: 'Price without Wine'},
-    		{ price: self.price_wine, ccy: self.ccy, title: 'Price with Wine'}
-    	]
     end
 
     def list_serialize
@@ -115,6 +116,33 @@ class Book < ActiveRecord::Base
     	h
     end
     alias_method :serialize, :list_serialize
+
+	def members_serialize
+		ary = []
+		ary << { title: self.member1, name: self.member1_name } if self.member1
+		ary << { title: self.member2, name: self.member2_name } if self.member2
+		ary << { title: self.member3, name: self.member3_name } if self.member3
+		ary << { title: self.member4, name: self.member4_name } if self.member4
+		ary
+	end
+
+	def photos_serialize
+		ary = []
+		ary << { url: self.photo1, detail: self.photo1_name } if self.photo1
+		ary << { url: self.photo2, detail: self.photo2_name } if self.photo2
+		ary << { url: self.photo3, detail: self.photo3_name } if self.photo3
+		ary << { url: self.photo4, detail: self.photo4_name } if self.photo4
+		ary
+	end
+
+    def prices_serialize
+    	ary = []
+    	ary << { price: self.price1, ccy: self.ccy, title: self.price1_name } if self.price1
+    	ary << { price: self.price2, ccy: self.ccy, title: self.price2_name } if self.price2
+    	ary
+    end
+
+# ---------------
 
 	def timezone
 		self.merchant ? self.merchant.time_zone : "Pacific Time (US & Canada)"
@@ -144,21 +172,6 @@ class Book < ActiveRecord::Base
         "#{CLEAR_CACHE}/share/menu_items/#{token}/book"
     end
 
-    def get_photo
-    	self.photos.each do |name, photo_url|
-    		if photo_url.blank?
-    			next
-    		else
-    			return photo_url
-    			break
-    		end
-    	end
-    	nil
-    end
-
-    def photo
-    	get_photo
-    end
 
 # ---------------
 
@@ -174,169 +187,17 @@ class Book < ActiveRecord::Base
 		"#{BOOKING_ID}-#{self.id}-#{make_slug(self.name)}"
 	end
 
-	def price_dollars
-		display_money(cents: self.price)
-	end
 
-	def price_dollars= val
-		self.price = currency_to_cents(val)
-	end
+# ---------------
 
-	def price_wine_dollars
-		display_money(cents: self.price_wine)
-	end
-
-	def price_wine_dollars= val
-		self.price_wine = currency_to_cents(val)
-	end
 
 
 # ---------------
 
 
-	def get_member_key key
-		return nil if self.members.nil?
-		self.members[key]
-	end
-
-	def set_member_key key, str
-		str = str.to_s.strip
-		if self.members.nil?
-			h = { key => str }
-		else
-			h = self.members
-			h[key] = str.strip
-		end
-		self.members = h
-	end
-
-	def chef
-		get_member_key 'chef'
-	end
-
-	def chef= str
-		set_member_key 'chef', str
-	end
-
-	def sommelier
-		get_member_key 'sommelier'
-	end
-
-	def sommelier= str
-		set_member_key 'sommelier', str
-	end
-
-	def general_manager
-		get_member_key 'general_manager'
-	end
-
-	def general_manager= str
-		set_member_key 'general_manager', str
-	end
-
-	def other
-		get_member_key 'other'
-	end
-
-	def other= str
-		set_member_key 'other', str
-	end
-
-	def members_serialize
-		ps = self.members
-		ary = []
-		ps.each do |k,v|
-			ary << { name: v, title: k.titleize }
-		end
-		ary
-	end
-
-# ---------------
-
-
-	# def method_missing(method_name)
-	# 	puts "#{method_name}"
-	# 	str = method_name.to_s
-	# 	if str.match(/photo/)
-	# 		get_photo_key(method_name)
-	# 	end
-	# end
-
-	def get_photo_key key
-		return nil if self.photos.nil?
-		self.photos[key]
-	end
-
-	def set_photo_key key, photo_url
-		photo_url = photo_url.to_s.strip
-		if self.photos.nil?
-			h = { key => photo_url }
-		else
-			h = self.photos
-			h[key] = photo_url
-		end
-		self.photos = h
-	end
-
-	def photos_serialize
-		ps = self.photos
-		ary = []
-		ps.each do |k,v|
-			ary << { url: v, detail: "#{k} detail" }
-		end
-		ary
-	end
-
-	def photo1
-		get_photo_key 'photo1'
-	end
-
-	def photo1= photo_url
-		set_photo_key 'photo1', photo_url
-	end
-
-	def photo2
-		get_photo_key 'photo2'
-	end
-
-	def photo2= photo_url
-		set_photo_key 'photo2', photo_url
-	end
-
-	def photo3
-		get_photo_key 'photo3'
-	end
-
-	def photo3= photo_url
-		set_photo_key 'photo3', photo_url
-	end
-
-	def photo4
-		get_photo_key 'photo4'
-	end
-
-	def photo4= photo_url
-		set_photo_key 'photo4', photo_url
-	end
 
 
 private
 
-    def members_tight
-    	self.members.select{ |k,v| !v.blank? }
-    end
-
-    def photos_tight
-    	self.photos.select{ |k,v| !v.blank? }
-    end
-
-	def tighten_up_json
-		if self.members.present?
-			self.members = members_tight
-		end
-		if self.photos.present?
-			self.photos = photos_tight
-		end
-	end
 
 end
