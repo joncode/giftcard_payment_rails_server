c52f7aa499a2901f1e1540e69b4741d0059a6770
Jon Gutwillig <programcodex@gmail.com>
2017-07-24 17:39:22 -0700

    merging schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20170721200849) do
-=======
 ActiveRecord::Schema.define(version: 20170724225208) do
->>>>>>> master
+
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
