a781b514870d0287e5236053e107b3adf58751eb
Jon Gutwillig <programcodex@gmail.com>
2017-09-07 15:56:35 -0700

    adding test to print xml test

--- app/services/print_test_redemption.rb
+++ app/services/print_test_redemption.rb
@@ -11,11 +11,21 @@ class PrintTestRedemption
 		# make a fake redemption at location
 		redemption = Redemption.new(merchant: merchant)
 		# add a fake hex_id  xx_7234_h23i
-		redemption.hex_id = "xx_1234abcd"
+		redemption.hex_id = "xx_#{@job}"
 		redemption.gift = Gift.where(cat: 300, receiver_name: "David Leibner", giver_name: 'David Leibner').first
 		# get xml from calling :to_epson_xml on the fake redemption
 		# insert test xml in between the actual xml
-		redemption.to_epson_xml
+		xml = redemption.to_epson_xml
+
+		make_test(xml)
+	end
+
+	def make_test(xml)
+		xml.gsub('<feed line="', '<feed line="2"/>
+<text align="center"/>
+<text reverse="false" ul="false" em="true"/>
+<text width="2" height="1"/>
+<text>TEST VOID TEST VOID</text><feed line="')
 	end
 
 end
