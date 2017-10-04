0f6c941be645032e530c50df7e537a1ebafa003b
Jon Gutwillig <programcodex@gmail.com>
2017-10-03 18:34:38 -0700

    adding better hex_id

--- app/services/print_test_redemption.rb
+++ app/services/print_test_redemption.rb
@@ -11,7 +11,7 @@ class PrintTestRedemption
 		# make a fake redemption at location
 		redemption = Redemption.new(merchant: merchant)
 		# add a fake hex_id  xx_7234_h23i
-		redemption.hex_id = "xx_#{@job}"
+		redemption.hex_id = "xx_1234abcd"
 		redemption.gift = Gift.where(cat: 300, receiver_name: "David Leibner", giver_name: 'David Leibner').first
 		# get xml from calling :to_epson_xml on the fake redemption
 		# insert test xml in between the actual xml
