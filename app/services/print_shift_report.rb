915016fe9b1e41d49de364f121aa816b60a35090
Jon Gutwillig <programcodex@gmail.com>
2017-10-03 18:08:30 -0700

    adding shift report

--- app/services/print_shift_report.rb
+++ app/services/print_shift_report.rb
@@ -1,18 +1,15 @@
 class PrintShiftReport
 	include MoneyHelper
-	include PrintXmlWrap
-    include PrintXmlHeader
-    include PrintXmlTitle
-    include PrintXmlFooter
 
 
-	attr_reader :job, :merchant, :redemptions, :range, :ary, :total, :quantity
+	attr_reader :job, :merchant, :redemptions, :range, :ary, :total, :quantity, :columns
 
 	def initialize merchant, job=nil
 		@job = job
 		@merchant = merchant
 		@range = [@merchant.shift_start .. @merchant.now]
 		@redemptions = Redemption.done_for_merchant_in_range(@merchant, @range)
+		@columns = ""
 	end
 
 	def perform
@@ -22,28 +19,76 @@ class PrintShiftReport
 			@total += r.amount
 			[display_money(cents: r.amount, ccy: r.ccy) , r.paper_id ,  r.redemption_time]
 		end
+		cols = @ary.map do |data_ary|
+"<feed line='2'/>
+<text align='left'/>
+<text font='font_a'/>
+<text width='1' height='1'/>
+<text reverse='false' ul='false' em='false' color='color_1'/>
+<text>#{data_ary[1]}</text>
+<text>&#9;&#9;</text>
+<text>#{data_ary[0]}</text>"
+		end
+		@columns = cols.join('')
 		@total = display_money(cents: @total, ccy: @merchant.try(:ccy))
 	end
 
 	def to_epson_xml
-		xml_wrap(epson_xml)
+%{
+<ePOSPrint>
+<Parameter>
+<devid>local_printer</devid>
+<timeout>20000</timeout>
+<printjobid>#{job}</printjobid>
+</Parameter>
+<PrintData>
+<epos-print xmlns="http://www.epson-pos.com/schemas/2011/03/epos-print">
+<text lang="en"/>
+<text smooth="true"/>
+<text align="center"/>
+<text font="font_b"/>
+<text width="3" height="3"/>
+<text reverse="false" ul="false" em="true" color="color_1"/>
+<text>ItsOnMe Shift Report</text>
+<feed unit="12"/>
+<feed line="2"/>
+<text font="font_a"/>
+<text width="2" height="2"/>
+<text reverse="false" ul="false" em="false" color="color_1"/>
+<text>#{@merchant.name}</text>
+<feed line="2"/>
+<text font="font_c"/>
+<text width="1" height="1"/>
+<text reverse="false" ul="false" em="false" color="color_1"/>
+<text>#{@merchant.address}</text>
+<feed line="2"/>
+<text font="font_b"/>
+<text width="1" height="2"/>
+<text reverse="false" ul="false" em="false" color="color_1"/>
+<text>#{@merchant.current_time}</text>
+#{columns}
+<feed line="2"/>
+<text width="1" height="2"/>
+<text reverse="false" ul="false" em="false" color="color_1"/>
+<text>Total</text>
+<text>&#9;&#9;</text>
+<text>#{total}</text>
+<text reverse="false" ul="false" em="false"/>
+<text width="1" height="1"/>
+<feed unit="12"/>
+<feed line="2"/>
+<text align="center"/>
+<text width="1" height="1"/>
+<text reverse="false" ul="false" em="false" color="color_1"/>
+<text>Text Support for any reason #{TWILIO_QUICK_NUM}</text>
+<feed line="3"/>
+<cut type="feed"/>
+</epos-print>
+</PrintData>
+</ePOSPrint>
+}
 	end
 
-	def epson_xml
-		xml_header +
-		xml_title +
-'<feed unit="12"/>
-<text>&#10;</text>
-<text align="left"/>
-<text font="font_a"/>
-<text width="1" height="1"/>
-<text reverse="false" ul="false" em="false" color="color_1"/>
-<text>SHIFT REPORT&#10;</text>
-<text width="1" height="1"/>
-<text reverse="false" ul="false" em="false" color="color_1"/>
-<text>#{merchant.current_time}</text>' +
-		xml_footer
-	end
 
 end
 
