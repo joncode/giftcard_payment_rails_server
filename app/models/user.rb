33a4a34d27aacdabbb8d541b12607bf75ac29fe9
Jon Gutwillig <programcodex@gmail.com>
2017-10-03 17:49:32 -0700

    adding brandon to QA app

--- app/models/user.rb
+++ app/models/user.rb
@@ -97,7 +97,7 @@ class User < ActiveRecord::Base
 
 	def coupons
 		admin_user_ary = [ 8248, 30003, 7689, 27891, 19192, 378 ]
-		admin_user_ary = [ 328 ] if Rails.env.staging?
+		admin_user_ary = [ 328, 841 ] if Rails.env.staging?
 		if admin_user_ary.include?(self.id)
 			[ 'no service fee', '10% off value']
 		else
