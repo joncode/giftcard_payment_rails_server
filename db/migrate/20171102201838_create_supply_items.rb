f441aa00008ac5498ca36b0329993c5a8b64f99a
Jon Gutwillig <programcodex@gmail.com>
2017-11-02 16:01:19 -0500

    seeding data

--- db/migrate/20171102201838_create_supply_items.rb
+++ db/migrate/20171102201838_create_supply_items.rb
@@ -1,15 +1,34 @@
 class CreateSupplyItems < ActiveRecord::Migration
-  def change
-    create_table :supply_items do |t|
-      t.string :hex_id
-      t.string :name
-      t.integer :price
-      t.string :ccy, default: 'USD'
-      t.string :detail
-      t.string :photo_url
-      t.boolean :active, default: true
+	def change
+		create_table :supply_items do |t|
+			t.string :hex_id
+			t.string :name
+			t.integer :price
+			t.string :ccy, default: 'USD'
+			t.string :detail
+			t.string :photo_url
+			t.boolean :active, default: true
 
-      t.timestamps null: false
-    end
-  end
+			t.timestamps null: false
+		end
+
+		seed_supply_items
+	end
+
+	def seed_supply_items
+		[{
+		    name: "Epson Printer (TN-T88VI)",
+		    price: 55000,
+		    ccy: 'USD'
+		},{
+		    name:    "100 Check Presenters",
+		    price: 650,
+		    ccy: 'USD'
+		},{
+		    name:  "50 Table Tents",
+		    price: 1000,
+		    ccy: 'USD'
+		}].map { |s| SupplyItem.create s }
+	end
+
 end
