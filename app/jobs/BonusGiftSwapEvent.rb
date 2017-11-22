3ac34998e03d27ce17889cc6a624591badd3b5fe
Terra Bilderback <terra@itson.me>
2017-11-21 17:40:50 -0800

    Added logging, cleanup to BonusGiftSwapEvent

--- app/jobs/BonusGiftSwapEvent.rb
+++ app/jobs/BonusGiftSwapEvent.rb
@@ -1,39 +1,36 @@
 class BonusGiftSwapEvent
-
     @queue = :database
 
 	def self.perform proto_id
-
 		# we do not need set_value , the swap exists based on the value of the underlying proto
-
 		proto = Proto.where(bonus: true, id: proto_id).first
-		if proto.bonus
-			if target == proto.target
+		return  unless proto.bonus
 
-					# do the swap
-
-				if proto.bonus_on?
-					set_menu_item_bonus proto, target
-				else
-					set_menu_item_normal proto, target
-				end
-			end
+		if proto.bonus_on?
+			set_menu_item_bonus proto, proto.target
+		else
+			set_menu_item_normal proto, proto.target
 		end
-
 	end
 
 
 	def self.set_menu_item_bonus proto, target
+		puts "[job BonusGiftSwapEvent :: set_menu_item_bonus]"
 		target.update(photo: proto.bonus_photo, detail: proto.bonus_detail)
+		puts "[job BonusGiftSwapEvent :: set_menu_item_bonus] target: #{target.inspect}"
+
 		# bust the WWW menu item cache
 		WwwHttpService.clear_merchant_cache
 	end
+
 
 	def self.set_menu_item_normal proto, target
+		puts "[job BonusGiftSwapEvent :: set_menu_item_normal]"
 		target.update(photo: proto.item_photo, detail: proto.item_detail)
+		puts "[job BonusGiftSwapEvent :: set_menu_item_bonus] target: #{target.inspect}"
+
 		# bust the WWW menu item cache
 		WwwHttpService.clear_merchant_cache
 	end
-
 
 end
