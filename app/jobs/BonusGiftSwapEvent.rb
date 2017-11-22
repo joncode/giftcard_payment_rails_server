e874a706e4930c25a41fb1ea751943538086294b
Jon Gutwillig <programcodex@gmail.com>
2017-11-21 20:09:46 -0700

    better documentation for menu system

--- app/jobs/BonusGiftSwapEvent.rb
+++ app/jobs/BonusGiftSwapEvent.rb
@@ -12,14 +12,19 @@ class BonusGiftSwapEvent
 			set_menu_item_normal proto, proto.target
 		end
 
-		# compile menu to app
-		merchant = proto.merchant
-		merchant.menu.compile_menu_to_app
-		# creatte the new json response for menu
-		cache_resp = merchant.menu_string
-		# save that menu to Redis
-		RedisWrap.set_menu(merchant.menu_id, cache_resp)
-		WwwHttpService.clear_merchant_cache
+		# here is the process for busting the 3-layer menu cache system that exists
+			# Layers
+				# 1 - menu items are pre-compiled into menu.json column ( called via merchant.menu_string )
+					# compile menu to app
+					merchant = proto.merchant
+					merchant.menu.compile_menu_to_app
+				# 2 - menu string JSON is stored in the redis layer
+					# create the new json response for menu
+					cache_resp = merchant.menu_string
+					# save that menu to Redis
+					RedisWrap.set_menu(merchant.menu_id, cache_resp)
+				# 3 - some WBG gifting pages use a cache on that layer , you must bust that
+					WwwHttpService.clear_merchant_cache
 	end
 
 
