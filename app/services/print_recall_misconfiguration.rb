d537ab084f41f85aaedf774eaec0242ef9cbbc09
Terra Bilderback <terra@itson.me>
2017-12-22 14:25:07 -0800

    Restored correct PrintRecallMisconfiguration class

--- app/services/print_recall_misconfiguration.rb
+++ app/services/print_recall_misconfiguration.rb
@@ -2,32 +2,37 @@ class PrintRecallMisconfiguration
     include PrintXmlWrap
     include PrintXmlRecallHeader
     include PrintXmlRecallFooter
+    include PrintUtility
 
     attr_reader :job
 
     def initialize
-        @job = 8612
+        @job = 8600
     end
 
+
     def to_epson_xml
-        complete_xml_wrap(epson_xml)
+        xml_wrap(epson_xml)
     end
 
     def epson_xml
         xml_recall_header +
-        xml_recall_content_faulty +
+        xml_recall_content_misconfiguration +
+        line_xml +
         xml_recall_footer
     end
 
 
 private
 
-    def xml_recall_content_faulty
-        '<text align="left"/>' +
+    def xml_recall_content_misconfiguration
+        '<text align="center"/>' +
         '<text font="font_a"/>' +
         '<text width="1" height="1"/>' +
         '<text reverse="false" ul="false" em="false" color="color_1"/>' +
-        '<text>Your printer has been recalled. This is likely due to damage or defects we\'ve detected.</text>'
+        '<text>This printer\'s configuration is incorrect</text>' +
+        '<feed line="1" />'  +
+        '<text>and requires support.</text>'
     end
 
 end
