581a64acfb0fa119def4aab6e6d4d63d1c9401b6
Terra Bilderback <terra@itson.me>
2018-01-17 20:17:56 -0800

    Fixed typo

--- app/models/client.rb
+++ app/models/client.rb
@@ -25,7 +25,7 @@ class Client < ActiveRecord::Base
 
 	has_many :gifts
 	has_many :redemptions
-	mas_many :epson_printers
+	has_many :epson_printers
 
 	belongs_to :list
 	belongs_to :partner, polymorphic: true
