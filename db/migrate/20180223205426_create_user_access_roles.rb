9380ad6f26ede10228074de7738b007257ff42ae
Terra Bilderback <terra@itson.me>
2018-03-03 04:09:29 -0800

    Fixed erroneous change in migration

--- db/migrate/20180223205426_create_user_access_roles.rb
+++ db/migrate/20180223205426_create_user_access_roles.rb
@@ -1,7 +1,7 @@
 class CreateUserAccessRoles < ActiveRecord::Migration
   def change
     create_table :user_access_roles do |t|
-      t.string   :code,    null: false   # Role identifier
+      t.string   :role,    null: false   # Role identifier
       t.string   :label,   null: false   # Displayed to the user
       t.boolean  :active,  null: false,  default: true
     end
