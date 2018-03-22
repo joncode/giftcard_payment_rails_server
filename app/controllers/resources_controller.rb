9f616345cc335b0d044cfee6d29049461b603660
Terra Bilderback <terra@itson.me>
2018-03-21 17:25:28 -0700

    Added content checks and 400, and exposed size= option on qr code api

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -2,8 +2,17 @@ class ResourcesController < ApplicationController
 
     # GET /resources/qr?content=
     def qr
+        return head 400  if params[:content].nil? || params[:content].strip.empty?
+
+        # Not particularly useful, but let's expose it anyway.
+        options = {}
+        options[:size] = params[:size].to_i  if params[:size].present?
+
         send_data(
-            RQRCode::QRCode.new(params[:content]).as_png,
+            RQRCode::QRCode.new(
+                params[:content],
+                options
+            ).as_png,
             type: 'image/png',
             disposition: 'inline'
         )
