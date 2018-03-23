10c16fa078f578a5daf32cff7c528098f10fefa0
Terra Bilderback <terra@itson.me>
2018-03-22 21:08:29 -0700

    Updated sizing of test code handout pdf

--- app/controllers/resources/merchant_controller.rb
+++ app/controllers/resources/merchant_controller.rb
@@ -1,7 +1,7 @@
 class Resources::MerchantController < ResourcesController
 
     def handout
-        render pdf: "team_code_handout", encoding: :utf8
+        render pdf: "team_code_handout", encoding: :utf8, page_size: nil, page_width: '7.5in', page_height: '10in'
     end
 
 end
