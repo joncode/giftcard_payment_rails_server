77fe8b3fe915e847bb20279c61d7d37841cafd1f
Terra Bilderback <terra@itson.me>
2018-05-23 18:02:14 -0700

    Added Epson printer daily system alert

---
+++ app/alerts/epson_printer_daily_sys_alert.rb
@@ -0,0 +1,63 @@
+class EpsonPrinterDailySysAlert < Alert
+
+	# description:
+	# Send a summary of all printers requiring attention (critical issue + offline count)
+
+#   -------------
+
+	def text_msg
+		data = get_data
+
+		sms  = "Daily IOM Printer Report: "
+		sms += "#{data[:attention]} printers have critical issues"
+		sms += "; #{data[:offline].count} of them are OFFLINE"  unless data[:offline].empty?
+		sms += ". See https://admin.itson.me/epson_printers for details."
+		sms
+	end
+
+	def email_msg
+		data = get_data
+
+		markup  = "<h3>Epson Printer Daily Report</h3>"
+		markup += "<strong>There are #{data[:attention]} printers with critical issues"
+		markup += "; <span style=\"color: firebrick\">#{data[:offline].count} of them are OFFLINE</span>"  unless data[:offline].empty?
+		markup += ".</strong>"
+
+		unless data[:offline].empty?
+			markup += "<br/><br/>"
+			markup += "Here's the list of offline printers:<br/>"
+			markup += "<ul>"
+			data[:offline].each do |name|
+				markup += "<li>#{name}</li>"
+			end
+			markup += "</ul>"
+		end
+
+		markup += "<br/>"
+		markup += "Open the <a href=\"https://admin.itson.me/epson_printers\">printer tracking page</a> for more information."
+
+		markup.html_safe
+	end
+
+	def msg
+		text_msg
+	end
+
+#   -------------
+
+	def get_data
+		data = { attention: 0, offline: [] }
+		# Order printers by offline duration
+		EpsonPrinter.all.order(last_poll_capture_at: :asc).each do |printer|
+			if printer.offline? || printer.paper_out? || printer.cover_left_open? \
+				|| printer.has_recent_mechanical_error? || printer.has_recent_cutter_error?
+				data[:attention] += 1
+			end
+			next  unless printer.offline?
+			data[:offline] << (printer.client.partner.name  rescue "(error; printer ##{printer.id})" )
+		end
+
+		data
+	end
+
+end
--- app/models/epson_printer.rb
+++ app/models/epson_printer.rb
@@ -1,3 +1,41 @@
 class EpsonPrinter < ActiveRecord::Base
     belongs_to :client
+
+    def offline?
+        (self.last_poll_capture_at.present? && self.last_poll_capture_at <= 12.hours.ago)
+    end
+
+    def paper_low?
+        self.paper_low_at.present?
+    end
+
+    def paper_out?
+        # Within 12 minutes they may be changing the paper.
+        (self.paper_out_at.present? && self.paper_out_at <= 12.minutes.ago)
+    end
+
+    def has_mechanical_error?
+        self.last_mechanical_error_at.present?
+    end
+
+    def has_cutter_error?
+        self.last_cutter_error_at.present?
+    end
+
+    def has_recent_mechanical_error?
+        has_mechanical_error? && self.last_mechanical_error_at > 1.week.ago
+    end
+
+    def has_recent_cutter_error?
+        has_cutter_error? && self.last_cutter_error_at > 1.week.ago
+    end
+
+    def cover_open?
+        self.cover_open_at.present?
+    end
+
+    def cover_left_open?
+        cover_open? && self.cover_open_at <= 30.minutes.ago
+    end
+
 end
