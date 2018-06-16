f4c995a78a37699d17d6094f768f584099439c2e
Terra Bilderback <terra@itson.me>
2018-06-15 20:26:52 -0700

    Refactored paper cert generation

--- app/controllers/papergifts_controller.rb
+++ app/controllers/papergifts_controller.rb
@@ -1,46 +1,52 @@
 class PapergiftsController < ApplicationController
 
+    # GET  /papergifts/:id
     def paper_cert
-        puts "PAPER GIFT REQUEST #{params.inspect}"
+        # Publicly-accessible API despite the controller location
+        puts "[api Papergifts :: paper_cert]  params: #{params.inspect}"
 
         @gift = Gift.includes(:merchant).find_with(params[:id])
         raise ActiveRecord::RecordNotFound  unless @gift.kind_of?(Gift)
+        raise ActiveRecord::RecordInvalid   unless valid_status?(@gift.status)
 
         @hand_delivery = (@gift.status == 'hand_delivery')
+
+        # Create redemption of the appropriate type
         type = :paper
         type = :hand_delivery  if @hand_delivery
+        resp = Redeem.start(gift: @gift, api: "/papergifts/#{params[:id]}", type_of: type)
+        unless resp['success']
+            # Something's amiss.
+            render :text => resp['response_text']  and return
+        end
 
+        # 'Kay, everything's coo'
+        # Pluck out the data needed to build the cert
+        @redemption = resp['redemption']
+        @gift       = resp['gift']        ##?  Doesn't this point at the same gift?
+        @items      = @gift.cart.map{|item| "#{item['quantity']}x #{item['item_name']}" }
 
-        if ['hand_delivery', 'incomplete', 'open', 'notified', 'schedule'].include?(@gift.status)
-            resp = Redeem.start(gift: @gift, api: "/papergifts/#{params[:id]}", type_of: type)
-            # puts resp.inspect
-            if resp['success']
-                @redemption = resp['redemption']
-                @gift = resp['gift']
+        if @items.count > 3
+            _count = @items.count
+            @items = @items[0..1]
+            @items << "(And #{_count} more)"
+        end
 
-                @items = @gift.cart.collect do |item|
-                    "#{item['quantity']}x #{item['item_name']}"
-                end
+        @value_dollars, @value_cents = @gift.value.split(".")
+        @value_cents ||= "00"
 
-                if @items.count > 3
-                    _count = @items.count
-                    @items = @items[0..1]
-                    @items << "(And #{_count} more)"
-                end
-
-                @value_dollars, @value_cents = @gift.value.split(".")
-                @value_cents ||= "00"
-
-                respond_to do |format|
-                    format.html
-                    format.pdf do
-                        render pdf: "paper_gifts", encoding: :utf8
-                    end
-                end
-            else
-                render :text => resp['response_text']
+        respond_to do |format|
+            format.html
+            format.pdf do
+                render pdf: "paper_gifts", encoding: :utf8
             end
         end
+    end
+
+private
+
+    def valid_status?(status)
+        ['hand_delivery', 'incomplete', 'open', 'notified', 'schedule'].include?(status)
     end
 
 end
