22ec8348df588d0055e4e78815231938c20ddba5
Terra Bilderback <terra@itson.me>
2018-06-28 18:02:43 -0700

    Disable zipcode validation to allow purchases with 4-digit international postal codes

--- app/models/card.rb
+++ app/models/card.rb
@@ -36,7 +36,7 @@ class Card < ActiveRecord::Base
 	validate :month_and_year_should_be_in_future
 	validate :at_least_two_words_in_name
 	validates_presence_of :csv, :last_four, :month, :year, :nickname, :user_id, :name
-	validates :zip, zip_code: true, allow_blank: true
+	# validates :zip, zip_code: true, allow_blank: true
 
 #   -------------
 
