6203f0ef69f4e99be0c41d448451272dcc0813ca
Terra Bilderback <terra@itson.me>
2018-07-03 13:52:31 -0700

    (minor) Correct mixed indentation

--- app/alerts/daily_report_sys_alert.rb
+++ app/alerts/daily_report_sys_alert.rb
@@ -19,7 +19,7 @@ class DailyReportSysAlert < Alert
 
 	def get_data
 		time_period = 24.hours.ago.beginning_of_day
-        @header = TimeGem.dt_to_s(time_period)
+		@header = TimeGem.dt_to_s(time_period)
 		super between: (time_period ... (time_period+24.hours))
 	end
 
