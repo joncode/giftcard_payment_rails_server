e8c7b75d3072f71dd1d3de0b4b58ad1dbe3b5b3a
Terra Bilderback <terra@itson.me>
2018-08-31 23:36:26 -0700

    (bugfix) Double texting: disable secondary text notification for cat300 gifts within background job

--- app/jobs/GiftCreatedEvent.rb
+++ app/jobs/GiftCreatedEvent.rb
@@ -14,8 +14,16 @@ class GiftCreatedEvent
             unless gift.status == 'schedule'
                 puts "\n#{_signature}  -> notify_via_facebook()"
                 notify_via_facebook gift
-                puts "\n#{_signature}  -> gift.notify_via_text()"
-                gift.notify_via_text
+
+                # Disable notifying for cat300 (Standard gift) because these are already notified after creation.
+                # However, 301 (StndRegift) and 307 (StndBoom) may still need to notify.
+                # (I don't know this portion of the system well enough to say.)
+                if gift.cat != 300
+                    puts "\n#{_signature}  -> gift.notify_via_text()"
+                    gift.notify_via_text
+                else
+                    puts "\n#{_signature}  Cat 300; Not notifying via text."
+                end
             end
 
             # PointsForSaleJob.perform gift_id
