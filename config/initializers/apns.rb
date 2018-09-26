c9c611647532f289059a570e1dec3c3028d13355
Terra Bilderback <terra@itson.me>
2018-09-26 16:29:50 -0700

    Apple Certs: Replaced expiring certs

--- config/initializers/apns.rb
+++ config/initializers/apns.rb
@@ -1,11 +1,11 @@
 
 if Rails.env.production? || Rails.env.development?
 	APNS.host = "gateway.push.apple.com"
-	APNS.pem = File.join(Rails.root, "config/certs/aps-8-28-2017-PROD.pem")
+	APNS.pem = File.join(Rails.root, "config/certs/aps-9-26-2018-PROD.pem")
 end
 if Rails.env.staging?
 	APNS.host = "gateway.push.apple.com"
-	APNS.pem = File.join(Rails.root, "config/certs/aps-8-28-2017-QA.pem")
+	APNS.pem = File.join(Rails.root, "config/certs/aps-9-26-2018-QA.pem")
 end
 # if Rails.env.development?
 # 	APNS.host = "gateway.sandbox.push.apple.com"
