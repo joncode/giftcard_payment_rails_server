c2572448c7f30b34e4035b4ec2d3a83f99294be2
Terra Bilderback <terra@itson.me>
2018-09-26 17:11:39 -0700

    Apple Cert: added private key password override

--- config/initializers/apns.rb
+++ config/initializers/apns.rb
@@ -2,10 +2,12 @@
 if Rails.env.production? || Rails.env.development?
 	APNS.host = "gateway.push.apple.com"
 	APNS.pem = File.join(Rails.root, "config/certs/aps-9-26-2018-PROD.pem")
+	APNS.pass = "password"
 end
 if Rails.env.staging?
 	APNS.host = "gateway.push.apple.com"
 	APNS.pem = File.join(Rails.root, "config/certs/aps-9-26-2018-QA.pem")
+	APNS.pass = "password"
 end
 # if Rails.env.development?
 # 	APNS.host = "gateway.sandbox.push.apple.com"
